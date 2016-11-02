651783dd4fae0e38c8a18cf1b11f0e15753b75d3
Marc Torrent <marc.torrent@cea.fr>
2016-11-02 16:23:54 +0100

    New error message for getxxx needs changes in some tests

--- src/57_iovars/outvar_o_z.F90
+++ src/57_iovars/outvar_o_z.F90
@@ -601,7 +601,7 @@
  dprarr(3,:)=dtsets(:)%pvelmax(3)
  call prttagm(dprarr,intarr,iout,jdtset_,1,marr,3,narrm,ncid,ndtset_alloc,'pvelmax','DPR',0)
 
- intarr(1,:)=dtsets(:)%pw_unbal_thresh
+ dprarr(1,:)=dtsets(:)%pw_unbal_thresh
  call prttagm(dprarr,intarr,iout,jdtset_,1,marr,1,narrm,ncid,ndtset_alloc,'pw_unbal_thresh','DPR',0)
 
 !###########################################################
