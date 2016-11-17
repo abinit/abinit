a82c2b5565f47650a26563ca2b259264b69e5866
Matteo Giantomassi <matteo.giantomassi@uclouvain.be>
2016-11-17 20:01:30 +0100

    Replace get_rank_1kp with listkk

--- src/78_eph/m_sigmaph.F90
+++ src/78_eph/m_sigmaph.F90
@@ -1345,10 +1345,11 @@ end subroutine sigmaph_free
 
 subroutine sigmaph_solve(self, ikcalc, spin, ebands)
 
+
 !This section has been created automatically by the script Abilint (TD).
 !Do not modify the following lines by hand.
 #undef ABI_FUNC
-#define ABI_FUNC 'nbe'
+#define ABI_FUNC 'sigmaph_solve'
 !End of the abilint section
 
  implicit none
