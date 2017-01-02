b3642eef6cf657a6f7c0cd0845d6e56db121f702
Matteo Giantomassi <matteo.giantomassi@uclouvain.be>
2017-01-02 17:49:34 +0100

    Only master should write MSQD_T and PHDOS files

--- src/98_main/anaddb.F90
+++ src/98_main/anaddb.F90
@@ -454,16 +454,16 @@ program anaddb
 
    call mkphdos(Phdos,Crystal,Ifc, inp%prtdos,inp%dosdeltae,inp%dossmear, inp%ng2qpt, inp%q2shft)
 
-   phdos_fname = TRIM(filnam(2))//"_MSQD_T"
-   call phdos_print_msqd(Phdos, phdos_fname, inp%ntemper, inp%tempermin, inp%temperinc)
-
-   phdos_fname = TRIM(filnam(2))//"_PHDOS"
-   call phdos_print(Phdos,phdos_fname)
-
-   call phdos_print_debye(Phdos, Crystal%ucvol)
-
    if (iam_master) then
 #ifdef HAVE_NETCDF
+     phdos_fname = TRIM(filnam(2))//"_MSQD_T"
+     call phdos_print_msqd(Phdos, phdos_fname, inp%ntemper, inp%tempermin, inp%temperinc)
+
+     phdos_fname = TRIM(filnam(2))//"_PHDOS"
+     call phdos_print(Phdos,phdos_fname)
+
+     call phdos_print_debye(Phdos, Crystal%ucvol)
+
      ncerr = nctk_open_create(phdos_ncid, trim(phdos_fname)//".nc", xmpi_comm_self)
      NCF_CHECK_MSG(ncerr, "Creating PHDOS.nc file")
      NCF_CHECK(crystal_ncwrite(Crystal, phdos_ncid))
