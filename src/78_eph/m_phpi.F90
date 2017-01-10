815110dcdd87158392dbac20a1d5ab2fdd974394
gmatteo <gmatteo@gmail.com>
2017-01-10 18:39:19 +0100

    Fix import

--- src/78_eph/m_phpi.F90
+++ src/78_eph/m_phpi.F90
@@ -26,6 +26,7 @@ module m_phpi
 
  use defs_basis
  use defs_abitypes
+ use defs_datatypes
  use m_profiling_abi
  use m_xmpi
  use m_errors
@@ -45,7 +46,6 @@ module m_phpi
  use m_hamiltonian
  use m_pawcprj
 
- use defs_datatypes,    only : ebands_t
  use m_time,            only : cwtime
  use m_fstrings,        only : sjoin, itoa, ftoa, ktoa, ltoa, strcat
  use m_io_tools,        only : iomode_from_fname
