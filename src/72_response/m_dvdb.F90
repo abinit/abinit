91fc156d12ee46e9f83b0e516eb2d28dd96b5f22
Florian Altvater <altvater@n0001.scs00>
2017-02-22 23:07:33 -0800

    Fixed mrgdv issue with large number of files
    mrgdv used to open all POTx files simulatiously to read headers
    and data in two seperate loops. However, some kernels only allow
    1024 files to be open at the same time, freezing mrgdv if the
    number of files is larger.
    
    Merging the two loops, opening on file at a time and closing it
    right after header and data are read and written fixes this issue.
    The only drawback is that a corrupt file at the end of the loop
    could waste a lot of time.

--- src/72_response/m_dvdb.F90
+++ src/72_response/m_dvdb.F90
@@ -2717,9 +2717,22 @@ subroutine dvdb_merge_files(nfiles, v1files, dvdb_path, prtvol)
  ! Read the headers
  ABI_DT_MALLOC(hdr1_list, (nfiles))
 
+ nperts = size(hdr1_list)
+
+ ! Write dvdb file (we only support fortran binary format)
+ if (open_file(dvdb_path, msg, newunit=ount, form="unformatted", action="write", status="unknown") /= 0) then
+   MSG_ERROR(msg)
+ end if
+ write(ount, err=10, iomsg=msg) dvdb_last_version
+ write(ount, err=10, iomsg=msg) nperts
+ 
+ ! Validate headers.
+ ! TODO: Should perform consistency check on the headers, rearrange them in blocks of q-points.
+ ! ignore POT1 files that do not correspond to atomic perturbations.
+
  do ii=1,nfiles
    write(std_out,"(a,i0,2a)")"- Reading header of file [",ii,"]: ",trim(v1files(ii))
-
+   
    if (endswith(v1files(ii), ".nc")) then
 #ifdef HAVE_NETCDF
       NCF_CHECK(nctk_open_read(units(ii), v1files(ii), xmpi_comm_self))
@@ -2746,21 +2759,8 @@ subroutine dvdb_merge_files(nfiles, v1files, dvdb_path, prtvol)
    ! 111  POT1 files with extra record with vh1(G=0) after FFT data.
    has_rhog1_g0(ii) = .True.
    if (fform == 109) has_rhog1_g0(ii) = .False.
- end do
 
- ! Validate headers.
- ! TODO: Should perform consistency check on the headers, rearrange them in blocks of q-points.
- ! ignore POT1 files that do not correspond to atomic perturbations.
- nperts = size(hdr1_list)
-
- ! Write dvdb file (we only support fortran binary format)
- if (open_file(dvdb_path, msg, newunit=ount, form="unformatted", action="write", status="unknown") /= 0) then
-   MSG_ERROR(msg)
- end if
- write(ount, err=10, iomsg=msg) dvdb_last_version
- write(ount, err=10, iomsg=msg) nperts
-
- do ii=1,nperts
+   write(std_out,"(a,i0,2a)")"- Merging file [",ii,"]: ",trim(v1files(ii))
    jj = ii
    hdr1 => hdr1_list(jj)
    call hdr_fort_write(hdr1, ount, fform_pot, ierr)
@@ -2800,6 +2800,14 @@ subroutine dvdb_merge_files(nfiles, v1files, dvdb_path, prtvol)
       if (dvdb_last_version > 1) write(ount, err=10, iomsg=msg) rhog1_g0
 #endif
    end if
+   
+   if (.not. endswith(v1files(ii), ".nc")) then
+     close(units(ii))
+   else
+#ifdef HAVE_NETCDF
+     NCF_CHECK(nf90_close(units(ii)))
+#endif
+   end if
 
    ABI_FREE(v1)
  end do ! nperts
@@ -2808,13 +2816,6 @@ subroutine dvdb_merge_files(nfiles, v1files, dvdb_path, prtvol)
 
  do ii=1,size(hdr1_list)
    call hdr_free(hdr1_list(ii))
-   if (.not. endswith(v1files(ii), ".nc")) then
-     close(units(ii))
-   else
-#ifdef HAVE_NETCDF
-     NCF_CHECK(nf90_close(units(ii)))
-#endif
-   end if
  end do
  ABI_DT_FREE(hdr1_list)
 
