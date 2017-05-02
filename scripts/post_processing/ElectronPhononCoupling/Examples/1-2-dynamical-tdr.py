b18ff457a7eff7de14d054dcec65e3875ddb4be2
Gabriel Antonius <gabriel.antonius@gmail.com>
2017-05-01 20:56:03 -0700

    Refactored ElectronPhononCoupling.

--- scripts/post_processing/ElectronPhononCoupling/Examples/1-2-dynamical-tdr.py
+++ scripts/post_processing/ElectronPhononCoupling/Examples/1-2-dynamical-tdr.py
@@ -3,13 +3,13 @@ Compute the temperature dependent renormalization (TDR)
 using the dynamical AHC theory (ieig2rf=5).
 """
 
-from ElectronPhononCoupling import compute_epc
+from ElectronPhononCoupling import compute
 
 
 # Lists of files used
 # ===================
 
-DDB_fnames = """
+ddb_fnames = """
 Calculations/01-LiF-dynamical/odat_calc_DS5_DDB.nc
 Calculations/01-LiF-dynamical/odat_calc_DS9_DDB.nc
 Calculations/01-LiF-dynamical/odat_calc_DS13_DDB.nc
@@ -21,46 +21,44 @@ Calculations/01-LiF-dynamical/odat_calc_DS10_EIG.nc
 Calculations/01-LiF-dynamical/odat_calc_DS14_EIG.nc
 """.split()
 
-EIGR2D_fnames = """
+eigr2d_fnames = """
 Calculations/01-LiF-dynamical/odat_calc_DS7_EIGR2D.nc
 Calculations/01-LiF-dynamical/odat_calc_DS11_EIGR2D.nc
 Calculations/01-LiF-dynamical/odat_calc_DS15_EIGR2D.nc
 """.split()
 
-GKK_fnames = """
+gkk_fnames = """
 Calculations/01-LiF-dynamical/odat_calc_DS7_GKK.nc
 Calculations/01-LiF-dynamical/odat_calc_DS11_GKK.nc
 Calculations/01-LiF-dynamical/odat_calc_DS15_GKK.nc
 """.split()
 
-eig0_fname = 'Calculations/01-LiF-dynamical/odat_calc_DS3_EIG.nc'
+eigk_fname = 'Calculations/01-LiF-dynamical/odat_calc_DS3_EIG.nc'
 
 
 # Computation of the TDR
 # ======================
 
-epc = compute_epc(
-    calc_type = 2,          # Perform dynamical AHC calculation
-    temperature = True,     # Do compute temperature dependence
-    lifetime = False,       # Do not compute lifetime
+epc = compute(
+    renormalization=True,   # Compute the eigenvalues renormalization
+    temperature = True,     # Compute at several temperatures
+    broadening = False,     # Do not compute broadening
 
     write = True,           # Do write the results
-    output = 'Out/1-2',     # Rootname for the output
+    rootname = 'Out/1-2',   # Rootname for the output
     
-    smearing_eV = 0.01,         # Imaginary parameter for broadening.
-    temp_range = [0, 600, 50],  # Temperature range (min, max, step)
+    smearing_eV = 0.01,           # Imaginary parameter for broadening.
+    temp_range = [0, 1000, 250],  # Temperature range (min, max, step)
 
     nqpt = 3,                   # Number of q-points (2x2x2 qpt grid)
     wtq = [0.125, 0.5, 0.375],  # Weights of the q-points.
                                 # These can be obtained by running Abinit
                                 # with the corresponding k-point grid.
     
-    eig0_fname = eig0_fname,        # All the files needed for
+    eigk_fname = eigk_fname,        # All the files needed for
     eigq_fnames = eigq_fnames,      # this calculation.
-    DDB_fnames = DDB_fnames,        #
-    EIGR2D_fnames = EIGR2D_fnames,  #
-    GKK_fnames = GKK_fnames,        # Note that instead of GKK.nc files,
-                                    # one can also use the FAN.nc files
-                                    # with the FAN_fnames keyword argument.
+    ddb_fnames = ddb_fnames,        #
+    eigr2d_fnames = eigr2d_fnames,  #
+    gkk_fnames = gkk_fnames,        #
     )
 
