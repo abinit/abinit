64eacea20a4ad42d4f4ce32562d05a899eb67a3c
Matthieu Verstraete <matthieu.jean.verstraete@gmail.com>
2017-05-03 21:32:00 +0200

    thanks Matteo

--- src/72_response/m_ddk.F90
+++ src/72_response/m_ddk.F90
@@ -488,9 +488,8 @@ subroutine ddk_free(ddk)
  ! integer arrays
 
  ! real arrays
- if (allocated(ddk%velocity)) then
-   ABI_FREE(ddk%velocity)
- end if
+ if (allocated(ddk%velocity)) ABI_FREE(ddk%velocity)
+ if (allocated(ddk%velocity_fsavg)) ABI_FREE(ddk%velocity_fsavg)
 
  ! types
  call crystal_free(ddk%cryst)
