f5cbc86ba0a51f92652d2f4c5389b7b0874edc4c
JOLLET Francois 122155 <jolletf@u-chaumet.bruyeres.cea.fr>
2017-06-26 16:21:29 +0200

    modification of add_topic.py

--- doc/playground/add_topic.py
+++ doc/playground/add_topic.py
@@ -18,22 +18,9 @@ for oline in Input:
     for oline1 in Input1:
       if oline1 == oline:
         oline2=Input1.next()
-        if oline2[0:15] in "    topic_class":
-          outfile.write(oline2)
-          oline3=Input1.next()
-          outfile.write(oline3)
-          outfile.write(oline)
-          break
-        if oline2[0:10] in "    topics":
-          outfile.write(oline2)
-          oline3=Input1.next()
-          outfile.write(oline3)
-          oline3=Input1.next()
-          outfile.write(oline3)
-          outfile.write(oline)
-          break
-
-
+        outfile.write(oline2)
+        outfile.write(oline)
+        break
 outfile.close()
 
 
