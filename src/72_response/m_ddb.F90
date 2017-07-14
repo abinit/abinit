8c4bca41e4f1ea97289dfa8e3b0b412e86d745f9
Gabriel Antonius <gabriel.antonius@gmail.com>
2017-07-13 17:44:00 -0700

    Temporarily reverted functions ddb_from_file and rdddb9 for debugging.

--- src/72_response/m_ddb.F90
+++ src/72_response/m_ddb.F90
@@ -32,7 +32,7 @@ MODULE m_ddb
  use m_errors
  use m_xmpi
 
- use m_ddb_hdr,        only : ddb_hdr_type, ddb_hdr_open_read, ddb_hdr_free
+ use m_ddb_hdr
  use m_fstrings,       only : sjoin, itoa, ktoa
  use m_numeric_tools,  only : mkherm
  use m_io_tools,       only : get_unit
@@ -41,6 +41,9 @@ MODULE m_ddb
  use m_crystal,        only : crystal_t, crystal_init
  use m_dynmat,         only : cart29, d2sym3, cart39, d3sym, chneu9, asria_calc, asria_corr, asrprs, &
 &                             dfpt_phfrq, sytens
+! DEBUG
+ use m_pawtab,         only : pawtab_type,pawtab_nullify,pawtab_free
+! END DEBUG
 
  implicit none
 
@@ -1180,6 +1183,9 @@ subroutine rdddb9(acell,atifc,amu,ddb,&
  real(dp),allocatable :: d2cart(:,:,:,:,:),d3cart(:,:,:,:,:,:,:),ekb(:,:)
  real(dp),allocatable :: kpt(:,:),occ(:),spinat(:,:),tmpval(:,:,:,:,:,:,:)
  real(dp),allocatable :: wtk(:)
+! DEBUG
+ type(pawtab_type),allocatable :: pawtab(:)
+! END DEBUG
 
 ! *********************************************************************
 
@@ -1189,54 +1195,61 @@ subroutine rdddb9(acell,atifc,amu,ddb,&
  vrsddb=DDB_VERSION
 
 !The checking of pseudopotentials is not done presently so that dimensions are fake
-! ABI_MALLOC(ekb,(dimekb,ntypat))
-! ABI_MALLOC(indlmn,(6,lmnmax,ntypat))
-! ABI_MALLOC(pspso,(ntypat))
-! ABI_DATATYPE_ALLOCATE(pawtab,(ntypat*usepaw))
-! call pawtab_nullify(pawtab)
-!
-! ABI_MALLOC(kpt,(3,nkpt))
-! ABI_MALLOC(nband,(nkpt))
-! ABI_MALLOC(occ,(nkpt*mband*msppol))
-! ABI_MALLOC(spinat,(3,natom))
-! ABI_MALLOC(wtk,(nkpt))
+ ABI_MALLOC(ekb,(dimekb,ntypat))
+ ABI_MALLOC(indlmn,(6,lmnmax,ntypat))
+ ABI_MALLOC(pspso,(ntypat))
+ ABI_DATATYPE_ALLOCATE(pawtab,(ntypat*usepaw))
+ call pawtab_nullify(pawtab)
+
+ ABI_MALLOC(kpt,(3,nkpt))
+ ABI_MALLOC(nband,(nkpt))
+ ABI_MALLOC(occ,(nkpt*mband*msppol))
+ ABI_MALLOC(spinat,(3,natom))
+ ABI_MALLOC(wtk,(nkpt))
 
 !Open the input derivative database file and read the preliminary information
 !Note that in this call, mkpt has been replaced by nkpt, mtypat by ntypat, and matom by natom.
-! nunit=ddbun
+ nunit=ddbun
 
 ! To avoid aliasing
-! matom = natom
-! mkpt = nkpt
-! mtypat = ntypat
+ matom = natom
+ mkpt = nkpt
+ mtypat = ntypat
 
-! call ioddb8_in(filnam,matom,mband,mkpt,msym,mtypat,nunit,vrsddb,&
-!& acell,amu,dilatmx,ecut,ecutsm,intxc,iscf,ixc,kpt,kptnrm,&
-!& natom,nband,ngfft,nkpt,nspden,nspinor,nsppol,nsym,ntypat,occ,occopt,&
-!& pawecutdg,rprim,dfpt_sciss,spinat,symafm,symrel,tnons,tolwfr,tphysel,tsmear,&
-!& typat,usepaw,wtk,xred,zion,znucl)
+ call ioddb8_in(filnam,matom,mband,mkpt,msym,mtypat,nunit,vrsddb,&
+& acell,amu,dilatmx,ecut,ecutsm,intxc,iscf,ixc,kpt,kptnrm,&
+& natom,nband,ngfft,nkpt,nspden,nspinor,nsppol,nsym,ntypat,occ,occopt,&
+& pawecutdg,rprim,dfpt_sciss,spinat,symafm,symrel,tnons,tolwfr,tphysel,tsmear,&
+& typat,usepaw,wtk,xred,zion,znucl)
+!Read the psp information of the input DDB
+ useylm=usepaw;choice=1
+ call psddb8 (choice,dimekb,ekb,fullinit,indlmn,lmnmax,&
+& ddb%nblok,ntypat,nunit,pawtab,pspso,usepaw,useylm,vrsddb)
 
- call ddb_hdr_open_read(ddb_hdr, filnam, ddbun, vrsddb, &
-&                       msym=msym, mband=mband)
 
- !nkpt = ddb_hdr%nkpt
- !ntypat = ddb_hdr%ntypat
- nsym = ddb_hdr%nsym
- acell = ddb_hdr%acell
- rprim = ddb_hdr%rprim
 
- amu(:) = ddb_hdr%amu(1:ntypat)
- typat(:) = ddb_hdr%typat(1:natom)
- zion(:) = ddb_hdr%zion(1:ntypat)
- znucl(:) = ddb_hdr%znucl(1:ntypat)
 
- symafm(:) = ddb_hdr%symafm(:)
- symrel(:,:,:) = ddb_hdr%symrel(:,:,:)
- tnons(:,:) = ddb_hdr%tnons(:,:)
-
- xred(:,:) = ddb_hdr%xred(:,:)
-
- call ddb_hdr_free(ddb_hdr)
+! call ddb_hdr_open_read(ddb_hdr, filnam, ddbun, vrsddb, &
+!&                       msym=msym, mband=mband)
+!
+! !nkpt = ddb_hdr%nkpt
+! !ntypat = ddb_hdr%ntypat
+! nsym = ddb_hdr%nsym
+! acell = ddb_hdr%acell
+! rprim = ddb_hdr%rprim
+!
+! amu(:) = ddb_hdr%amu(1:ntypat)
+! typat(:) = ddb_hdr%typat(1:natom)
+! zion(:) = ddb_hdr%zion(1:ntypat)
+! znucl(:) = ddb_hdr%znucl(1:ntypat)
+!
+! symafm(:) = ddb_hdr%symafm(:)
+! symrel(:,:,:) = ddb_hdr%symrel(:,:,:)
+! tnons(:,:) = ddb_hdr%tnons(:,:)
+!
+! xred(:,:) = ddb_hdr%xred(:,:)
+!
+! call ddb_hdr_free(ddb_hdr)
 
 !Compute different matrices in real and reciprocal space, also
 !checks whether ucvol is positive.
@@ -1259,11 +1272,6 @@ subroutine rdddb9(acell,atifc,amu,ddb,&
 !on which the referenced one is sent and also the translation bringing
 !back this atom to the referenced unit cell
  call symatm(indsym,natom,nsym,symrec,tnons,tolsym8,typat,xred)
-
-!!Read the psp information of the input DDB
-! useylm=usepaw;choice=1
-! call psddb8 (choice,dimekb,ekb,fullinit,indlmn,lmnmax,&
-!& ddb%nblok,ntypat,nunit,pawtab,pspso,usepaw,useylm,vrsddb)
 
 !Check the correctness of some input parameters, and perform small treatment if needed.
  call chkin9(atifc,natifc,natom)
@@ -1373,17 +1381,17 @@ subroutine rdddb9(acell,atifc,amu,ddb,&
  call wrtout(std_out,message,'COLL')
  call wrtout(iout,message,'COLL')
 
-! ABI_FREE(ekb)
-! ABI_FREE(indlmn)
-! ABI_FREE(kpt)
-! ABI_FREE(nband)
-! ABI_FREE(occ)
-! ABI_FREE(pspso)
-! ABI_FREE(spinat)
-! ABI_FREE(wtk)
-!
-! call pawtab_free(pawtab)
-! ABI_DATATYPE_DEALLOCATE(pawtab)
+ ABI_FREE(ekb)
+ ABI_FREE(indlmn)
+ ABI_FREE(kpt)
+ ABI_FREE(nband)
+ ABI_FREE(occ)
+ ABI_FREE(pspso)
+ ABI_FREE(spinat)
+ ABI_FREE(wtk)
+
+ call pawtab_free(pawtab)
+ ABI_DATATYPE_DEALLOCATE(pawtab)
 
  DBG_EXIT("COLL")
 
@@ -1717,22 +1725,22 @@ subroutine ddb_from_file(ddb,filename,brav,natom,natifc,atifc,crystal,comm,prtvo
  DBG_ENTER("COLL")
 
 ! Must read natom from the DDB before being able to allocate some arrays needed for invars9
-!call ddb_getdims(dimekb,filename,lmnmax,mband,mtyp,msym,ddb_natom,nblok,nkpt,ntypat,get_unit(),usepaw,DDB_VERSION,comm)
- call ddb_hdr_open_read(ddb_hdr,filename,ddbun,DDB_VERSION, &
-&                       dimonly=1)
-
- nblok = ddb_hdr%nblok
- mtyp = ddb_hdr%mblktyp
- msym = ddb_hdr%msym
- ddb_natom = ddb_hdr%natom
- ntypat = ddb_hdr%ntypat
- mband = ddb_hdr%mband
- nkpt = ddb_hdr%nkpt
- usepaw = ddb_hdr%usepaw
- dimekb = ddb_hdr%psps%dimekb
- lmnmax = ddb_hdr%psps%lmnmax
-
- call ddb_hdr_free(ddb_hdr)
+ call ddb_getdims(dimekb,filename,lmnmax,mband,mtyp,msym,ddb_natom,nblok,nkpt,ntypat,get_unit(),usepaw,DDB_VERSION,comm)
+! call ddb_hdr_open_read(ddb_hdr,filename,ddbun,DDB_VERSION, &
+!&                       dimonly=1)
+!
+! nblok = ddb_hdr%nblok
+! mtyp = ddb_hdr%mblktyp
+! msym = ddb_hdr%msym
+! ddb_natom = ddb_hdr%natom
+! ntypat = ddb_hdr%ntypat
+! mband = ddb_hdr%mband
+! nkpt = ddb_hdr%nkpt
+! usepaw = ddb_hdr%usepaw
+! dimekb = ddb_hdr%psps%dimekb
+! lmnmax = ddb_hdr%psps%lmnmax
+!
+! call ddb_hdr_free(ddb_hdr)
 
  if (ddb_natom /= natom) then
    MSG_ERROR(sjoin("input natom:",itoa(natom),"does not agree with DDB value:",itoa(natom)))
