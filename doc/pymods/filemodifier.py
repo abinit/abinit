213c61b20c1c082ee427e589d4fa3952981ec9ff
gonze <xavier.gonze@uclouvain.be>
2017-07-27 21:08:20 +0200

    Small script, will be used as template to modify a bunch of files

---
+++ doc/pymods/filemodifier.py
@@ -0,0 +1,61 @@
+#! /usr/bin/env python 
+"""
+   This script is intended as a template for quick modification of large files, e.g. 
+   inserting one more field in abinit_vars.yml
+   Modify at your will !
+
+   Feed it with a list of files (at least one file is needed)
+   filemodifier file1 file2 file3 ...
+"""
+
+from __future__ import print_function
+
+import sys
+import os
+import yaml
+import re
+import string
+import argparse
+
+from variables import *
+from lib_to_assemble_html import *
+
+################################################################################
+
+# Generic checks
+
+################################################################################
+
+filelist = sys.argv[1:]
+if filelist != [] :
+  if cmdline_params[0] == "-h" or cmdline_params[0] == "--help" :
+    print(__doc__)
+    sys.exit()
+else:
+  print(__doc__)
+  sys.exit()
+
+################################################################################
+
+for path in filelist:
+  print(" Working on file '%s'."%path)
+  with open(path,'r') as f_old:
+    file_old=f_old.readlines()
+    f_new=open(path+"_new","w")
+
+################################################################################
+
+    #The file transformations can be made in this section
+    for line in file_old:
+      f_new.print(line)
+      if "excludes" in line:
+        f_new.print("    executables: abinit")
+
+################################################################################
+
+  #Finishes by moving the new file at the new place
+  print(" Changes done")
+  os.system("mv %s %s_old"%(path,path))
+  os.system("mv %s_new %s"%(path,path))
+  os.system("rm %s_old"%(path))
+  print(" New file %s written"%path)
