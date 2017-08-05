7166623012bc5d982d741456e32d8380fc833fc7
gonze <xavier.gonze@uclouvain.be>
2017-08-05 16:07:59 +0200

    Shorten more links

--- doc/generate_doc.py
+++ doc/generate_doc.py
@@ -929,7 +929,8 @@ for list_infos in list_infos_dir:
             abivarname=var.abivarname
             #for varset in ["varbas","varfil","varff","vargs","varrf","varint","varpar","varbse","vardev","vardmft","vareph","vargeo","vargw","varpaw","varrlx","varvdw","varw90","varoptic","varaim","varanaddb"]:
             for varset in ["bas","fil","ff","gs","rf","int","par","bse","dev","dmft","eph","geo","gw","paw","rlx","vdw","w90","optic","aim","anaddb"]:
-              string_old='<a href="../../input_variables/generated_files/var%s.html#%s" onclick="return (false);">%s</a>'%(varset,abivarname,abivarname)
+              #string_old='<a href="../../input_variables/generated_files/var%s.html#%s" onclick="return (false);">%s</a>'%(varset,abivarname,abivarname)
+              string_old='<a href="../../input_variables/generated_files/var%s.html">%s</a>'%(varset,abivarname)
               string_new="[["+abivarname+"]]"
               file_str=file_str.replace(string_old,string_new)
 
