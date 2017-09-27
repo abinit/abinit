3f05d30f65743fa7b5d01dfe6e50e1c0d8b98bb9
Xavier Gonze gonzex@u-tsumura.bruyeres.cea.fr <xavier.gonze@uclouvain.be>
2017-09-27 14:23:13 +0200

    Correct the args of hartre in respfn

--- src/95_drive/respfn.F90
+++ src/95_drive/respfn.F90
@@ -847,8 +847,8 @@ subroutine respfn(codvsn,cpui,dtfil,dtset,etotal,iexit,&
  end if
 
 !Set up hartree and xc potential. Compute kxc here.
-
- call hartre(1,gsqcut,psps%usepaw,mpi_enreg,nfftf,ngfftf,dtset%paral_kgb,rhog,rprimd,vhartr)
+ qphon(:)=zero
+ call hartre(1,gsqcut,psps%usepaw,mpi_enreg,nfftf,ngfftf,dtset%paral_kgb,qphon,rhog,rprimd,vhartr)
 
  option=2 ; nk3xc=1
  nkxc=2*min(dtset%nspden,2)-1;if(dtset%xclevel==2)nkxc=12*min(dtset%nspden,2)-5
