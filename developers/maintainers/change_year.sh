0ea551caba1ad8c37c1661b2dfd11b44874e2dc3
Xavier Gonze gonzex@u-tsumura.bruyeres.cea.fr <xavier.gonze@uclouvain.be>
2018-01-25 09:21:40 +0100

    Update script to 2018

--- developers/maintainers/change_year.sh
+++ developers/maintainers/change_year.sh
@@ -6,7 +6,7 @@
 # First you should reapply the present script without changing it, because it has been seen that some people bring
 # routines with erroneous date during a few months after the beginning of a new one ... !
 #
-# Then one should update the present script to put the current year (at present, valid for going from 2015 to 2016 !) !
+# Then one should update the present script to put the current year (at present, valid for going from 2017 to 2018 !) !
 #
 # Then should be called from the top directory  (here a list of generic filenames, ordered on the basis of the alphanumeric string)
 # developers/maintainers/change_year.sh *.ac */*.ac */*/*.am */*/*.bat */*/*/*.bat */*/*.c */*/*/*.c */*/*.cf */*/*.cnf */*/*.com */*/*.conf */*/*.cu */*/*.csh 
@@ -32,7 +32,7 @@
 # grep 'past_year ABINIT' * */* */*/* */*/*/* */*/*/*/*
 # and treat by hand the remaining files ...
 #
-#XG 100118 Still other problems with copyrights might be detected by using the following command (replace 2016 by the present year !):
+#XG 100118 Still other problems with copyrights might be detected by using the following command (replace 2018 by the present year !):
 # grep -i opyright * */* */*/* */*/*/* */*/*/*/* | grep -v 2017 | grep -v '!! COPYRIGHT' | grep -v 'Oldenburg' | grep -v 'Stefan Goedecker' | grep -v 'doc/rel' | grep -v 'Remove' | grep -v 'tests/' | grep -v 'EXC group' | grep -v 'PWSCF group' | grep -v 'Makefile' | grep -v 'abinit.d' | grep -v 'fallbacks' | grep -v 'doc/features/features' | grep -v 'doc/install_notes/install' | grep -v 'COPYING' | grep -v 'gui'
 
 for file in "$@"
@@ -40,33 +40,34 @@ do
  echo "working on $file"
  rm -f tmp.yr*  
  sed -e 's&Copyright (c)&Copyright (C)&' $file > tmp.yrup
- sed -e 's&(C) 1987-2016 ABINIT&(C) 1987-2017 ABINIT&' tmp.yrup > tmp.yr87
- sed -e 's&(C) 1991-2016 ABINIT&(C) 1991-2017 ABINIT&' tmp.yr87 > tmp.yr91
- sed -e 's&(C) 1992-2016 ABINIT&(C) 1992-2017 ABINIT&' tmp.yr91 > tmp.yr92
- sed -e 's&(C) 1993-2016 ABINIT&(C) 1993-2017 ABINIT&' tmp.yr92 > tmp.yr93
- sed -e 's&(C) 1996-2016 ABINIT&(C) 1996-2017 ABINIT&' tmp.yr93 > tmp.yr96
- sed -e 's&(C) 1997-2016 ABINIT&(C) 1997-2017 ABINIT&' tmp.yr96 > tmp.yr97
- sed -e 's&(C) 1998-2016 ABINIT&(C) 1998-2017 ABINIT&' tmp.yr97 > tmp.yr98
- sed -e 's&(C) 1999-2016 ABINIT&(C) 1999-2017 ABINIT&' tmp.yr98 > tmp.yr99
- sed -e 's&(C) 2000-2016 ABINIT&(C) 2000-2017 ABINIT&' tmp.yr99 > tmp.yr00
- sed -e 's&(C) 2001-2016 ABINIT&(C) 2001-2017 ABINIT&' tmp.yr00 > tmp.yr01
- sed -e 's&(C) 2002-2016 ABINIT&(C) 2002-2017 ABINIT&' tmp.yr01 > tmp.yr02
- sed -e 's&(C) 2003-2016 ABINIT&(C) 2003-2017 ABINIT&' tmp.yr02 > tmp.yr03
- sed -e 's&(C) 2004-2016 ABINIT&(C) 2004-2017 ABINIT&' tmp.yr03 > tmp.yr04
- sed -e 's&(C) 2005-2016 ABINIT&(C) 2005-2017 ABINIT&' tmp.yr04 > tmp.yr05
- sed -e 's&(C) 2006-2016 ABINIT&(C) 2006-2017 ABINIT&' tmp.yr05 > tmp.yr06
- sed -e 's&(C) 2007-2016 ABINIT&(C) 2007-2017 ABINIT&' tmp.yr06 > tmp.yr07
- sed -e 's&(C) 2008-2016 ABINIT&(C) 2008-2017 ABINIT&' tmp.yr07 > tmp.yr08
- sed -e 's&(C) 2009-2016 ABINIT&(C) 2009-2017 ABINIT&' tmp.yr08 > tmp.yr09
- sed -e 's&(C) 2010-2016 ABINIT&(C) 2010-2017 ABINIT&' tmp.yr09 > tmp.yr10
- sed -e 's&(C) 2011-2016 ABINIT&(C) 2011-2017 ABINIT&' tmp.yr10 > tmp.yr11
- sed -e 's&(C) 2012-2016 ABINIT&(C) 2012-2017 ABINIT&' tmp.yr11 > tmp.yr12
- sed -e 's&(C) 2013-2016 ABINIT&(C) 2013-2017 ABINIT&' tmp.yr12 > tmp.yr13
- sed -e 's&(C) 2014-2016 ABINIT&(C) 2014-2017 ABINIT&' tmp.yr13 > tmp.yr14
- sed -e 's&(C) 2015-2016 ABINIT&(C) 2015-2017 ABINIT&' tmp.yr14 > tmp.yr15
+ sed -e 's&(C) 1987-2017 ABINIT&(C) 1987-2018 ABINIT&' tmp.yrup > tmp.yr87
+ sed -e 's&(C) 1991-2017 ABINIT&(C) 1991-2018 ABINIT&' tmp.yr87 > tmp.yr91
+ sed -e 's&(C) 1992-2017 ABINIT&(C) 1992-2018 ABINIT&' tmp.yr91 > tmp.yr92
+ sed -e 's&(C) 1993-2017 ABINIT&(C) 1993-2018 ABINIT&' tmp.yr92 > tmp.yr93
+ sed -e 's&(C) 1996-2017 ABINIT&(C) 1996-2018 ABINIT&' tmp.yr93 > tmp.yr96
+ sed -e 's&(C) 1997-2017 ABINIT&(C) 1997-2018 ABINIT&' tmp.yr96 > tmp.yr97
+ sed -e 's&(C) 1998-2017 ABINIT&(C) 1998-2018 ABINIT&' tmp.yr97 > tmp.yr98
+ sed -e 's&(C) 1999-2017 ABINIT&(C) 1999-2018 ABINIT&' tmp.yr98 > tmp.yr99
+ sed -e 's&(C) 2000-2017 ABINIT&(C) 2000-2018 ABINIT&' tmp.yr99 > tmp.yr00
+ sed -e 's&(C) 2001-2017 ABINIT&(C) 2001-2018 ABINIT&' tmp.yr00 > tmp.yr01
+ sed -e 's&(C) 2002-2017 ABINIT&(C) 2002-2018 ABINIT&' tmp.yr01 > tmp.yr02
+ sed -e 's&(C) 2003-2017 ABINIT&(C) 2003-2018 ABINIT&' tmp.yr02 > tmp.yr03
+ sed -e 's&(C) 2004-2017 ABINIT&(C) 2004-2018 ABINIT&' tmp.yr03 > tmp.yr04
+ sed -e 's&(C) 2005-2017 ABINIT&(C) 2005-2018 ABINIT&' tmp.yr04 > tmp.yr05
+ sed -e 's&(C) 2006-2017 ABINIT&(C) 2006-2018 ABINIT&' tmp.yr05 > tmp.yr06
+ sed -e 's&(C) 2007-2017 ABINIT&(C) 2007-2018 ABINIT&' tmp.yr06 > tmp.yr07
+ sed -e 's&(C) 2008-2017 ABINIT&(C) 2008-2018 ABINIT&' tmp.yr07 > tmp.yr08
+ sed -e 's&(C) 2009-2017 ABINIT&(C) 2009-2018 ABINIT&' tmp.yr08 > tmp.yr09
+ sed -e 's&(C) 2010-2017 ABINIT&(C) 2010-2018 ABINIT&' tmp.yr09 > tmp.yr10
+ sed -e 's&(C) 2011-2017 ABINIT&(C) 2011-2018 ABINIT&' tmp.yr10 > tmp.yr11
+ sed -e 's&(C) 2012-2017 ABINIT&(C) 2012-2018 ABINIT&' tmp.yr11 > tmp.yr12
+ sed -e 's&(C) 2013-2017 ABINIT&(C) 2013-2018 ABINIT&' tmp.yr12 > tmp.yr13
+ sed -e 's&(C) 2014-2017 ABINIT&(C) 2014-2018 ABINIT&' tmp.yr13 > tmp.yr14
+ sed -e 's&(C) 2015-2017 ABINIT&(C) 2015-2018 ABINIT&' tmp.yr14 > tmp.yr15
+ sed -e 's&(C) 2016-2017 ABINIT&(C) 2016-2018 ABINIT&' tmp.yr15 > tmp.yr16
 #The next lines are both needed, as some developers decide to use one, and some the other ...
- sed -e 's&(C) 2016-2016 ABINIT&(C) 2016-2017 ABINIT&' tmp.yr15 > tmp.yr16
- sed -e 's&(C) 2016 ABINIT&(C) 2016-2017 ABINIT&' tmp.yr16 > tmp.yr
+ sed -e 's&(C) 2017-2017 ABINIT&(C) 2017-2018 ABINIT&' tmp.yr16 > tmp.yr17
+ sed -e 's&(C) 2017 ABINIT&(C) 2017-2018 ABINIT&' tmp.yr17 > tmp.yr
  echo "changes done "
  # put the modified file at the correct place
  mv tmp.yr $file
