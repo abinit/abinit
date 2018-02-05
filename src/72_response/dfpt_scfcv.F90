75c75512790a4458b0dabb2b4e7fb25f87a4bcaf
gonze <xavier.gonze@uclouvain.be>
2018-02-05 06:47:50 +0100

    Fix merge

--- src/72_response/dfpt_scfcv.F90
+++ src/72_response/dfpt_scfcv.F90
@@ -1029,17 +1029,14 @@ subroutine dfpt_scfcv(atindx,blkflg,cg,cgq,cg1,cg1_active,cplex,cprj,cprjq,cpus,
      optene = 0 ! use direct scheme
      call dfpt_etot(dtset%berryopt,deltae,eberry,edocc,eeig0,eew,efrhar,efrkin,&
 &     efrloc,efrnl,efrx1,efrx2,ehart1,ek0,ek1,eii,elast,eloc0,elpsp1,&
-<<<<<<< HEAD
-&     enl0,enl1,epaw1,etotal,evar,evdw,exc1,elmag1,ipert,dtset%natom,optene)
+!&     enl0,enl1,epaw1,etotal,evar,evdw,exc1,elmag1,ipert,dtset%natom,optene)
 !    !debug: compute the d2E/d-qd+q energy, should be equal to the one from previous line
 !    if(.not.kramers_deg) then
 !      call dfpt_etot(dtset%berryopt,deltae_mq,eberry_mq,edocc_mq,eeig0_mq,eew,efrhar,efrkin,&
 !&       efrloc,efrnl,efrx1,efrx2,ehart1_mq,ek0_mq,ek1_mq,eii,elast_mq,eloc0_mq,elpsp1_mq,&
 !&       enl0_mq,enl1_mq,epaw1_mq,etotal_mq,evar_mq,evdw,exc1_mq,elmag1_mq,ipert,dtset%natom,optene)
 !     end if
-=======
 &     enl0,enl1,epaw1,etotal,evar,evdw,exc1,ipert,dtset%natom,optene)
->>>>>>> hotfix-8.6
 
      call timab(152,1,tsec)
      choice=2
