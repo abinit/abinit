4d5a8dcc1550005e34fdce8f528fc111ef3d10e7
Matteo Giantomassi <matteo.giantomassi@uclouvain.be>
2018-03-31 13:02:14 +0200

    Begin to group procedures in 64_psp

--- src/64_psp/psp2in.F90
+++ src/64_psp/psp2in.F90
@@ -243,11 +243,155 @@ subroutine psp2in(dtset,ekb,epsatm,ffspl,indlmn,ipsp,lmax,nproj,psps,vlspl,dvlsp
    call psp2nl(ekb,ffspl,h1p,h1s,h2s,psps%lnmax,psps%mqgrid_ff,psps%qgrid_ff,rrp,rrs)
  end if
 
- return 
+ return
 
  ! Handle IO error
  10 continue
  MSG_ERROR(errmsg)
 
 end subroutine psp2in
+!!***
+
+!!****f* ABINIT/psp2nl
+!! NAME
+!! psp2nl
+!!
+!! FUNCTION
+!! Goedecker-Teter-Hutter nonlocal pseudopotential (from preprint of 1996).
+!! Uses Gaussians for fully nonlocal form, analytic expressions.
+!!
+!! INPUTS
+!!  h1p=factor defining strength of 1st projector for l=1 channel
+!!  h1s=factor defining strength of 1st projector for l=0 channel
+!!  h2s=factor defining strength of 2nd projector for l=0 channel
+!!  lnmax=max. number of (l,n) components over all type of psps
+!!  mqgrid=number of grid points for qgrid
+!!  qgrid(mqgrid)=array of |G| values
+!!  rrp=core radius for p channel (bohr)
+!!  rrs=core radius for s channel (bohr)
+!!
+!! OUTPUT
+!!  ekb(lnmax)=Kleinman-Bylander energy
+!!  ffspl(mqgrid,2,lnmax)=Kleinman-Bylander form factor f_l(q) and
+!!   second derivative from spline fit for each angular momentum
+!!   and each projector
+!!
+!! PARENTS
+!!      psp2in
+!!
+!! CHILDREN
+!!      spline
+!!
+!! SOURCE
+
+subroutine psp2nl(ekb,ffspl,h1p,h1s,h2s,lnmax,mqgrid,qgrid,rrp,rrs)
+
+ use defs_basis
+ use m_splines
+ use m_profiling_abi
+
+!This section has been created automatically by the script Abilint (TD).
+!Do not modify the following lines by hand.
+#undef ABI_FUNC
+#define ABI_FUNC 'psp2nl'
+!End of the abilint section
+
+ implicit none
+
+!Arguments ------------------------------------
+!scalars
+ integer,intent(in) :: lnmax,mqgrid
+ real(dp),intent(in) :: h1p,h1s,h2s,rrp,rrs
+!arrays
+ real(dp),intent(in) :: qgrid(mqgrid)
+ real(dp),intent(inout) :: ekb(lnmax),ffspl(mqgrid,2,lnmax) !vz_i
+
+!Local variables-------------------------------
+!scalars
+ integer :: iln,iqgrid
+ real(dp) :: qmax,yp1,ypn
+!arrays
+ real(dp),allocatable :: work(:)
+
+! *************************************************************************
+
+ ABI_ALLOCATE(work,(mqgrid))
+
+!Kleinman-Bylander energies ekb were set to zero in calling program
+
+!Compute KB energies
+ iln=0
+ if (abs(h1s)>1.d-12) then
+   iln=iln+1
+   ekb(iln)=h1s*32.d0*rrs**3*(pi**(2.5d0)/(4.d0*pi)**2)
+ end if
+ if (abs(h2s)>1.d-12) then
+   iln=iln+1
+   ekb(iln) =h2s*(128.d0/15.d0)*rrs**3*(pi**(2.5d0)/(4.d0*pi)**2)
+ end if
+ if (abs(h1p)>1.d-12) then
+   iln=iln+1
+   ekb(iln)=h1p*(64.d0/3.d0)*rrp**5*(pi**(2.5d0)/(4.d0*pi)**2)
+ end if
+
+!Compute KB form factor
+ iln=0
+
+!l=0 first projector
+ if (abs(h1s)>1.d-12) then
+   iln=iln+1
+   do iqgrid=1,mqgrid
+     ffspl(iqgrid,1,iln)=exp(-0.5d0*(two_pi*qgrid(iqgrid)*rrs)**2)
+   end do
+!  Compute yp1,ypn=derivatives of f(q) at q=0, q=qgrid(mqgrid)
+   yp1=0.d0
+   qmax=qgrid(mqgrid)
+   ypn=-4.d0*pi**2*qmax*rrs**2*exp(-0.5d0*(two_pi*qmax*rrs)**2)
+!  Fit spline to get second derivatives by spline fit
+   call spline(qgrid,ffspl(:,1,iln),mqgrid,yp1,ypn,ffspl(:,2,iln))
+!  else
+!  or else put first projector nonlocal correction at l=0 to 0
+!  ffspl(:,:,iln)=0.0d0
+ end if
+
+!l=0 second projector
+ if (abs(h2s)>1.d-12) then
+   iln=iln+1
+   do iqgrid=1,mqgrid
+     ffspl(iqgrid,1,iln)=exp(-0.5d0*(two_pi*qgrid(iqgrid)*rrs)**2) * &
+&     (3.d0-(two_pi*qgrid(iqgrid)*rrs)**2)
+   end do
+!  Compute yp1,ypn=derivatives of f(q) at q=0, q=qgrid(mqgrid)
+   yp1=0.d0
+   qmax=qgrid(mqgrid)
+   ypn=4.d0*pi**2*qmax*rrs**2*exp(-0.5d0*(two_pi*qmax*rrs)**2) * &
+&   (-5.d0+(two_pi*qmax*rrs)**2)
+!  Fit spline to get second derivatives by spline fit
+   call spline(qgrid,ffspl(:,1,iln),mqgrid,yp1,ypn,ffspl(:,2,iln))
+!  else if(mproj>=2)then
+!  or else put second projector nonlocal correction at l=0 to 0
+!  ffspl(:,:,iln)=0.0d0
+ end if
+
+!l=1 first projector
+ if (abs(h1p)>1.d-12) then
+   iln=iln+1
+   do iqgrid=1,mqgrid
+     ffspl(iqgrid,1,iln)=exp(-0.5d0*(two_pi*qgrid(iqgrid)*rrp)**2) * &
+&     (two_pi*qgrid(iqgrid))
+   end do
+!  Compute yp1,ypn=derivatives of f(q) at q=0, q=qgrid(mqgrid)
+   yp1=two_pi
+   qmax=qgrid(mqgrid)
+   ypn=-two_pi*((two_pi*qmax*rrp)**2-1.d0) * exp(-0.5d0*(two_pi*qmax*rrp)**2)
+!  Fit spline to get second derivatives by spline fit
+   call spline(qgrid,ffspl(:,1,iln),mqgrid,yp1,ypn,ffspl(:,2,iln))
+!  else if(mpsang>=2)then
+!  or else put first projector l=1 nonlocal correction to 0
+!  ffspl(:,:,iln)=0.0d0
+ end if
+
+ ABI_DEALLOCATE(work)
+
+end subroutine psp2nl
 !!***
--- src/64_psp/psp6cc.F90
+++ src/64_psp/psp6cc.F90
@@ -9,7 +9,7 @@
 !! for the format 6 of pseudopotentials.
 !!
 !! COPYRIGHT
-!! Copyright (C) 1998-2018 ABINIT group (AF)
+!! Copyright (C) 1998-2018 ABINIT group (AF, GJ,FJ,MT)
 !! This file is distributed under the terms of the
 !! GNU General Public License, see ~abinit/COPYING
 !! or http://www.gnu.org/copyleft/gpl.txt .
@@ -215,11 +215,161 @@ subroutine psp6cc(mmax,n1xccc,rchrg,xccc1d,znucl,&
  ABI_DEALLOCATE(work)
  ABI_DEALLOCATE(xx)
 
- return 
+ return
 
  ! Handle IO error
  10 continue
  MSG_ERROR(errmsg)
 
 end subroutine psp6cc
+!!***
+
+!!****f* ABINIT/psden
+!! NAME
+!! psden
+!!
+!! FUNCTION
+!! Calculate a pseudo-density from an original density on a radial grid (regular or logarithmic)
+!!
+!! INPUTS
+!!  ilog=1 if grid is logarithmic, else 0
+!!  mesh= dimension of nc
+!!  nc(mesh)= density to be pseudized
+!!  rc= cut-off radius
+!!  rad(mesh) = radial mesh
+!!
+!! OUTPUT
+!!  ff(mesh)= pseudized density
+!!
+!!SIDE EFFECTS
+!!  Optional:
+!!    ff1(mesh)= 1st derivative of pseudo density (only r<rc modified)
+!!    ff2(mesh)= 2nd derivative of pseudo density (only r<rc modified)
+!!
+!! NOTES
+!!    ff=exp(-(a+b.r^2+c.r^4))
+!!
+!! PARENTS
+!!      psp6cc
+!!
+!! CHILDREN
+!!      ctrap
+!!
+!! SOURCE
+
+subroutine psden(ilog,ff,mesh,nc,rc,rad,ff1,ff2)
+
+ use defs_basis
+ use m_profiling_abi
+ use m_errors
+
+ use m_numeric_tools, only : ctrap
+
+!This section has been created automatically by the script Abilint (TD).
+!Do not modify the following lines by hand.
+#undef ABI_FUNC
+#define ABI_FUNC 'psden'
+!End of the abilint section
+
+ implicit none
+
+!Arguments ------------------------------------
+!scalars
+ integer,intent(in) :: ilog,mesh
+ real(dp),intent(in) :: rc
+!arrays
+ real(dp),intent(in) :: nc(mesh),rad(mesh)
+ real(dp),intent(out) :: ff(mesh)
+ real(dp),intent(inout),optional :: ff1(mesh),ff2(mesh)
+
+!Local variables-------------------------------
+!scalars
+ integer :: ii,nc1
+ real(dp) :: aa,aa1,aa2,bb,cc,c1,c3,f0,f0p,norm1,norm2,rc1,step
+!arrays
+ real(dp),allocatable :: fpir(:),gg(:)
+
+! *************************************************************************
+
+ rc1=rc/four
+
+ ABI_ALLOCATE(fpir,(mesh))
+ fpir(1:mesh)=four_pi*rad(1:mesh)**2
+ if (ilog==1) fpir(1:mesh)=fpir(1:mesh)*rad(1:mesh)
+
+ if (ilog==0) then
+   step=rad(2)-rad(1)
+   nc1=int(rc1/step)+1
+   rc1=(nc1-1)*step
+ else if (ilog==1) then
+   step=log(rad(2)/rad(1))
+   nc1=int(log(rc1/rad(1))/step)+1
+   rc1=rad(nc1)
+ end if
+ ff(1:nc1)=nc(1:nc1)*fpir(1:nc1)
+ call ctrap(nc1,ff(1:nc1),step,c3)
+ if (ilog==1) c3=c3+half*ff(1)
+ f0=nc(nc1);c1=-log(f0)
+ f0p=half*(nc(nc1+1)-nc(nc1-1))/step
+
+ ii=0;aa1=zero;norm1=c3+one
+ do while (norm1>c3.and.ii<100)
+   ii=ii+1;aa1=aa1+one
+   aa=c1-aa1*rc1**4+rc1*(f0p/f0+four*aa1*rc1**3)*half
+   bb=-half*(f0p/f0+four*aa1*rc1**3)/rc1
+   ff(1:nc1)=fpir(1:nc1)*exp(-aa-bb*rad(1:nc1)**2-aa1*rad(1:nc1)**4)
+   call ctrap(nc1,ff(1:nc1),step,norm1)
+   if (ilog==1) norm1=norm1+half*ff(1)
+ end do
+ if (ii==100) then
+   MSG_ERROR('Big pb 1 in psden !')
+ end if
+
+ ii=0;aa2=zero;norm2=c3-one
+ do while (norm2<c3.and.ii<100)
+   ii=ii+1;aa2=aa2-one
+   aa=c1-aa2*rc1**4+rc1*(f0p/f0+four*aa2*rc1**3)*half
+   bb=-half*(f0p/f0+four*aa2*rc1**3)/rc1
+   ff(1:nc1)=fpir(1:nc1)*exp(-aa-bb*rad(1:nc1)**2-aa2*rad(1:nc1)**4)
+   call ctrap(nc1,ff(1:nc1),step,norm2)
+   if (ilog==1) norm2=norm2+half*ff(1)
+ end do
+ if (ii==100) then
+   MSG_ERROR('Big pb 2 in psden !')
+ end if
+
+ do while (abs(norm2-c3)>tol10)
+
+   cc=(aa1+aa2)*half
+   aa=c1-cc*rc1**4+rc1*(f0p/f0+four*cc*rc1**3)*half
+   bb=-half*(f0p/f0+four*cc*rc1**3)/rc1
+   ff(1:nc1)=fpir(1:nc1)*exp(-aa-bb*rad(1:nc1)**2-cc*rad(1:nc1)**4)
+   call ctrap (nc1,ff(1:nc1),step,norm2)
+   if (ilog==1) norm2=norm2+half*ff(1)
+   if ((norm1-c3)*(norm2-c3)>zero) then
+     aa1=cc
+     norm1=norm2
+   else
+     aa2=cc
+   end if
+
+ end do ! while
+
+ ff(1)=exp(-aa);if (ilog==1) ff(1)=ff(1)*exp(-bb*rad(1)**2-cc*rad(1)**4)
+ ff(2:nc1)=ff(2:nc1)/fpir(2:nc1)
+ if (nc1<mesh) ff(nc1+1:mesh)=nc(nc1+1:mesh)
+ if (present(ff1)) ff1(1:nc1)=-(two*bb*rad(1:nc1)+four*cc*rad(1:nc1)**3)*ff(1:nc1)
+ if (present(ff2)) ff2(1:nc1)=-(two*bb+12.0_dp*cc*rad(1:nc1)**2)*ff(1:nc1) &
+& +(two*bb*rad(1:nc1)+four*cc*rad(1:nc1)**3)**2*ff(1:nc1)
+
+ ABI_ALLOCATE(gg,(mesh))
+ gg(1:mesh)=fpir(1:mesh)*ff(1:mesh)
+ call ctrap(mesh,gg(1:mesh),step,norm1)
+ if (ilog==1) norm1=norm1+half*gg(1)
+ write(std_out,*) 'psden: tild_nc integral= ',norm1
+ ABI_DEALLOCATE(gg)
+
+ ABI_DEALLOCATE(fpir)
+
+end subroutine psden
 !!***
