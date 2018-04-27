57b39e713c0584edfa5be121bddd27f106264639
Matteo Giantomassi <matteo.giantomassi@uclouvain.be>
2018-04-27 15:24:32 +0200

    Mv freeze_displ_allmodes to m_phonons

--- src/77_ddb/m_phonons.F90
+++ src/77_ddb/m_phonons.F90
@@ -1432,7 +1432,7 @@ subroutine thermal_supercell_make(Crystal, Ifc, nconfig,&
    return
  end if
 
- temperature = temperature_K /  Ha_K 
+ temperature = temperature_K /  Ha_K
 
  ! build qpoint grid used for the Fourier interpolation.
  !(use no syms for the moment!)
@@ -1485,7 +1485,7 @@ subroutine thermal_supercell_make(Crystal, Ifc, nconfig,&
  ! for all modes at all q in whole list, sorted
  do iq = 1, nqibz
    do imode = 1, 3*Crystal%natom
-     
+
      ! skip modes with too low or negative frequency -> Bose factor explodes (eg acoustic at Gamma)
      ! TODO: check the convergence wrt the tolerance
      ! several philosophies to be implemented for the unstable modes:
@@ -1493,15 +1493,15 @@ subroutine thermal_supercell_make(Crystal, Ifc, nconfig,&
      ! 2) populate them according to a default amplitude
      ! 3) populate according to their modulus squared
      if (abs(phfrq_allq(imode, iq))<tol6) cycle
-   
+
      phdispl1 = phdispl_allq(:,:,:,imode,iq)
 
      ! loop over configurations
      do iconfig = 1, nconfig
-   
+
        ! trick supercell object into using present q point
        thm_scells(iconfig)%qphon(:) = qibz(:,iq)
-   
+
        ! find thermal displacement amplitude eq 4 of Zacharias
        !   combined with l_nu,q expression in paragraph before
        if (phfrq_allq(imode, iq) > tol6) then
@@ -1518,7 +1518,7 @@ subroutine thermal_supercell_make(Crystal, Ifc, nconfig,&
            !Default amplitude for all the frequencies
            sigma = 100._dp
          case(3)
-           !absolute value of the frequencie 
+           !absolute value of the frequencie
            sigma=sqrt((bose_einstein(abs(phfrq_allq(imode,iq)),temperature)+half)/&
 &                abs(phfrq_allq(imode,iq)))
          case(4)
@@ -1529,7 +1529,7 @@ subroutine thermal_supercell_make(Crystal, Ifc, nconfig,&
        call RANDOM_NUMBER(rand)
        rand = two * rand - one
 
-       ! from TDEP documentation for gaussian distribution of displacements 
+       ! from TDEP documentation for gaussian distribution of displacements
        !call RANDOM_NUMBER(rand1)
        !call RANDOM_NUMBER(rand2)
        ! rand = sqrt(-two*rand1) * sin(twopi*rand2)
@@ -1541,7 +1541,7 @@ subroutine thermal_supercell_make(Crystal, Ifc, nconfig,&
        ! end if
 
        freeze_displ =  rand * sigma
-       
+
        call freeze_displ_supercell (phdispl1(:,:,:), freeze_displ, thm_scells(iconfig))
      end do !iconfig
    end do !imode
@@ -1709,7 +1709,7 @@ subroutine thermal_supercell_print(fname, nconfig, temperature_K, thm_scells)
  type(supercell_type), intent(in) :: thm_scells(nconfig)
  character(len=fnlen), intent(in) :: fname
  real(dp), intent(in) :: temperature_K
- 
+
 ! local
  integer :: iconfig,itemp
  character(len=80) :: title1, title2
@@ -3752,6 +3752,99 @@ subroutine dfpt_symph(iout,acell,eigvec,indsym,natom,nsym,phfrq,rprim,symrel)
  ABI_DEALLOCATE(symind)
 
 end subroutine dfpt_symph
+!!***
+
+!!****f* m_phonons/freeze_displ_allmodes
+!!
+!! NAME
+!! freeze_displ_allmodes
+!!
+!! FUNCTION
+!!  From a given set of phonon modes, generate and output supercells and
+!!  displaced configurations of atoms.
+!!  Typically useful to follow soft modes and see distorsions of crystal structures
+!!
+!! INPUTS
+!! amu(ntypat) = mass of the atoms (atomic mass unit)
+!! displ(2,3*natom,3*natom) = phonon mode displacements (complex)
+!! freeze_displ = amplitude of the displacement to freeze into the supercell
+!! natom = number of atoms in the unit cell
+!! ntypat = number of atom types
+!! phfrq(3*natom) = phonon frequencies
+!! qphnrm = norm of phonon q vector (should be 1 or 0)
+!! qphon = phonon wavevector
+!! rprimd(3,3) = dimensionfull primitive translations in real space
+!! typat(natom) = integer label of each type of atom (1,2,...)
+!! xcart(3,natom) = cartesian coords of atoms in unit cell (bohr)
+!!
+!! OUTPUT
+!! for the moment only prints to file, but could also return pointer to supercell object, with
+!! rprimd and atomic positions, for further use
+!!
+!! NOTES
+!! freeze_displ could be determined automatically from a temperature and the phonon frequency,
+!! as the average displacement of the mode with a Bose distribution.
+!!
+!! PARENTS
+!!      m_phonons
+!!
+!! CHILDREN
+!!      destroy_supercell,freeze_displ_supercell,init_supercell_for_qpt
+!!      prt_supercell_for_qpt
+!!
+!! SOURCE
+!!
+
+subroutine freeze_displ_allmodes(displ, freeze_displ, natom, outfile_radix, phfreq,  &
+&         qphon, rprimd, typat, xcart, znucl)
+
+
+!Do not modify the following lines by hand.
+#undef ABI_FUNC
+#define ABI_FUNC 'freeze_displ_allmodes'
+!End of the abilint section
+
+ implicit none
+
+! arguments
+! scalar
+ integer,intent(in) :: natom
+ character(len=*),intent(in) :: outfile_radix
+ real(dp), intent(in) :: freeze_displ
+
+!arrays
+ integer,intent(in) :: typat(natom)
+
+ real(dp),intent(in) :: displ(2,3*natom,3*natom)
+ real(dp),intent(in) :: rprimd(3,3)
+ real(dp),intent(in) :: phfreq(3*natom)
+ real(dp),intent(in) :: qphon(3)
+ real(dp),intent(in) :: xcart(3,natom)
+ real(dp),intent(in) :: znucl(:)
+
+! local vars
+ integer :: jmode
+ type(supercell_type) :: scell
+
+! *************************************************************************
+
+!determine supercell needed to freeze phonon
+ call init_supercell_for_qpt(natom, qphon, rprimd, typat, xcart, znucl, scell)
+
+ do jmode = 1, 3*natom
+! reset positions
+   scell%xcart = scell%xcart_ref
+
+!  displace atoms according to phonon jmode
+   call freeze_displ_supercell(displ(:,:,jmode), freeze_displ, scell)
+
+!  print out everything for this wavevector and mode
+   call prt_supercell_for_qpt (phfreq(jmode), jmode, outfile_radix, scell)
+ end do
+
+ call destroy_supercell (scell)
+
+end subroutine freeze_displ_allmodes
 !!***
 
 end module m_phonons
