2d1b5f29f9cdd30504aca1719aa754a6f7a143ca
Matteo Giantomassi <matteo.giantomassi@uclouvain.be>
2018-05-04 21:17:10 +0200

    Modularize mlwfovlp routines

--- src/94_scfcv/m_outscfcv.F90
+++ src/94_scfcv/m_outscfcv.F90
@@ -207,6 +207,7 @@ subroutine outscfcv(atindx1,cg,compch_fft,compch_sph,cprj,dimcprj,dmatpawu,dtfil
  use m_mlwfovlp_qp,      only : mlwfovlp_qp
  use m_pawmkaewf,        only : pawmkaewf
  use m_dens,             only : mag_constr_e, calcdensph
+ use m_mlwfovlp,         only : mlwfovlp
 
 !This section has been created automatically by the script Abilint (TD).
 !Do not modify the following lines by hand.
