55441b1588bbc6b3dcbf65c7d77191a140b2639d
Matteo Giantomassi <matteo.giantomassi@uclouvain.be>
2018-05-08 22:18:31 +0200

    Mv ionion_ routines inside m_setvtr

--- src/67_common/m_setvtr.F90
+++ src/67_common/m_setvtr.F90
@@ -925,5 +925,227 @@ subroutine spatialchempot(e_chempot,chempot,grchempottn,natom,ntypat,nzchempot,t
 end subroutine spatialchempot
 !!***
 
+!!****f* ABINIT/ionion_realspace
+!!
+!! NAME
+!! ionion_realspace
+!!
+!! FUNCTION
+!! Compute the ion/ion interaction energies and forces in real space
+!! case. Use ewald() instead if computations are done in reciprocal
+!! space since it also includes the correction for the shift done in
+!! potentials calculations and includes replica interactions.
+!!
+!! INPUTS
+!!  dtset <type(dataset_type)>=all input variables in this dataset
+!!  rmet(3,3)=metric tensor in real space (bohr^2)
+!!  xred(3,natom)=relative coords of atoms in unit cell (dimensionless)
+!!  zion(ntypat)=charge on each type of atom (real number)
+!!
+!! OUTPUT
+!!  eew=final ion/ion energy in hartrees
+!!  grewtn(3,natom)=grads of ion/ion wrt xred(3,natom), hartrees.
+!!
+!! PARENTS
+!!      setvtr
+!!
+!! CHILDREN
+!!      xred2xcart
+!!
+!! SOURCE
+
+subroutine ionion_realSpace(dtset, eew, grewtn, rprimd, xred, zion)
+
+!This section has been created automatically by the script Abilint (TD).
+!Do not modify the following lines by hand.
+#undef ABI_FUNC
+#define ABI_FUNC 'ionion_realSpace'
+!End of the abilint section
+
+ implicit none
+
+!Arguments ------------------------------------
+!scalars
+ real(dp),intent(out) :: eew
+ type(dataset_type),intent(in) :: dtset
+!arrays
+ real(dp),intent(in) :: rprimd(3,3),zion(dtset%ntypat)
+ real(dp),intent(in) :: xred(3,dtset%natom)
+ real(dp),intent(out) :: grewtn(3,dtset%natom)
+
+!Local variables-------------------------------
+!scalars
+ integer :: ia1,ia2,iatom,igeo
+ real(dp) :: r
+!arrays
+ real(dp),allocatable :: grew_cart(:,:),xcart(:,:)
+
+! *************************************************************************
+
+!Store xcart for each atom
+ ABI_ALLOCATE(xcart,(3, dtset%natom))
+ call xred2xcart(dtset%natom, rprimd, xcart, xred)
+
+!Summing the interaction between ions.
+ eew = 0._dp
+ do ia1 = 1, dtset%natom, 1
+   do ia2 = ia1 + 1, dtset%natom, 1
+     r = sqrt((xcart(1, ia1) - xcart(1, ia2)) ** 2 + &
+&     (xcart(2, ia1) - xcart(2, ia2)) ** 2 + &
+&     (xcart(3, ia1) - xcart(3, ia2)) ** 2)
+     eew = eew + zion(dtset%typat(ia1)) * zion(dtset%typat(ia2)) / r
+   end do
+ end do
+
+!Allocate temporary array to store cartesian gradients.
+ ABI_ALLOCATE(grew_cart,(3, dtset%natom))
+
+!Summing the forces for each atom
+ do ia1 = 1, dtset%natom, 1
+   grew_cart(:, ia1) = 0._dp
+   do ia2 = 1, dtset%natom, 1
+     if (ia1 /= ia2) then
+       r = (xcart(1, ia1) - xcart(1, ia2)) ** 2 + &
+&       (xcart(2, ia1) - xcart(2, ia2)) ** 2 + &
+&       (xcart(3, ia1) - xcart(3, ia2)) ** 2
+       do igeo = 1, 3, 1
+         grew_cart(igeo, ia1) = grew_cart(igeo, ia1) - (xcart(igeo, ia1) - xcart(igeo, ia2)) * &
+&         zion(dtset%typat(ia1)) * zion(dtset%typat(ia2)) / (r ** 1.5_dp)
+       end do
+     end if
+   end do
+ end do
+
+ ABI_DEALLOCATE(xcart)
+
+!Transform cartesian gradients to reduced gradients.
+ do iatom = 1, dtset%natom, 1
+   do igeo = 1, 3, 1
+     grewtn(igeo, iatom) = rprimd(1, igeo) * grew_cart(1, iatom) + &
+&     rprimd(2, igeo) * grew_cart(2, iatom) + &
+&     rprimd(3, igeo) * grew_cart(3, iatom)
+   end do
+ end do
+ ABI_DEALLOCATE(grew_cart)
+
+end subroutine ionion_realSpace
+!!***
+
+!!****f* ABINIT/ionion_surface
+!!
+!! NAME
+!! ionion_surface
+!!
+!! FUNCTION
+!! Compute the ion/ion interaction energies and forces in real space
+!! case. Use ewald() instead if computations are done in reciprocal
+!! space since it also includes the correction for the shift done in
+!! potentials calculations and includes replica interactions.
+!!
+!! INPUTS
+!!  dtset <type(dataset_type)>=all input variables in this dataset
+!!  rmet(3,3)=metric tensor in real space (bohr^2)
+!!  xred(3,natom)=relative coords of atoms in unit cell (dimensionless)
+!!  zion(ntypat)=charge on each type of atom (real number)
+!!
+!! OUTPUT
+!!  eew=final ion/ion energy in hartrees
+!!  grewtn(3,natom)=grads of ion/ion wrt xred(3,natom), hartrees.
+!!
+!! PARENTS
+!!      setvtr
+!!
+!! CHILDREN
+!!      ionicenergyandforces,xred2xcart
+!!
+!! SOURCE
+
+subroutine ionion_surface(dtset, eew, grewtn, me, nproc, rprimd, wvl, wvl_den, xred)
+
+ use defs_basis
+ use defs_abitypes
+ use defs_wvltypes
+ use m_profiling_abi
+#if defined HAVE_BIGDFT
+ use BigDFT_API, only: IonicEnergyandForces
+#endif
+
+ use m_geometry,    only : xred2xcart
+
+!This section has been created automatically by the script Abilint (TD).
+!Do not modify the following lines by hand.
+#undef ABI_FUNC
+#define ABI_FUNC 'ionion_surface'
+!End of the abilint section
+
+ implicit none
+
+!Arguments ------------------------------------
+!scalars
+ integer, intent(in) :: me, nproc
+ real(dp),intent(out) :: eew
+ type(dataset_type),intent(in) :: dtset
+ type(wvl_internal_type), intent(in) :: wvl
+ type(wvl_denspot_type), intent(inout) :: wvl_den
+!arrays
+ real(dp),intent(in) :: rprimd(3,3)
+ real(dp),intent(in) :: xred(3,dtset%natom)
+ real(dp),intent(out) :: grewtn(3,dtset%natom)
+
+!Local variables-------------------------------
+!scalars
+ integer :: dispersion, iatom, igeo
+ real(dp) :: psoffset
+!arrays
+ real(dp),allocatable :: xcart(:,:)
+ real(dp),pointer :: grew_cart(:,:),fdisp(:,:)
+#if defined HAVE_BIGDFT
+ real(dp) :: edisp
+ real(dp) :: ewaldstr(6)
+#endif
+
+! *************************************************************************
+
+!Store xcart for each atom
+ ABI_ALLOCATE(xcart,(3, dtset%natom))
+ call xred2xcart(dtset%natom, rprimd, xcart, xred)
+
+ nullify(fdisp)
+ nullify(grew_cart)
+ dispersion = 0
+ psoffset = 0._dp
+#if defined HAVE_BIGDFT
+ call IonicEnergyandForces(me, nproc, wvl_den%denspot%dpbox,&
+& wvl%atoms, dtset%efield, xcart, &
+& eew, grew_cart, dispersion, edisp, fdisp,&
+& ewaldstr,wvl%Glr%d%n1,wvl%Glr%d%n2,wvl%Glr%d%n3,&
+& wvl_den%denspot%V_ext, wvl_den%denspot%pkernel,psoffset)
+
+ if (associated(fdisp)) then
+   ABI_DEALLOCATE(fdisp)
+ end if
+#endif
+
+ ABI_DEALLOCATE(xcart)
+
+!Transform cartesian gradients to reduced gradients.
+ do iatom = 1, dtset%natom, 1
+   do igeo = 1, 3, 1
+     grewtn(igeo, iatom) = -rprimd(1, igeo) * grew_cart(1, iatom) - &
+&     rprimd(2, igeo) * grew_cart(2, iatom) - &
+&     rprimd(3, igeo) * grew_cart(3, iatom)
+   end do
+ end do
+ if (associated(grew_cart)) then
+   ABI_DEALLOCATE(grew_cart)
+ end if
+
+#if !defined HAVE_BIGDFT
+ if (.false.) write(std_out,*) me,nproc,wvl%h(1),wvl_den%symObj
+#endif
+
+end subroutine ionion_surface
+!!***
+
 end module m_setvtr
 !!***
