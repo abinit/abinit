84a559aeb70b7bffaa0847d3616790442a3d1267
Bernard Amadon <bernard.amadon@cea.fr>
2018-05-09 11:51:29 +0200

    Include correct reading of dmft variables

--- src/57_iovars/invars2.F90
+++ src/57_iovars/invars2.F90
@@ -1839,7 +1839,7 @@ subroutine invars2(bravais,dtset,iout,jdtset,lenstr,&
 &       'Action: add dmftqmc_therm keyword in input file.'
        MSG_ERROR(message)
      end if
-     if(dtset%dmft_solv==5) then
+     if(dtset%dmft_solv==5.or.dtset%dmft_solv==8) then
        call intagm(dprarr,intarr,jdtset,marr,1,string(1:lenstr),'dmftctqmc_basis',tread,'INT')
        if(tread==1) dtset%dmftctqmc_basis  =intarr(1)
        call intagm(dprarr,intarr,jdtset,marr,1,string(1:lenstr),'dmftctqmc_check',tread,'INT')
