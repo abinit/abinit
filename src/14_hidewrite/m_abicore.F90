63d9f1a8b35771de1b709b1cc4c03c509a111242
Matteo Giantomassi <matteo.giantomassi@uclouvain.be>
2018-08-23 23:25:27 +0200

    Add m_abicore

---
+++ src/14_hidewrite/m_abicore.F90
@@ -0,0 +1,39 @@
+!{\src2tex{textfont=tt}}
+!!****m* ABINIT/m_abicore
+!! NAME
+!!  m_abicore
+!!
+!! FUNCTION
+!!
+!! COPYRIGHT
+!! Copyright (C) 2008-2018 ABINIT group
+!! This file is distributed under the terms of the
+!! GNU General Public License, see ~abinit/COPYING
+!! or http://www.gnu.org/copyleft/gpl.txt .
+!!
+!! PARENTS
+!!
+!! CHILDREN
+!!
+!! SOURCE
+
+#if defined HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "abi_common.h"
+
+module m_abicore
+
+ !use defs_basis
+ !use m_build_info
+ use m_specialmsg
+ use m_profiling_abi
+
+ implicit none
+
+ private
+!!***
+
+END MODULE m_specialmsg
+!!***
