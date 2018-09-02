c3d3da16c89850687699746f34381dddfb3c914a
Alexandre Martin <alexandre.martin@uliege.be>
2018-09-02 18:25:42 +0200

    add missing files for the multibinit tutorials

---
+++ tests/tutomultibinit/__init__.py
@@ -0,0 +1,16 @@
+"""Global variables associated to the test suite."""
+
+#: List of CPP variables that should be defined in config.h in order to enable this suite.
+need_cpp_vars = [
+]
+
+#: List of keywords that are automatically added to all the tests of this suite.
+keywords = [
+]
+
+#: List of input files
+inp_files = [
+    "tmulti1_1.in",
+    "tmulti1_2.in",
+    "tmulti1_3.in",
+]
