14d9f3d1546ca2794ec723de2d0a4aa699cf045f
biederj <biederj@u-tracy>
2018-10-15 15:50:40 +0200

    Fix routine for testing purpose only

--- src/79_seqpar_mpi/m_lobpcgwf.F90
+++ src/79_seqpar_mpi/m_lobpcgwf.F90
@@ -211,15 +211,15 @@ subroutine lobpcgwf2(cg,dtset,eig,enl_out,gs_hamk,kinpw,mpi_enreg,&
 
  !call xg_init(xgeigen,SPACE_R,nband,1,l_mpi_enreg%comm_bandspinorfft)
  ! Trick the with C to change rank of arrays (:) to (:,:)
- cptr = c_loc(eig)
- call c_f_pointer(cptr,eig_ptr,(/ nband,1 /))
- call xgBlock_map(xgeigen,eig_ptr,SPACE_R,nband,1)
+! cptr = c_loc(eig)
+! call c_f_pointer(cptr,eig_ptr,(/ nband,1 /))
+! call xgBlock_map(xgeigen,eig_ptr,SPACE_R,nband,1)
 
  !call xg_init(xgresidu,SPACE_R,nband,1,l_mpi_enreg%comm_bandspinorfft)
  ! Trick the with C to change rank of arrays (:) to (:,:)
- cptr = c_loc(resid)
- call c_f_pointer(cptr,resid_ptr,(/ nband,1 /))
- call xgBlock_map(xgresidu,resid_ptr,SPACE_R,nband,1)
+! cptr = c_loc(resid)
+! call c_f_pointer(cptr,resid_ptr,(/ nband,1 /))
+! call xgBlock_map(xgresidu,resid_ptr,SPACE_R,nband,1)
 
  !ABI_MALLOC(l_gvnlc,(2,l_npw*l_nspinor*blockdim))
 
@@ -229,7 +229,8 @@ subroutine lobpcgwf2(cg,dtset,eig,enl_out,gs_hamk,kinpw,mpi_enreg,&
 
 ! Copy initial xgx0
 ABI_MALLOC(first,(2,l_npw*l_nspinor*nband))
-call xgBlock_get(xgx0,first,0,l_npw*l_nspinor)
+first = cg
+!call xgBlock_get(xgx0,first,0,l_npw*l_nspinor)
 
 ! Get full number of npw
 call xmpi_sum(l_npw,mpi_enreg%comm_bandspinorfft,iband)
@@ -242,20 +243,13 @@ cputime = 0
 ncpuCols = dtset%npband
 ncpuRows = dtset%npfft*dtset%npspinor
 
-
-! Debug
-!call xgBlock_map(xgx0,cg,space,l_icplx*4,8,l_mpi_enreg%comm_bandspinorfft)
-!ABI_MALLOC(l_gvnlc,(2,l_icplx*4*4*2))
-!call xgBlock_map(xgeigen,l_gvnlc,space,l_icplx*4*4,2,l_mpi_enreg%comm_fft)
-!call xgBlock_print(xgx0,6)
-
-write(*,*) dtset%useric
-l_istwf = 10
 call xgTransposer_init(xgTransposer,xgx0,xgeigen,ncpuRows,ncpuCols,STATE_LINALG,dtset%useric)
-do iband =1 , l_istwf
+
+do iband =1 , 10
   walltime = abi_wtime()
   call xgTransposer_transpose(xgTransposer,STATE_COLSROWS)
-  call xgBlock_scale(xgx0,0.d0,1)
+  call random_number(cg)
+  !call xgBlock_scale(xgx0,0.d0,1)
   !call xgBlock_print(xgeigen,6)
   call xgTransposer_transpose(xgTransposer,STATE_LINALG)
   !call xgBlock_print(xgx0,6)
@@ -274,193 +268,16 @@ call xmpi_sum(errmax,l_mpi_enreg%comm_bandspinorfft,nthreads)
 write(std_out,*) "difference:",errmax
 call flush(std_out)
 ABI_FREE(first)
-call xmpi_barrier(l_mpi_enreg%comm_bandspinorfft)
 call xmpi_abort()
-!!------------------------ TEST --------------------------!
-
-!###########################################################################
-!################    RUUUUUUUN    ##########################################
-!###########################################################################
- 
- ! Run lobpcg
- call lobpcg_run(lobpcg,xgx0,getghc_gsc,precond,xgeigen,xgresidu,prtvol)
-
- ! Free preconditionning since not needed anymore
- ABI_FREE(l_pcon)
-
- ! Scale back
- if(l_istwf == 2) then
-   call xgBlock_scale(xgx0,inv_sqrt2,1)
-   if(l_mpi_enreg%me_g0 == 1) cg(:, 1:npw*nspinor*nband:npw) = cg(:, 1:npw*nspinor*nband:npw) * sqrt2
- end if
-
- ! Compute enlout (nonlocal energy for each band if necessary) This is the best
- ! quick and dirty trick to compute this part in NC. gvnlc cannot be part of
- ! lobpcg algorithm
- if ( .not. l_paw ) then
-   !Check l_gvnlc size
-   !if ( size(l_gvnlc) < 2*nband*l_npw*l_nspinor ) then
-   if ( size(l_gvnlc) /= 0 ) then
-     ABI_FREE(l_gvnlc)
-     !ABI_MALLOC(l_gvnlc,(2,nband*l_npw*l_nspinor))
-     ABI_MALLOC(l_gvnlc,(0,0))
-   end if
-   !Call nonlop
-   if (mpi_enreg%paral_kgb==0) then
- 
-     call nonlop(choice,l_cpopt,cprj_dum,enl_out,l_gs_hamk,0,eig,mpi_enreg,nband,1,paw_opt,&
-&                signs,gsc_dummy,l_tim_getghc,cg,l_gvnlc)
- 
-   else
-     do iband=1,nband/blockdim
-       shift = (iband-1)*blockdim*l_npw*l_nspinor
-      call prep_nonlop(choice,l_cpopt,cprj_dum, &
-&       enl_out((iband-1)*blockdim+1:iband*blockdim),l_gs_hamk,0,&
-&       eig((iband-1)*blockdim+1:iband*blockdim),blockdim,mpi_enreg,1,paw_opt,signs,&
-&       gsc_dummy,l_tim_getghc, &
-&       cg(:,shift+1:shift+blockdim*l_npw*l_nspinor),&
-!&       l_gvnlc(:,shift+1:shift+blockdim*l_npw*l_nspinor),&
-&       l_gvnlc(:,:),&
-&       already_transposed=.false.)
-     end do
-   end if
-   !Compute enlout
-!   do iband=1,nband
-!     shift = npw*nspinor*(iband-1)
-!       call dotprod_g(enl_out(iband),dprod_i,l_gs_hamk%istwf_k,npw*nspinor,1,cg(:, shift+1:shift+npw*nspinor),&
-!  &     l_gvnlc(:, shift+1:shift+npw*nspinor),mpi_enreg%me_g0,mpi_enreg%comm_bandspinorfft)
-!   end do
- end if
-
- ABI_FREE(l_gvnlc)
 
  ! Free lobpcg
  call lobpcg_free(lobpcg)
-
-!###########################################################################
-!################    SORRY IT'S ALREADY FINISHED : )  ######################
-!###########################################################################
-
-
- call timab(tim_lobpcgwf2,2,tsec)
- cputime = abi_cpu_time() - cputime
- walltime = abi_wtime() - walltime
- nthreads = xomp_get_num_threads(open_parallel = .true.)
- if ( cputime/walltime/dble(nthreads) < 0.75 .and. (int(cputime/walltime)+1) /= nthreads) then
-   if ( prtvol >= 3 ) then
-     write(std_out,'(a)',advance='no') sjoin(" Lobpcg took", sec2str(cputime), "of cpu time")
-     write(std_out,*) sjoin("for a wall time of", sec2str(walltime))
-     write(std_out,'(a,f6.2)') " -> Ratio of ", cputime/walltime
-   end if
-   MSG_COMMENT(sjoin("You should set the number of threads to something close to",itoa(int(cputime/walltime)+1)))
- end if
-
 
  DBG_EXIT("COLL")
 
 end subroutine lobpcgwf2
 
 
- subroutine getghc_gsc(X,AX,BX)
-   use m_xg, only : xg_t, xgBlock_get, xgBlock_set, xgBlock_getSize, xgBlock_t
-#ifdef HAVE_OPENMP
-   use omp_lib
-#endif
-
-!This section has been created automatically by the script Abilint (TD).
-!Do not modify the following lines by hand.
-#undef ABI_FUNC
-#define ABI_FUNC 'getghc_gsc'
- use interfaces_66_wfs
-!End of the abilint section
-
-  type(xgBlock_t), intent(inout) :: X
-  type(xgBlock_t), intent(inout) :: AX
-  type(xgBlock_t), intent(inout) :: BX
-  integer         :: blockdim
-  integer         :: spacedim
-  type(pawcprj_type) :: cprj_dum(l_gs_hamk%natom,0)
-  double precision :: dum
-  double precision, parameter :: inv_sqrt2 = 1/sqrt2
-  double precision, pointer :: cg(:,:)
-  double precision, pointer :: ghc(:,:)
-  double precision, pointer :: gsc(:,:)
- 
-  call xgBlock_getSize(X,spacedim,blockdim)
-  spacedim = spacedim/l_icplx
- 
- 
-  !call xgBlock_get(X,cg(:,1:blockdim*spacedim),0,spacedim)
-  call xgBlock_reverseMap(X,cg,l_icplx,spacedim*blockdim)
-  call xgBlock_reverseMap(AX,ghc,l_icplx,spacedim*blockdim)
-  call xgBlock_reverseMap(BX,gsc,l_icplx,spacedim*blockdim)
-
-  ! scale back cg
- if(l_istwf == 2) then
-   !cg(:,1:spacedim*blockdim) = cg(:,1:spacedim*blockdim) * inv_sqrt2
-   call xgBlock_scale(X,inv_sqrt2,1)
-   if(l_mpi_enreg%me_g0 == 1) cg(:, 1:spacedim*blockdim:l_npw) = cg(:, 1:spacedim*blockdim:l_npw) * sqrt2
- end if
-
- if ( size(l_gvnlc) < 2*blockdim*spacedim ) then
-   ABI_FREE(l_gvnlc)
-   ABI_MALLOC(l_gvnlc,(2,blockdim*spacedim))
- end if
- 
-  if (l_mpi_enreg%paral_kgb==0) then
-
-    call multithreaded_getghc(l_cpopt,cg(:,1:blockdim*spacedim),cprj_dum,ghc,gsc(:,1:blockdim*spacedim),&
-      l_gs_hamk,l_gvnlc,dum, l_mpi_enreg,blockdim,l_prtvol,l_sij_opt,l_tim_getghc,0)
-
-  else
-    call prep_getghc(cg(:,1:blockdim*spacedim),l_gs_hamk,l_gvnlc,ghc,gsc(:,1:blockdim*spacedim),dum,blockdim,l_mpi_enreg,&
-&                     l_prtvol,l_sij_opt,l_cpopt,cprj_dum,already_transposed=.false.)
-  end if
-
-  ! scale cg, ghc, gsc
-  if ( l_istwf == 2 ) then
-    !cg(:,1:spacedim*blockdim) = cg(:,1:spacedim*blockdim) * sqrt2
-    !ghc(:,1:spacedim*blockdim) = ghc(:,1:spacedim*blockdim) * sqrt2
-    call xgBlock_scale(X,sqrt2,1)
-    call xgBlock_scale(AX,sqrt2,1)
-    if(l_mpi_enreg%me_g0 == 1) then 
-      cg(:, 1:spacedim*blockdim:l_npw) = cg(:, 1:spacedim*blockdim:l_npw) * inv_sqrt2
-      ghc(:, 1:spacedim*blockdim:l_npw) = ghc(:, 1:spacedim*blockdim:l_npw) * inv_sqrt2
-    endif
-    if(l_paw) then
-      !gsc(:,1:spacedim*blockdim) = gsc(:,1:spacedim*blockdim) * sqrt2
-      call xgBlock_scale(BX,sqrt2,1)
-      if(l_mpi_enreg%me_g0 == 1) gsc(:, 1:spacedim*blockdim:l_npw) = gsc(:, 1:spacedim*blockdim:l_npw) * inv_sqrt2
-    end if
-  end if
-
-  if ( .not. l_paw ) call xgBlock_copy(X,BX)
-
-  !call xgBlock_set(AX,ghc,0,spacedim)
-  !call xgBlock_set(BX,gsc(:,1:blockdim*spacedim),0,spacedim)
- end subroutine getghc_gsc
-
- subroutine precond(W)
-   use m_xg, only : xg_t, xgBlock_colwiseMul
-
-!This section has been created automatically by the script Abilint (TD).
-!Do not modify the following lines by hand.
-#undef ABI_FUNC
-#define ABI_FUNC 'precond'
-!End of the abilint section
-
-   type(xgBlock_t), intent(inout) :: W
-   integer :: ispinor
-   !integer :: cplx
-
-   ! precondition resid_vec
-   do ispinor = 1,l_nspinor
-     !do cplx = 1, l_icplx
-     call xgBlock_colwiseMul(W,l_pcon,l_icplx*l_npw*(ispinor-1))
-      !end do
-   end do
-
- end subroutine precond
 
  end module m_lobpcgwf
 
