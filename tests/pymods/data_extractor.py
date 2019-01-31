d5dffd1f4630930e55faa45d0fcf8f3aab6e9e4e
Theo Cavignac <theo.cavignac@ecl16.ec-lyon.fr>
2019-01-31 14:51:07 +0100

    Erratum to 5cf0b248efdb278a4d8b81d2ccf0e69ebe7a017e

--- tests/pymods/data_extractor.py
+++ tests/pymods/data_extractor.py
@@ -4,11 +4,11 @@
 
 from __future__ import print_function, division, unicode_literals
 import re
-from yaml_tools import is_available as has_yaml
-from yaml_tools.abinit_metadata import ITERATOR_RANKS
+from .yaml_tools import is_available as has_yaml
+from .yaml_tools.abinit_metadata import ITERATOR_RANKS
 if has_yaml:
-    from yaml_tools.structures import IterStart
-    from yaml_tools import yaml_parse
+    from .yaml_tools.structures import IterStart
+    from .yaml_tools import yaml_parse
 else:
     IterStart = object
 
--- tests/pymods/yaml_tools/abinit_metadata.py
+++ tests/pymods/yaml_tools/abinit_metadata.py
@@ -1,3 +1,4 @@
+from __future__ import print_function, division, unicode_literals
 ITERATORS = [
     'idtset',
     'itimimage',
