bad0b20e1339a3e199febac7c35fe6600104ad6a
Theo Cavignac <theo.cavignac@ecl16.ec-lyon.fr>
2019-02-06 10:28:54 +0100

    Handle corrupted YAML documents.

--- tests/pymods/yaml_tools/__init__.py
+++ tests/pymods/yaml_tools/__init__.py
@@ -5,10 +5,25 @@ from __future__ import print_function, division, unicode_literals
 try:
     import yaml
 
-    yaml_parse = yaml.load
-    yaml_print = yaml.dump
-
     is_available = True
 
 except ImportError:
     is_available = False
+
+if is_available:
+
+    class CorruptedDocument(object):
+        def __init__(self, error):
+            self.error = self.error
+
+        def why(self):
+            return self.error.message
+
+    def yaml_parse(*args, **kwargs):
+        from . import structures
+        try:
+            return yaml.load(*args, **kwargs)
+        except Exception as e:
+            return CorruptedDocument(e)
+
+    yaml_print = yaml.dump
