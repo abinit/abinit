ea5c62f705c78cbce2af2147997ab8ce018d0f41
Theo Cavignac <theo.cavignac@ecl16.ec-lyon.fr>
2019-02-18 11:49:13 +0100

    Clean things up and add some infos in docstrings.

--- tests/pymods/yaml_tools/register_tag.py
+++ tests/pymods/yaml_tools/register_tag.py
@@ -7,6 +7,15 @@
 import re
 from inspect import ismethod
 
+
+def normalize_attr(string):
+    return '_'.join(re.findall(r'[a-zA-Z0-9_]+', string)) # .lower()
+
+
+def yaml_tag_mangle(Cls):
+    return '_' + Cls.__name__.lstrip('_') + '__yaml_tag'
+
+
 try:
     import yaml
     is_available = True
@@ -19,11 +28,10 @@ if is_available:
             Register a class as a known tag for YAML.
             The class must expose methods from_map and to_map.
             from_map must return a valid instance of Cls.
-            It can be a classmethod, a staticmethod or a normal
-            method but in the last case 'Cls()' must be a valid
-            initialisation.
+            It can be a classmethod or a normal method but in 
+            the last case 'Cls()' must be a valid initialisation.
         '''
-        tag = '!' + getattr(Cls, Cls.__name__ + '_yaml_tag', Cls.__name__)
+        tag = '!' + getattr(Cls, yaml_tag_mangle(Cls), Cls.__name__)
 
         def constructor(loader, node):
             map = dict(loader.construct_mapping(node, deep=True))
@@ -45,11 +53,10 @@ if is_available:
             Register a class as a known tag for YAML.
             The class must expose methods from_seq and to_seq.
             from_seq must return a valid instance of Cls.
-            It can be a classmethod, a staticmethod or a normal
-            method but in the last case 'Cls()' must be a valid
-            initialisation.
+            It can be a classmethod or a normal method but in 
+            the last case 'Cls()' must be a valid initialisation.
         '''
-        tag = '!' + getattr(Cls, Cls.__name__ + '_yaml_tag', Cls.__name__)
+        tag = '!' + getattr(Cls, yaml_tag_mangle(Cls), Cls.__name__)
 
         def constructor(loader, node):
             seq = list(loader.construct_sequence(node, deep=True))
@@ -71,11 +78,10 @@ if is_available:
             Register a class as a known tag for YAML.
             The class must expose methods from_scalar to_scalar.
             from_scalar must return a valid instance of Cls.
-            It can be a classmethod, a staticmethod or a normal
-            method but in the last case 'Cls()' must be a valid
-            initialisation.
+            It can be a classmethod or a normal method but in
+            the last case 'Cls()' must be a valid initialisation.
         '''
-        tag = '!' + getattr(Cls, Cls.__name__ + '_yaml_tag', Cls.__name__)
+        tag = '!' + getattr(Cls, yaml_tag_mangle(Cls), Cls.__name__)
 
         def constructor(loader, node):
             scalar = loader.construct_scalar(node)
@@ -95,21 +101,43 @@ if is_available:
     def auto_map(Cls):
         '''
             Automatically append methods from_map, to_map and __repr__ to a
-            class intended to be used with YAML tag
+            class intended to be used with YAML tag, as long as __getitem__
+            and __setitem__ to be accessed like a dictionnary. Attributes name
+            are normalized to be accessible as regular property even if the
+            original name contained spaces or special characters. The original
+            name can still be used in dict like access.
+            Example:
+                >>> @auto_map
+                ... class A(object):
+                ...     pass
+                ...
+                >>> a = A()
+                >>> a['attr w/ spaces'] = 78
+                >>> a.attr_w_spaces
+                78
+                >>> a.attr_w_spaces = 82
+                >>> a['attr w/ spaces']
+                82
+                >>> # be careful, simple normalization imply collisions
+                >>> a['attr .w. --spaces']
+                82
         '''
         class AutoMap(Cls):
             @classmethod
             def from_map(cls, d):
                 new = cls()
                 for attr in d:
-                    new.__dict__[attr] = d[attr]
+                    new[attr] = d[attr]
                 return new
 
             def to_map(self):
                 return self.__dict__
 
-            def __get_item__(self, key):
-                return self.__dict__[key]
+            def __getitem__(self, key):
+                return self.__dict__[normalize_attr(key)]
+
+            def __setitem__(self, key, val):
+                self.__dict__[normalize_attr(key)] = val
 
             def __repr__(self):
                 r = Cls.__name__ + '('
@@ -134,11 +162,13 @@ if is_available:
             Register a class as a known tag for YAML and a pattern
             wich imply this tag.
             The class must expose methods from_scalar to_scalar.
-            Moreover it must have a class attribute yaml_pattern matching
-            all recognised strings representing this kind of data.
+            Moreover it must have a class attribute yaml_pattern that 
+            can be either a string containing the regex pattern matching
+            all string representing this kind of data or the compiled
+            object of this same regex.
         '''
         yaml_scalar(Cls)  # register the constructor and the representer
-        tag = '!' + getattr(Cls, Cls.__name__ + '_yaml_tag', Cls.__name__)
+        tag = '!' + getattr(Cls, yaml_tag_mangle(Cls), Cls.__name__)
 
         re_pattern = Cls.yaml_pattern
         if not hasattr(re_pattern, 'match'):
