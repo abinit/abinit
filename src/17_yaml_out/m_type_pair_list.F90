83f2aab137c37a9095ed2eea6b1fab3789094b75
Theo Cavignac <theo.cavignac@ecl16.ec-lyon.fr>
2019-03-27 14:00:09 +0100

    Misc comments

--- src/17_yaml_out/m_type_pair_list.F90
+++ src/17_yaml_out/m_type_pair_list.F90
@@ -4,11 +4,11 @@
 
 module m_type_pair_list
   use iso_c_binding
-! type pair_list
-! Represent a list of key-value pairs
-! value can be either integerof double precision real
+! type c_pair_list
+! Represent a list of key-value pairs value can be either
+! integer, double precision real or string.
 ! Never manipulate member directly or you will corrupt
-! data. Use routnes of this module.
+! data. Use routines of m_pair_list instead
   type, bind(C) :: c_pair_list
     type(c_ptr) :: first = C_NULL_PTR
     type(c_ptr) :: cursor = C_NULL_PTR
