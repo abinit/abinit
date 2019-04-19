f14f544b375f6540e2fbfd6b534250a595cf4c22
Theo Cavignac <theo.cavignac@ecl16.ec-lyon.fr>
2019-04-19 14:27:42 +0200

    Add a switch to enable YAML output

--- src/57_iovars/m_outvar_o_z.F90
+++ src/57_iovars/m_outvar_o_z.F90
@@ -1209,6 +1209,9 @@ contains
  intarr(1,:)=dtsets(:)%use_slk
  call prttagm(dprarr,intarr,iout,jdtset_,2,marr,1,narrm,ncid,ndtset_alloc,'use_slk','INT',0, firstchar="-")
 
+ intarr(1,:)=dtsets(:)%use_yaml
+ call prttagm(dprarr,intarr,iout,jdtset_,2,marr,1,narrm,ncid,ndtset_alloc,'use_yaml','INT',0)
+
 
 !###########################################################
 !### 03. Print all the input variables (V)
