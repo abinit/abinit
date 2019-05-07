d8a3e47190c80d89253d0bbfbca52e9b355df8d3
Olivier Gingras <gingras1@ip19.m>
2019-05-07 13:37:40 -0400

    13th try. Bob still unpleased. Multiline was put on a single line (brute force).

--- src/68_dmft/m_forctqmc.F90
+++ src/68_dmft/m_forctqmc.F90
@@ -1641,8 +1641,7 @@ subroutine qmc_prep_ctqmc(cryst_struc,green,self,hu,paw_dmft,pawang,pawprtvol,we
             MSG_ERROR(message)
 #else
 #ifndef HAVE_MPI
-            write(message,'(2a)') ch10,' MPI requiered! For some reasons, MPI is necessitate (actually for some reasons, mpich'//
-                + ' and not openmpi) to communicate with TRIQS.'
+            write(message,'(2a)') ch10,' MPI requiered! For some reasons, MPI is necessitated (mpich) to communicate with TRIQS.'
             call wrtout(std_out,message,'COLL')
             MSG_ERROR(message)
 #else
