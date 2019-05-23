8b0dd2a39b9be34a8d30ab4c85accc1cef8fb2b4
Yann Pouillon <devops@materialsevolution.es>
2019-05-23 23:22:56 +0200

    Removed stale references to removed compilers and test farm bots

--- src/64_psp/m_psp1.F90
+++ src/64_psp/m_psp1.F90
@@ -1030,9 +1030,6 @@ subroutine sincos(iq,irmax,mmax,pspwk,rad,tpiq)
 !  fortunately, on the SGI - R10000 the normal computation is fast enough.
 
    testmipspro=.false.
-#ifdef FC_MIPSPRO
-   testmipspro=.true.
-#endif
    nstep=40
    if(iq-(iq/nstep)*nstep == 0 .or. testmipspro)then
 
