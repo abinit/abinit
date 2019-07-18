dcb03d57aae267089428f84f0414423062d2190f
Bogdan Sataric <bogdan.sataric@uvsq.fr>
2019-07-18 15:33:56 +0200

    Trying static on frontale
    Former-commit-id: 7cf1e62c6d94fff2f0971cbf3f729fea3feb1092

--- tests/tutorial/Input/Work5A/run_4.sh
+++ tests/tutorial/Input/Work5A/run_4.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 (cd ../../../../tmp/; make mj4)
-export OMP_NUM_THREADS=3
+export OMP_NUM_THREADS=1
 #mpirun -n 4 time abinit < t03_4_x.files  > log 2> err
 mpirun -n 4 time abinit < t03_4_x.files > log 2> err  | tail -f log  | grep -e ETOT 
 
