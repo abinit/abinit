b910f6c794630f0b6b93f116733362d2fc7f9312
Miquel Royo <mroyo@icmab.es>
2019-10-30 13:41:11 +0100

    correct msize in anaddb to not interfere with previous functionalities

--- src/98_main/anaddb.F90
+++ src/98_main/anaddb.F90
@@ -200,11 +200,7 @@ program anaddb
  call ddb_hdr_free(ddb_hdr)
 
  mpert=natom+MPERT_MAX
-#ifdef MR_DEV
- msize=3*mpert*3*mpert; if (mtyp==3.or.mtyp==33) msize=msize*3*mpert
-#else
  msize=3*mpert*3*mpert; if (mtyp==3) msize=msize*3*mpert
-#endif
 
  ! Read the input file, and store the information in a long string of characters
  ! strlen from defs_basis module
