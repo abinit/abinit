ba3dd1065a77efb23b54f5ecc3380048f684d7aa
Jordan Bieder <jordan.bieder@uliege.be>
2020-02-07 13:23:31 +0100

    Fix condition

--- src/57_iovars/m_chkinp.F90
+++ src/57_iovars/m_chkinp.F90
@@ -2145,7 +2145,7 @@ subroutine chkinp(dtsets,iout,mpi_enregs,ndtset,ndtset_alloc,npsp,pspheads,comm)
        MSG_ERROR_NOSTOP(msg, ierr)
      end if
    end if
-   if (response==1 .and. (dt%kptopt/=2 .or. dt%kptopt/=3)) then
+   if (response==1 .and. (dt%kptopt/=2 .and. dt%kptopt/=3)) then
        write(msg,'(a,i3,4a)' )&
 &        'The input variable optdriver=',dt%optdriver,ch10,&
 &        'which implies response functions, and requires kptopt=2 or 3.',&
