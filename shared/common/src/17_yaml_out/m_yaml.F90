5fb4a9818978157cd0137dc5ed78ff607f543b30
gmatteo <gmatteo@gmail.com>
2020-02-18 01:19:48 +0100

    minor changes

--- shared/common/src/17_yaml_out/m_yaml.F90
+++ shared/common/src/17_yaml_out/m_yaml.F90
@@ -87,8 +87,9 @@ module m_yaml
 
  contains
 
-     procedure :: write_and_free => yamldoc_write_unit_and_free !, yamldoc_write_units_and_free
-                                                                ! ifort13-16 does not generic interfaces
+   procedure :: write_and_free => yamldoc_write_unit_and_free
+    !, yamldoc_write_units_and_free ! ifort13-16 does not generic interfaces
+    !
     ! Write Yaml document to unit and free memory.
 
    procedure :: write_units_and_free => yamldoc_write_units_and_free
