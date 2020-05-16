a8b36a046008487ece7dbf0743c5bb74dccc0fca
gonze <xavier.gonze@uclouvain.be>
2020-05-16 06:26:44 +0200

    Change LDA+U in DFT+U in tests (Refs and input)

--- developers/maintainers/change.sh
+++ developers/maintainers/change.sh
@@ -9,7 +9,7 @@ for file in "$@"
 do
  echo "working on $file"
  rm -f tmp.file 
- sed -e 's!m_lda_to_qp!m_ks_to_qp!' $file > tmp.file
+ sed -e 's!LDA+U!DFT+U!' $file > tmp.file
  echo "changes done "
  # put the modified file at the correct place
  mv tmp.file $file
