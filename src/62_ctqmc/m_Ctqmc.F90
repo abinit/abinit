a568e8e3fd0aba2a39f302e9791d83f2bdce737c
Josef Zwanziger <jzwanzig@gmail.com>
2020-05-26 19:03:10 -0300

    fixed multiple if clause in m_Ctqmc.F90

--- src/62_ctqmc/m_Ctqmc.F90
+++ src/62_ctqmc/m_Ctqmc.F90
@@ -1647,9 +1647,12 @@ SUBROUTINE Ctqmc_loop(this,itotal,ilatex)
     
     IF ( MOD(isweep,measurements) .EQ. 0 ) THEN
       CALL ImpurityOperator_measDE(this%Impurity,this%measDE)
-      IF ( this%opt_spectra .GE. 1 .AND. MOD(isweep,measurements*this%opt_spectra) .EQ. 0 ) THEN
-        this%density(1:flavors,indDensity) = this%measN(3,1:flavors)
-        indDensity = indDensity+1
+      ! IF ( this%opt_spectra .GE. 1 .AND. MOD(isweep,measurements*this%opt_spectra) .EQ. 0 ) THEN
+      IF ( this%opt_spectra .GE. 1) then
+         IF ( MOD(isweep,measurements*this%opt_spectra) .EQ. 0) then
+            this%density(1:flavors,indDensity) = this%measN(3,1:flavors)
+            indDensity = indDensity+1
+         END IF
       END IF
     END IF
 
