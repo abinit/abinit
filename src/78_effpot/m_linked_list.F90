8ead555a3e51c972128e8f63d406986b8b7fadfe
Josef Zwanziger <jzwanzig@gmail.com>
2020-05-27 08:45:53 -0300

    fix if clause in m_linked_list.F90

--- src/78_effpot/m_linked_list.F90
+++ src/78_effpot/m_linked_list.F90
@@ -190,9 +190,14 @@ module m_linked_list
        do while(associated(self%iter))
           ! at the begining i<i0
           ! before the end,
-          if (i>self%iter%i .and. associated(self%iter%next) .and. i<self%iter%next%i) then
-             call llist_insert_after(self,self%iter,i,val)
-             return
+          ! if (i>self%iter%i .and. associated(self%iter%next) .and. i<self%iter%next%i) then
+          if ( i .GT. self%iter%i ) then
+             if ( associated(self%iter%next) ) then
+                if ( i .LT. self%iter%next%i ) then
+                   call llist_insert_after(self,self%iter,i,val)
+                   return
+                end if
+             end if
           else if(i==self%iter%i) then
              ! i<i0 or i>i
              if(mode==0) then
