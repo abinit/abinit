566500090204c673869e006566dec44881d6fff0
gonze <xavier.gonze@uclouvain.be>
2020-07-07 12:24:52 +0200

    Split too long line (error message usekden=1 needed for mGGA).

--- src/56_xc/m_rhotoxc.F90
+++ src/56_xc/m_rhotoxc.F90
@@ -401,7 +401,8 @@ subroutine rhotoxc(enxc,kxc,mpi_enreg,nfft,ngfft, &
  if (with_vxctau) with_vxctau=(size(vxctau)>0)
  if (usekden==1) then
    if (.not.present(taur)) then
-     message=' For mGGA functionals, kinetic energy density is needed. Set usekden to 1. Also use NC pseudopotentials without non-linear core correction.'
+     message=' For mGGA functionals, kinetic energy density is needed. Set input variable usekden to 1.' 
+     message=message//' Also use NC pseudopotentials without non-linear XC core correction.'
      MSG_BUG(message)
    else if (size(taur)/=nfft*nspden) then
      message=' Invalid size for taur!'
