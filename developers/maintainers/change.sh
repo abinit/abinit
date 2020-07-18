a1c56eac9df3e1624634fa53204f03a45cab513e
gonze <xavier.gonze@uclouvain.be>
2020-07-18 21:17:06 +0200

    Gather pseudopotentials from the Pseudodojo in Pseudodojo_paw_pbe_standard when appropriate

--- developers/maintainers/change.sh
+++ developers/maintainers/change.sh
@@ -9,7 +9,7 @@ for file in "$@"
 do
  echo "working on $file"
  rm -f tmp.file 
- sed -e 's!Pseudodojo_PAW_GGA!Pseudodojo_paw_pbe!' $file > tmp.file
+ sed -e 's!O.GGA_PBE-JTH.xml!Pseudodojo_paw_pbe_standard/O.xml!' $file > tmp.file
  echo "changes done "
  # put the modified file at the correct place
  mv tmp.file $file
