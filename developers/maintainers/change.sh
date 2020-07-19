570638cb411350d7b515fed8869e6009504d8b50
gonze <xavier.gonze@uclouvain.be>
2020-07-19 08:01:11 +0200

    Rename dir Psps_for_tests/HGH Psps_for_tests/PseudosHGH_pwteter

--- developers/maintainers/change.sh
+++ developers/maintainers/change.sh
@@ -9,7 +9,7 @@ for file in "$@"
 do
  echo "working on $file"
  rm -f tmp.file 
- sed -e 's!V.LDA_PW-JTH.xml!Pseudodojo_paw_pw_standard/V.xml!' $file > tmp.file
+ sed -e 's!HGH/!PseudosHGH_pwteter/!' $file > tmp.file
  echo "changes done "
  # put the modified file at the correct place
  mv tmp.file $file
