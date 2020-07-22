5416cb537278e53d1a4667579d1172224a95a9a7
gonze <xavier.gonze@uclouvain.be>
2020-07-22 09:07:16 +0200

    Upgraded input and refs files for *.pspnc

--- developers/maintainers/change.sh
+++ developers/maintainers/change.sh
@@ -9,7 +9,7 @@ for file in "$@"
 do
  echo "working on $file"
  rm -f tmp.file 
- sed -e 's!42mo.pspnc!PseudosTM_pwteter/42mo.pspnc!' $file > tmp.file
+ sed -e 's!9f.pspnc!PseudosTM_pwteter/9f.pspnc!' $file > tmp.file
  echo "changes done "
  # put the modified file at the correct place
  mv tmp.file $file
