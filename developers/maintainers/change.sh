89428393203d5c99d5aa04b404df78aeb8541b28
gonze <xavier.gonze@uclouvain.be>
2020-07-22 10:26:34 +0200

    mv GTH psps to separate dir. Also update some input and refs

--- developers/maintainers/change.sh
+++ developers/maintainers/change.sh
@@ -9,7 +9,7 @@ for file in "$@"
 do
  echo "working on $file"
  rm -f tmp.file 
- sed -e 's!9f.pspnc!PseudosTM_pwteter/9f.pspnc!' $file > tmp.file
+ sed -e 's!04be.pspgth!PseudosGTH_pwteter/04be.pspgth!' $file > tmp.file
  echo "changes done "
  # put the modified file at the correct place
  mv tmp.file $file
