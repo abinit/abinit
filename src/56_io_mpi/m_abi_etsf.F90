9ad9a47ad5bd65ce21a143950ed0430ea5eb9a62
beuken <jean-michel.beuken@uclouvain.be>
2020-11-13 12:42:03 +0100

    remove HAVE_ETSF_IO ( and codes ) + BigDFT return on alps ;-) **test**

--- src/56_io_mpi/m_abi_etsf.F90
+++ src/56_io_mpi/m_abi_etsf.F90
@@ -32,9 +32,6 @@ module m_abi_etsf
  use m_atomdata
  use m_nctk
  use iso_c_binding
-#ifdef HAVE_ETSF_IO
- use etsf_io
-#endif
 
  use m_fstrings,     only : endswith
  use defs_datatypes, only : pseudopotential_type
@@ -43,222 +40,10 @@ module m_abi_etsf
 
  private
 
-#ifdef HAVE_ETSF_IO
- public :: abi_etsf_dims_init
-#endif
  public :: abi_etsf_init
 
 CONTAINS  !===========================================================
 !!***
-
-!!****f* m_abi_etsf/etsf_dims_nullify
-!! NAME
-!! etsf_dims_nullify
-!!
-!! FUNCTION
-!!  Initialize the structure defining ETSF dimensions.
-!!
-!! INPUTS
-!!
-!! OUTPUT
-!!  dims=structure with ETSF dimensions.
-!!
-!! PARENTS
-!!
-!! CHILDREN
-!!
-!! SOURCE
-
-#ifdef HAVE_ETSF_IO
-
-subroutine etsf_dims_nullify(Dims,dimlen)
-
- implicit none
-
-!Arguments ------------------------------------
-!scalars
- integer,optional,intent(in) :: dimlen
- type(etsf_dims),intent(inout) :: Dims
-
-!Local variables-------------------------------
- integer :: my_dimlen
-
-! *************************************************************************
-
-  my_dimlen = 3; if (PRESENT(dimlen)) my_dimlen = dimlen
-
-  Dims%max_number_of_angular_momenta                 = etsf_no_dimension
-  Dims%max_number_of_basis_grid_points               = etsf_no_dimension
-  Dims%max_number_of_coefficients                    = etsf_no_dimension
-  Dims%max_number_of_projectors                      = etsf_no_dimension
-  Dims%max_number_of_states                          = etsf_no_dimension
-  Dims%number_of_atoms                               = etsf_no_dimension
-  Dims%number_of_atom_species                        = etsf_no_dimension
-  Dims%number_of_cartesian_directions                = my_dimlen
-  Dims%number_of_coefficients_dielectric_function    = etsf_no_dimension
-  Dims%number_of_components                          = etsf_no_dimension
-  Dims%number_of_frequencies_dielectric_function     = etsf_no_dimension
-  Dims%number_of_grid_points_vector1                 = etsf_no_dimension
-  Dims%number_of_grid_points_vector2                 = etsf_no_dimension
-  Dims%number_of_grid_points_vector3                 = etsf_no_dimension
-  Dims%number_of_kpoints                             = etsf_no_dimension
-  Dims%number_of_localization_regions                = etsf_no_dimension
-  Dims%number_of_qpoints_dielectric_function         = etsf_no_dimension
-  Dims%number_of_qpoints_gamma_limit                 = etsf_no_dimension
-  Dims%number_of_reduced_dimensions                  = my_dimlen
-  Dims%number_of_spinor_components                   = etsf_no_dimension
-  Dims%number_of_spins                               = etsf_no_dimension
-  Dims%number_of_symmetry_operations                 = etsf_no_dimension
-  Dims%number_of_vectors                             = my_dimlen
-  Dims%real_or_complex_coefficients                  = etsf_no_dimension
-  Dims%real_or_complex_density                       = etsf_no_dimension
-  Dims%real_or_complex_gw_corrections                = etsf_no_dimension
-  Dims%real_or_complex_potential                     = etsf_no_dimension
-  Dims%real_or_complex_wavefunctions                 = etsf_no_dimension
-  Dims%symbol_length                                 = etsf_chemlen
-
-  !Dimensions for variables that can be splitted.
-  Dims%my_max_number_of_coefficients  = etsf_no_dimension
-  Dims%my_max_number_of_states        = etsf_no_dimension
-  Dims%my_number_of_components        = etsf_no_dimension
-  Dims%my_number_of_grid_points_vect1 = etsf_no_dimension
-  Dims%my_number_of_grid_points_vect2 = etsf_no_dimension
-  Dims%my_number_of_grid_points_vect3 = etsf_no_dimension
-  Dims%my_number_of_kpoints           = etsf_no_dimension
-  Dims%my_number_of_spins             = etsf_no_dimension
-
-end subroutine etsf_dims_nullify
-!!***
-#endif
-
-!----------------------------------------------------------------------
-
-!!****f* m_abi_etsf/abi_etsf_dims_init
-!! NAME
-!! abi_etsf_dims_init
-!!
-!! FUNCTION
-!!  Initialize the structure defining ETSF dimensions.
-!!  starting from values stored in the dataset_type, the pseudopotential_type.
-!!  and the wave function handler for the BIGDFT part.
-!!
-!! INPUTS
-!!  dtset<type(dataset_type)>=all input variables for this dataset
-!!  psps<type(pseudopotential_type)>=variables related to pseudopotentials
-!!  wfs<wvl_wf_type>=Object to handle wave functions for the BIGDFT part
-!!    Presently not used, likely will be needed when ETSF-IO will be generalized to deal with PAW.
-!!  itype = an integer to define what to put in the output file. This can
-!!          be one of the following values (maybe a sum latter):
-!!          1 for a density file,
-!!          2 for a wavefunction file,
-!!          4 for a KSS file,
-!!          8 for the exchange potential,
-!!         16 for the correlation potential.
-!!
-!! OUTPUT
-!!  dims=structure with ETSF dimensions.
-!!
-!! PARENTS
-!!      m_abi_etsf
-!!
-!! CHILDREN
-!!
-!! SOURCE
-
-#ifdef HAVE_ETSF_IO
-
-subroutine abi_etsf_dims_init(dims, dtset, itype, psps, wfs)
-
- implicit none
-
-!Arguments ------------------------------------
-!scalars
- integer,intent(in) :: itype
- type(dataset_type),intent(in) :: dtset
- type(pseudopotential_type),intent(in) :: psps
- type(wvl_wf_type),intent(in) :: wfs
- type(etsf_dims),intent(inout) :: dims
-
-! *************************************************************************
-
-!Set-up the dimensions
-!=====================
- dims%max_number_of_angular_momenta = psps%mpsang
- dims%max_number_of_projectors      = 1
-
- dims%max_number_of_states   = dtset%mband
- dims%number_of_atoms        = dtset%natom
- dims%number_of_atom_species = dtset%ntypat
- dims%number_of_components   = dtset%nspden
-
- dims%number_of_kpoints              = dtset%nkpt
- dims%number_of_spinor_components    = dtset%nspinor
- dims%number_of_spins                = dtset%nsppol
- dims%number_of_symmetry_operations  = dtset%nsym
-
-!In the case of BigDFT, the number of coefficients are the number of wavelets.
- if (dtset%usewvl==0) then
-   dims%max_number_of_coefficients      = dtset%mpw
-   dims%max_number_of_basis_grid_points = etsf_no_dimension
- else
-#ifdef HAVE_BIGDFT
-   dims%max_number_of_coefficients      = wfs%ks%lzd%Glr%wfd%nvctr_c + 7 * wfs%ks%lzd%Glr%wfd%nvctr_f
-   dims%max_number_of_basis_grid_points = wfs%ks%lzd%Glr%wfd%nvctr_c
-#else
-   BIGDFT_NOTENABLED_ERROR()
-   if (.false.) write(std_out,*) wfs%ks
-#endif
- end if
-
- if (dtset%usepaw==1) then
-   dims%number_of_grid_points_vector1  = dtset%ngfftdg(1)
-   dims%number_of_grid_points_vector2  = dtset%ngfftdg(2)
-   dims%number_of_grid_points_vector3  = dtset%ngfftdg(3)
- else if (dtset%usewvl==1) then
-#ifdef HAVE_BIGDFT
-!In the case of BigDFT, the grid size is not defined by ngfft.
-   dims%number_of_grid_points_vector1  = wfs%ks%lzd%Glr%d%n1 * 2
-   dims%number_of_grid_points_vector2  = wfs%ks%lzd%Glr%d%n2 * 2
-   dims%number_of_grid_points_vector3  = wfs%ks%lzd%Glr%d%n3 * 2
-#endif
- else
-   dims%number_of_grid_points_vector1  = dtset%ngfft(1)
-   dims%number_of_grid_points_vector2  = dtset%ngfft(2)
-   dims%number_of_grid_points_vector3  = dtset%ngfft(3)
- end if
-
-!The density real_or_complex.
- dims%real_or_complex_density = etsf_no_dimension
- if (iand(itype, 1) /= 0 .or. dtset%usepaw==1) dims%real_or_complex_density = 1
-
-!The coefficient of wavefunctions real_or_complex.
- dims%real_or_complex_coefficients   = etsf_no_dimension
- if (iand(itype, 2) /= 0 .or. iand(itype, 4) /= 0) then
-   if (dtset%usewvl == 0) then
-     dims%real_or_complex_coefficients = 2 ! used in plane waves
-   else
-     dims%real_or_complex_coefficients = 1 ! used in wavelets
-   end if
- end if
-
-!The gw corrections real_or_complex.
-!Todo: Currently not exported.
-!if (.false. .and. iand(itype, 4) /= 0) then
- dims%real_or_complex_gw_corrections = etsf_no_dimension
- if (iand(itype, 4) /= 0) then
-   dims%real_or_complex_gw_corrections = 2 ! used in plane waves
- ! dims%real_or_complex_gw_corrections = 1 ! used in plane waves
- end if
-
-!The potential real_or_complex.
- dims%real_or_complex_potential = etsf_no_dimension
- if (iand(itype, 8) /= 0 .or. iand(itype, 16) /= 0) dims%real_or_complex_potential = 1
-
- dims%real_or_complex_wavefunctions = etsf_no_dimension
-
-end subroutine abi_etsf_dims_init
-!!***
-#endif
 
 !----------------------------------------------------------------------
 
@@ -312,105 +97,15 @@ subroutine abi_etsf_init(dtset,filapp,itype,kdep,lmn_size,psps,wfs)
  integer,intent(in) :: lmn_size(psps%npsp)
 
 !Local variables-------------------------------
-#ifdef HAVE_ETSF_IO
-!scalars
- integer :: ncid,var_main,usewvl
- logical :: ok
- character(len=80) :: file_title
- character(len=fnlen) :: filetsf
- type(etsf_dims) :: dims
- type(etsf_groups_flags) :: flags
- type(etsf_io_low_error) :: error
-#endif
 
 ! *************************************************************************
 
-#ifdef HAVE_ETSF_IO
-!Initialize the filename
- filetsf = nctk_ncify(filapp)
- call wrtout(std_out,"about to create file "//TRIM(filetsf),'COLL')
-
- usewvl = dtset%usewvl
-
-!Set-up the dimensions
-!=====================
- call abi_etsf_dims_init(dims,dtset,itype,psps,wfs)
-
-!Set-up the variables
-!====================
-!These mandatory values are always written by the hdr_io_etsf() routine.
- flags%geometry  = etsf_geometry_all
- flags%kpoints   = etsf_kpoints_red_coord_kpt + etsf_kpoints_kpoint_weights
- flags%electrons = etsf_electrons_all - etsf_electrons_x_functional - etsf_electrons_c_functional
- flags%basisdata = etsf_basisdata_basis_set
-
- if (usewvl==0) then
-   flags%basisdata = flags%basisdata + etsf_basisdata_kin_cutoff + etsf_basisdata_n_coeff
- end if
-
-!These variables may be written depending on prt<something> input variables.
- if (itype==1) then
-   flags%main = etsf_main_density
-   file_title = "Density file"
- else if (itype == 2) then
-   if (usewvl==0) then
-     flags%basisdata = flags%basisdata + etsf_basisdata_red_coord_pw
-   else
-     flags%basisdata = flags%basisdata + etsf_basisdata_coord_grid + etsf_basisdata_n_coeff_grid
-   end if
-   flags%main = etsf_main_wfs_coeff
-   file_title = "Wavefunctions file"
- else if (itype==4) then
-   if (usewvl==0) then
-     flags%basisdata = flags%basisdata + etsf_basisdata_red_coord_pw
-   else
-     flags%basisdata = flags%basisdata + etsf_basisdata_coord_grid + etsf_basisdata_n_coeff_grid
-   end if
-   flags%main   = etsf_main_wfs_coeff
-   flags%gwdata = etsf_gwdata_all
-   file_title = "KSS file"
- else if (itype==8) then
-   flags%main = etsf_main_pot_x_only
-   file_title = "Exchange potential file"
- else if (itype==16) then
-   flags%main = etsf_main_pot_c_only
-   file_title = "Correlation potential file"
- else if (itype==24) then
-   flags%main = etsf_main_pot_xc
-   file_title = "Exchange-correlation potential file"
- end if
-
-!Actually create the file
-!========================
-!If the group contains main, we remove it for a while to be sure to
-!add it at the end, after ABINIT private variables.
- var_main   = flags%main
- flags%main = etsf_main_none
-
- call etsf_io_data_init(filetsf, flags, dims, file_title, &
-& 'File generated by ABINIT with ETSF_IO', ok, error, overwrite = .true., k_dependent = kdep)
- ETSF_CHECK_ERROR(ok, error)
-
-!Add the private ABINIT information when required.
- NCF_CHECK(nctk_open_modify(ncid, filetsf, xmpi_comm_self))
-
-!Add the private data
- call ini_wf_etsf(ncid,usewvl,lmn_size,psps%npsp,psps%ntypat)
-
-! Add the main part as last variables in the ETSF file.
- call etsf_io_main_def(ncid, ok, error, flags = var_main)
- ETSF_CHECK_ERROR(ok, error)
-
-! Close the file.
- NCF_CHECK(nf90_close(ncid))
-#else
  ABI_UNUSED(dtset%ntypat)
  ABI_UNUSED(filapp)
  ABI_UNUSED(itype)
  ABI_UNUSED(kdep)
  ABI_UNUSED(lmn_size)
  if(.false. .and. present(wfs))write(std_out,*)' OK'
-#endif
 
 end subroutine abi_etsf_init
 !!***
@@ -452,62 +147,14 @@ subroutine ini_wf_etsf(ncid,usewvl,lmn_size,npsp,ntypat)
  integer,intent(in) :: lmn_size(npsp)
 
 !Local variables-------------------------------
-#ifdef HAVE_ETSF_IO
- integer :: ncerr
-#endif
 
 ! *************************************************************************
-
-#ifdef HAVE_ETSF_IO
-!Define dimensions.
- ncerr = nctk_def_dims(ncid, [nctkdim_t("npsp", npsp), nctkdim_t("codvsnlen", 8),nctkdim_t("psptitlen", 132)])
- NCF_CHECK(ncerr)
-
- if (usewvl==1) then ! Add the BigDFT private dimensions.
-   ncerr = nctk_def_dims(ncid, nctkdim_t("number_of_wavelet_resolutions", 2))
-   NCF_CHECK(ncerr)
- end if
-
-!Define scalars.
-!If variables already exist, it will check that definitions are coherent.
- ncerr = nctk_def_iscalars(ncid, [character(len=nctk_slen) :: &
-&   "date", "ixc", "intxc", "occopt", "pertcase", "headform", "fform", "usepaw", "usewvl"])
- NCF_CHECK(ncerr)
-
- ncerr = nctk_def_dpscalars(ncid, [character(len=nctk_slen) :: &
-&  "ecut_eff", "ecutdg", "ecutsm", "etot", "residm", "stmbias", "tphysel", "tsmear"])
- NCF_CHECK(ncerr)
-
-!Multi-dimensional variables.
- ncerr = nctk_def_arrays(ncid, [&
-   nctkarr_t("istwfk",  "i", "number_of_kpoints"),&
-   nctkarr_t("codvsn", "c", "codvsnlen"),&
-   nctkarr_t("pspcod",  "i", "npsp"),&
-   nctkarr_t("pspdat",  "i", "npsp"),&
-   nctkarr_t("pspso",  "i", "npsp"),&
-   nctkarr_t("pspxc",  "i", "npsp"),&
-   nctkarr_t("qptn",  "dp", "number_of_reduced_dimensions"),&
-   nctkarr_t("so_psp",  "i", "npsp"),&
-   nctkarr_t("symafm",  "i", "number_of_symmetry_operations"),&
-   nctkarr_t("title", "c", "psptitlen, npsp"),&
-   nctkarr_t("zionpsp",  "dp", "npsp"),&
-   nctkarr_t("znuclpsp",  "dp", "npsp"),&
-   nctkarr_t("lmn_size",  "i", "npsp")])
- NCF_CHECK(ncerr)
-
-!Add the BigDFT private variables.
- if (usewvl == 1) then
-   ncerr = nctk_def_arrays(ncid, nctkarr_t("number_of_wavelets", "i", "number_of_wavelet_resolutions"))
-   NCF_CHECK(ncerr)
- end if
-#else
 
 !if ETSF_IO is undefined, do nothing
  ABI_UNUSED(ncid)
  ABI_UNUSED(ntypat)
  ABI_UNUSED(lmn_size)
  ABI_UNUSED(usewvl)
-#endif
 
 end subroutine ini_wf_etsf
 !!***
--- src/62_wvl_wfs/m_wvl_rwwf.F90
+++ src/62_wvl_wfs/m_wvl_rwwf.F90
@@ -34,9 +34,6 @@ module m_wvl_rwwf
  use m_hdr
  use m_xmpi
  use m_dtset
-#if defined HAVE_ETSF_IO
-  use etsf_io
-#endif
 
  use defs_abitypes,  only : MPI_type
  use m_geometry,     only : xred2xcart
@@ -112,19 +109,6 @@ subroutine wvl_read(dtset, hdr0, hdr, mpi_enreg, option, rprimd, wff, wfs, wvl, 
   integer               :: comm,me
   real(dp), allocatable :: xcart(:,:), psifscf(:,:,:)
   real(dp), allocatable :: xcart_old(:,:)
-#if defined HAVE_ETSF_IO
-  integer               :: i, iCoeff, iCoarse, iFine
-  integer               :: n_old(3)
-  real(dp)              :: hgrid_old(3)
-  real(dp), allocatable :: psigold(:,:,:,:,:,:)
-  real(dp), allocatable, target :: wvl_band(:)
-  type(etsf_basisdata) :: basis_folder
-  type(etsf_main)      :: main_folder
-  type(etsf_electrons) :: electrons_folder
-  logical               :: reformat
-  logical              :: lstat
-  type(etsf_io_low_error) :: error
-#endif
 #endif
 
 ! *********************************************************************
@@ -163,127 +147,6 @@ subroutine wvl_read(dtset, hdr0, hdr, mpi_enreg, option, rprimd, wff, wfs, wvl, 
 &       wfs%ks%orbs%eval(iBand), psifscf)
      end do
      ABI_DEALLOCATE(psifscf)
-#if defined HAVE_ETSF_IO
-   else if (wff%iomode == IO_MODE_ETSF) then
-!    Read a NetCDF file.
-!    coordinates_of_grid_points is used to store the geometric
-!    position of coefficients of wavelets i, as integer in
-!    wvl%ni(:) dimensions.
-!    coefficients_of_wavefunctions is used to store the psi values for
-!    each wavelet.
-
-!    We check if we need reformating
-     if (abs(hdr%rprimd(1,1) / hdr%ngfft(1) - &
-&     hdr0%rprimd(1,1) / hdr0%ngfft(1)) < tol8 .and. &
-&     maxval(abs(hdr%nwvlarr - hdr0%nwvlarr)) == 0 .and. &
-&     maxval(abs(hdr%ngfft   - hdr0%ngfft  )) == 0 ) then
-       reformat = .false.
-       write(message, '(2a)' ) ch10,&
-&       ' wvl_read:  wavefunctions needs NO reformatting.'
-       call wrtout(std_out,message,'COLL')
-
-     else
-       reformat = .true.
-       write(message, '(2a)' ) ch10,&
-&       ' wvl_read:  wavefunctions needs reformatting.'
-       call wrtout(std_out,message,'COLL')
-
-!      Values for old system
-       call xred2xcart(dtset%natom, hdr0%rprimd, xcart_old, hdr0%xred)
-       displ = zero
-       do i = 1, dtset%natom, 1
-         displ = (xcart_old(1, i) - xcart(1, i)) ** 2 + &
-&         (xcart_old(2, i) - xcart(2, i)) ** 2 + &
-&         (xcart_old(3, i) - xcart(3, i)) ** 2
-       end do
-       displ = sqrt(displ)
-
-!      Do the reformating by expanding old wavefunctions on the grid
-!      and interpolate it on the new grid using BigDFT reformatonewave().
-       n_old = (hdr0%ngfft(1:3) - 31) / 2
-       hgrid_old(1) = hdr0%rprimd(1,1) / n_old(1)
-       hgrid_old(2) = hdr0%rprimd(2,2) / n_old(2)
-       hgrid_old(3) = hdr0%rprimd(3,3) / n_old(3)
-       ABI_ALLOCATE(psigold,(0:n_old(1), 2, 0:n_old(2), 2, 0:n_old(3), 2))
-       ABI_ALLOCATE(psifscf,(wvl%Glr%d%n1i, wvl%Glr%d%n2i, wvl%Glr%d%n3i))
-       ABI_ALLOCATE(basis_folder%coordinates_of_basis_grid_points%data2D,(3,hdr0%nwvlarr(1)))
-     end if
-
-!    We read the basis set definition
-     ABI_ALLOCATE(basis_folder%number_of_coefficients_per_grid_point%data1D,(hdr0%nwvlarr(1)))
-     call etsf_io_basisdata_get(wff%unwff, basis_folder, lstat, error)
-     ETSF_CHECK_ERROR(lstat, error)
-
-!    We allocate a temporary array to read the wavefunctions
-!    and reorder then into wfs%ks%psi
-     ABI_ALLOCATE(wvl_band,(sum(basis_folder%number_of_coefficients_per_grid_point%data1D)))
-!    We just read the file from disk to memory, band per band.
-     do iBand = 1, dtset%mband, 1
-       main_folder%wfs_coeff__state_access = iBand
-       main_folder%coefficients_of_wavefunctions%data1D => wvl_band
-       call etsf_io_main_get(wff%unwff, main_folder, lstat, error)
-       ETSF_CHECK_ERROR(lstat, error)
-
-!      Now we reorder
-       if (reformat) then
-!        Expand the wf on the old grid.
-         psigold = zero
-         iCoeff = 1
-         do i = 1, hdr0%nwvlarr(1), 1
-           coord = basis_folder%coordinates_of_basis_grid_points%data2D(:, i) / 2
-           psigold(coord(1), 1, coord(2), 1, coord(3), 1) = wvl_band(iCoeff)
-           iCoeff = iCoeff + 1
-           if (basis_folder%number_of_coefficients_per_grid_point%data1D(i) == 8) then
-             psigold(coord(1), 2, coord(2), 1, coord(3), 1) = wvl_band(iCoeff + 0)
-             psigold(coord(1), 1, coord(2), 2, coord(3), 1) = wvl_band(iCoeff + 1)
-             psigold(coord(1), 2, coord(2), 2, coord(3), 1) = wvl_band(iCoeff + 2)
-             psigold(coord(1), 1, coord(2), 1, coord(3), 2) = wvl_band(iCoeff + 3)
-             psigold(coord(1), 2, coord(2), 1, coord(3), 2) = wvl_band(iCoeff + 4)
-             psigold(coord(1), 1, coord(2), 2, coord(3), 2) = wvl_band(iCoeff + 5)
-             psigold(coord(1), 2, coord(2), 2, coord(3), 2) = wvl_band(iCoeff + 6)
-             iCoeff = iCoeff + 7
-           end if
-         end do
-
-         call reformatonewave(displ, wfs%ks%lzd%Glr%wfd, wvl%atoms, hgrid_old(1), &
-&         hgrid_old(2), hgrid_old(3), n_old(1), n_old(2), &
-&         n_old(3), xcart_old, psigold, &
-&         wvl%h(1), wvl%h(2), wvl%h(3), &
-&         wvl%Glr%d%n1, wvl%Glr%d%n2, &
-&         wvl%Glr%d%n3, xcart, psifscf, &
-&         wfs%ks%psi(bandSize * (iBand - me * wfs%ks%orbs%norbp - 1) + 1: &
-&         bandSize * (iBand - me * wfs%ks%orbs%norbp - 1) + bandSize))
-       else
-!        No reformating, just reorder
-         iCoeff = 1
-         iFine = bandSize * (iBand - me * wfs%ks%orbs%norbp - 1) + wfs%ks%lzd%Glr%wfd%nvctr_c + 1
-         do iCoarse = 1, hdr0%nwvlarr(1), 1
-           wfs%ks%psi(bandSize * (iBand - me * wfs%ks%orbs%norbp - 1) + iCoarse) = &
-&           wvl_band(iCoeff)
-           iCoeff  = iCoeff  + 1
-           if (basis_folder%number_of_coefficients_per_grid_point%data1D(iCoarse) == 8) then
-             wfs%ks%psi(iFine:iFine + 6) = wvl_band(iCoeff:iCoeff + 6)
-             iFine  = iFine  + 7
-             iCoeff = iCoeff + 7
-           end if
-         end do
-       end if
-     end do
-
-!    We read wfs%ks%eval (TODO maybe removed later).
-     electrons_folder%eigenvalues%data1D => wfs%ks%orbs%eval
-     call etsf_io_electrons_get(wff%unwff, electrons_folder, lstat, error)
-     ETSF_CHECK_ERROR(lstat, error)
-
-!    Deallocate temporary arrays
-     ABI_DEALLOCATE(wvl_band)
-     ABI_DEALLOCATE(basis_folder%number_of_coefficients_per_grid_point%data1D)
-     if (reformat) then
-       ABI_DEALLOCATE(basis_folder%coordinates_of_basis_grid_points%data2D)
-       ABI_DEALLOCATE(psigold)
-       ABI_DEALLOCATE(psifscf)
-     end if
-#endif
 
    else
      write(message,'(4a,i0,a)') ch10,&
@@ -368,19 +231,6 @@ subroutine wvl_write(dtset, eigen, mpi_enreg, option, rprimd, wff, wfs, wvl, xre
   integer               :: iorb
   integer               :: iseg, nseg, ipsi, npsi
   real(dp), allocatable :: xcart(:,:)
-#if defined HAVE_ETSF_IO
-  integer               :: i, i0, i1, i2, i3, jj, j0, j1, ii
-  integer               :: iGrid, iCoeff, iCoarse, iFine
-  integer               :: iband
-  integer, allocatable  :: coeff_map(:,:,:)
-  integer, allocatable, target :: wvl_coord(:,:), wvl_ncoeff(:)
-  real(dp), allocatable, target :: wvl_band(:)
-  type(etsf_basisdata) :: basis_folder
-  type(etsf_main)      :: main_folder
-  type(etsf_electrons) :: electrons_folder
-  logical              :: lstat
-  type(etsf_io_low_error) :: error
-#endif
 #endif
 
 ! *********************************************************************
@@ -425,113 +275,6 @@ subroutine wvl_write(dtset, eigen, mpi_enreg, option, rprimd, wff, wfs, wvl, xre
 &       npsi * (iorb - me * wfs%ks%orbs%norbp - 1) + npsi), &
 &       wfs%ks%orbs%eval(iorb))
      end do
-#if defined HAVE_ETSF_IO
-   else if (wff%iomode == IO_MODE_ETSF) then
-!    Write a NetCDF file.
-!    coordinates_of_grid_points is used to store the geometric
-!    position of coefficients of wavelets i, as integer in
-!    wvl%dpSize(:) dimensions.
-!    coefficients_of_wavefunctions is used to store the psi values for
-!    each wavelet.
-
-!    We write the basis set.
-!    =======================
-     ABI_ALLOCATE(wvl_coord,(3, wfs%ks%lzd%Glr%wfd%nvctr_c))
-     ABI_ALLOCATE(wvl_ncoeff,(wfs%ks%lzd%Glr%wfd%nvctr_c))
-!    Will store the grid index for a given geometric point
-     ABI_ALLOCATE(coeff_map,(0:wvl%Glr%d%n1,0:wvl%Glr%d%n2,0:wvl%Glr%d%n3))
-!    coarse part
-     iGrid = 0
-     coeff_map = 0
-     do iseg = 1, wfs%ks%lzd%Glr%wfd%nseg_c
-       jj = wfs%ks%lzd%Glr%wfd%keyvloc(iseg)
-       j0 = wfs%ks%lzd%Glr%wfd%keygloc(1, iseg)
-       j1 = wfs%ks%lzd%Glr%wfd%keygloc(2, iseg)
-       ii = j0 - 1
-       i3 = ii / ((wvl%Glr%d%n1 + 1) * &
-&       (wvl%Glr%d%n2 + 1))
-       ii = ii - i3 * (wvl%Glr%d%n1 + 1) * &
-&       (wvl%Glr%d%n2 + 1)
-       i2 = ii / (wvl%Glr%d%n1 + 1)
-       i0 = ii - i2 * (wvl%Glr%d%n1 + 1)
-       i1 = i0 + j1 - j0
-       do i = i0, i1
-         iGrid = iGrid + 1
-         coeff_map(i, i2, i3) = iGrid
-         wvl_coord(:, iGrid) = (/ i * 2, i2 * 2, i3 * 2 /)
-         wvl_ncoeff(iGrid) = 1
-       end do
-     end do
-!    fine part
-     do iseg = 1, wfs%ks%lzd%Glr%wfd%nseg_f
-       jj = wfs%ks%lzd%Glr%wfd%keyvloc(wfs%ks%lzd%Glr%wfd%nseg_c + iseg)
-       j0 = wfs%ks%lzd%Glr%wfd%keygloc(1, wfs%ks%lzd%Glr%wfd%nseg_c + iseg)
-       j1 = wfs%ks%lzd%Glr%wfd%keygloc(2, wfs%ks%lzd%Glr%wfd%nseg_c + iseg)
-       ii = j0 - 1
-       i3 = ii / ((wvl%Glr%d%n1 + 1) * &
-&       (wvl%Glr%d%n2 + 1))
-       ii = ii - i3 * (wvl%Glr%d%n1 + 1) * &
-&       (wvl%Glr%d%n2 + 1)
-       i2 = ii / (wvl%Glr%d%n1 + 1)
-       i0 = ii - i2 * (wvl%Glr%d%n1 + 1)
-       i1 = i0 + j1 - j0
-       do i = i0, i1
-         iGrid = coeff_map(i , i2 , i3)
-         wvl_ncoeff(iGrid) = wvl_ncoeff(iGrid) + 7
-       end do
-     end do
-     basis_folder%coordinates_of_basis_grid_points%data2D => wvl_coord
-     basis_folder%number_of_coefficients_per_grid_point%data1D => wvl_ncoeff
-
-     call etsf_io_basisdata_put(wff%unwff, basis_folder, lstat, error)
-
-     ETSF_CHECK_ERROR(lstat, error)
-
-!    We write the wavefuctions.
-!    ==========================
-!    We write the wavefunctions band per band.
-     ABI_ALLOCATE(wvl_band,(wfs%ks%lzd%Glr%wfd%nvctr_c + 7 * wfs%ks%lzd%Glr%wfd%nvctr_f))
-!    We reorder the wfs%ks%psi(:,iband) into wvl_band
-     do iband = 1, dtset%mband, 1
-!      iCoeff is the index of the coefficient we are writing in wvl_band
-       iCoeff  = 1
-!      iCoarse is the index of the coarse part in wfs%ks%psi (=iGrid)
-       iCoarse = 1
-!      iFine is the index of the fine part in wfs%ks%psi
-       iFine   = wfs%ks%lzd%Glr%wfd%nvctr_c + 1
-       npsi    = wfs%ks%lzd%Glr%wfd%nvctr_c + 7 * wfs%ks%lzd%Glr%wfd%nvctr_f
-       do iGrid = 1, wfs%ks%lzd%Glr%wfd%nvctr_c, 1
-         wvl_band(iCoeff) = &
-&         wfs%ks%psi((iband - me * wfs%ks%orbs%norbp - 1) * npsi + iCoarse)
-         iCoeff  = iCoeff + 1
-         iCoarse = iCoarse + 1
-         if (wvl_ncoeff(iGrid) == 8) then
-           wvl_band(iCoeff:iCoeff + 6) = &
-&           wfs%ks%psi((iband - me * wfs%ks%orbs%norbp - 1) * npsi + iFine: &
-&           (iband - me * wfs%ks%orbs%norbp - 1) * npsi + iFine + 6)
-           iCoeff = iCoeff + 7
-           iFine  = iFine + 7
-         end if
-       end do
-       main_folder%wfs_coeff__state_access = iband
-       main_folder%coefficients_of_wavefunctions%data1D => wvl_band
-       call etsf_io_main_put(wff%unwff, main_folder, lstat, error)
-       ETSF_CHECK_ERROR(lstat, error)
-     end do
-
-!    We write the electronic informations.
-!    =====================================
-     electrons_folder%eigenvalues%data1D => eigen
-     electrons_folder%eigenvalues__number_of_states = dtset%mband
-     call etsf_io_electrons_put(wff%unwff, electrons_folder, lstat, error)
-     ETSF_CHECK_ERROR(lstat,error)
-
-!    We deallocate all arrays
-     ABI_DEALLOCATE(wvl_band)
-     ABI_DEALLOCATE(wvl_coord)
-     ABI_DEALLOCATE(wvl_ncoeff)
-     ABI_DEALLOCATE(coeff_map)
-#endif
    else
      write(message,'(3a,i0,a)')&
 &     '  wff%iomode argument is wrong,', ch10, &
