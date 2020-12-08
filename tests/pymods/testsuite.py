8ff9a40bd7f56d623c23f7ab23bf9c6391858562
gmatteo <gmatteo@gmail.com>
2020-12-08 21:47:34 +0100

    Add python code to change filexts

--- tests/pymods/testsuite.py
+++ tests/pymods/testsuite.py
@@ -3197,6 +3197,57 @@ class AbinitTestSuite(object):
             assert need_cpp_vars is None, ("need_cpp_vars argument is not expected with test_list.")
             self.tests = tuple(test_list)
 
+    def git_rename(self):
+
+        def rename(test):
+            #print(test, type(test))
+            root, ext = os.path.splitext(test.inp_fname)
+            assert ext == ".in"
+            new = root + ".abi"
+            inbase = os.path.basename(root)
+            cmd = f"git mv {test.inp_fname} {new}"
+            import subprocess
+            #subprocess.run(cmd, shell=True, check=True)
+            #print("cmd", cmd)
+            #call(cmd)
+            #print("inp_fname", test.inp_fname)
+
+            # Rename ref files
+            old_new = []
+            for f in test.files_to_test:
+                root, ext = os.path.splitext(test.inp_fname)
+                if not f.name.endswith(".out"): continue
+                if f.name == inbase + ".out" or "MPI" in f.name:
+                    old_ref = os.path.join(test.ref_dir, f.name)
+                    new, ext = os.path.splitext(old_ref)
+                    new = new + ".abo"
+                    cmd = f"git mv {old_ref} {new}"
+                    #print(cmd)
+                    old_new.append((os.path.basename(old_ref), os.path.basename(new)))
+
+                else:
+                    continue
+                    #print("strange f.name", f.name)
+
+            # Change names in TEST_INFO section
+            if old_new:
+                with open(test.inp_fname, "rt") as fh:
+                    s = fh.read()
+                    for old, new in old_new:
+                        print("replacing`", old, "`with:`", new, "`in:", test.inp_fname)
+                        s = s.replace(old, new)
+                #print(s)
+                #with open(os.path.splitext(test.inp_fname), "wt") as fh:
+                #    fh.write(s)
+
+
+        for test in self:
+            if isinstance(test, ChainOfTests):
+                for t in test:
+                    rename(t)
+            else:
+                rename(test)
+
     def __str__(self):
         return "\n".join(str(t) for t in self.tests)
 
