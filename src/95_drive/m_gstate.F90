dd7c0283ac0767b9c2bdca14f7cbe18a674c8e87
cpaillard <charles.paillard@centralesupelec.fr>
2021-01-18 19:57:36 +0100

    Implemented occopt 9 option to describe population of thermalized electrons and holes with their own fermi levels in conduction and valence bands respectively. Used in prl 123, 087601 (2019) to mimick photo-excitation and discuss potential photo-induced phase transitions.

--- src/95_drive/m_gstate.F90
+++ src/95_drive/m_gstate.F90
@@ -2063,7 +2063,7 @@ subroutine clnup1(acell,dtset,eigen,fermie,fermih, fnameabo_dos,fnameabo_eig,fre
    call getnel(doccde,dtset%dosdeltae,eigen,entropy,fermie,fermih,&
 &   maxocc,dtset%mband,dtset%nband,nelect,dtset%nkpt,&
 &   dtset%nsppol,occ,dtset%occopt,option,dtset%tphysel,&
-&   dtset%tsmear,unitdos,dtset%wtk,1,dtset%nband(1))!CP: added 1, nband(0) to fit new definition of getnel; parameters only used if
+&   dtset%tsmear,unitdos,dtset%wtk,1,dtset%nband(1))!CP: added 1, nband(1) to fit new definition of getnel; parameters only used if
 ! occopt 9
    ! End CP modified
    ABI_DEALLOCATE(doccde)
