2fd70af2c048f712e580ab236237a76e3f6b0f9d
gonze <xavier.gonze@uclouvain.be>
2021-03-12 21:52:29 +0100

    Create tests v9 95-99 for inaccurate POSCAR files.

--- tests/v9/__init__.py
+++ tests/v9/__init__.py
@@ -85,4 +85,9 @@ inp_files = [
 # GS Coulomb cut-off
 "t90.abi", # checkG Coulomb cut-off, large tolerance a.t.m.
 "t91.abi", # occopt 9 tests on Si
+"t95.abi", # test treatment of inaccurate POSCAR file
+"t96.abi", # test treatment of inaccurate POSCAR file
+"t97.abi", # test treatment of inaccurate POSCAR file
+"t98.abi", # test treatment of inaccurate POSCAR file
+"t99.abi", # test treatment of inaccurate POSCAR file
 ]
