45c58275c02e64d542caedc354b724699916bde6
xavier.gonze <xavier.gonze@uclouvain.be>
2021-03-21 16:50:20 +0100

    Create tpsic_01, and mv tpsic[2] to tpsic[3]. Also create new tpsic[2], but it is not operational.

--- tests/tutoparal/__init__.py
+++ tests/tutoparal/__init__.py
@@ -57,7 +57,9 @@ inp_files = [
 #"tmoldyn_05.abi",    # Run is too long : more than 20 minutes on 64 procs
 #"tmoldyn_06.abi",
 #"tmoldyn_07.abi",    # Run is too long : more than 20 minutes on 2 procs.
-"tpsic_02.abi", 
+"tpsic_01.abi", 
+#"tpsic_02.abi",   # Still a problem with NaN presently
+"tpsic_03.abi", 
 "tucrpa_1.abi",
 "tucrpa_2.abi",
 #"tucrpa_3.abi",
