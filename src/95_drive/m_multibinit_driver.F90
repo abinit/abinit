af2f45e1052a944cc32c3d7aa22f56c39815d571
mschmitt <mrcsschmitt@ooglemail.com>
2021-09-10 14:40:21 +0200

    Implement new keyword build_penalty.
    Former-commit-id: 4f91e36fe075a1333cee5bdafe66a2a73b890830

--- src/95_drive/m_multibinit_driver.F90
+++ src/95_drive/m_multibinit_driver.F90
@@ -521,7 +521,7 @@ elec_eval = .FALSE.
     call wrtout(std_out,message,'COLL')
     call wrtout(ab_out,message,'COLL')
 
-    call opt_effpotbound(reference_effective_potential,inp%bound_rangePower,hist,inp%bound_EFS,inp%bound_factors,comm) 
+    call opt_effpotbound(reference_effective_potential,inp%bound_rangePower,hist,inp%bound_EFS,inp%bound_factors,inp%bound_penalty,comm) 
 
     end if
 
