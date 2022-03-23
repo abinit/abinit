eabd4462dd9d80c3536a27024c341cc8fa4fab31
Miquel Royo <mroyo@icmab.es>
2022-03-23 18:05:53 +0100

    Fix printing d3etot terms

--- src/72_response/m_dfptlw_pert.F90
+++ src/72_response/m_dfptlw_pert.F90
@@ -440,8 +440,8 @@ subroutine dfptlw_pert(atindx,cg,cg1,cg2,cplex,d3e_pert1,d3e_pert2,d3etot,d3etot
      write(msg,'(2(a,f18.8))') &
      '          d3etot_t4 = ',d3etot_t4(1,1),  ',',d3etot_t4(2,1)
    else if (n2dq==2) then
-     write(msg,'(2(2(a,f18.8)))') &
-     'd3etot_t4(dw shear) = ',d3etot_t4(1,1),  ',',d3etot_t4(2,1),&
+     write(msg,'(2(2(a,f18.8)a))') &
+     'd3etot_t4(dw shear) = ',d3etot_t4(1,1),  ',',d3etot_t4(2,1),ch10,&
      'd3etot_t4(up shear) = ',d3etot_t4(1,2),  ',',d3etot_t4(2,2)
    end if
    call wrtout(std_out,msg,'COLL')
@@ -450,8 +450,8 @@ subroutine dfptlw_pert(atindx,cg,cg1,cg2,cplex,d3e_pert1,d3e_pert2,d3etot,d3etot
      write(msg,'(2(a,f18.8))') &
      '          d3etot_t5 = ',d3etot_t5(1,1),  ',',d3etot_t5(2,1)
    else if (n1dq==2) then
-     write(msg,'(2(2(a,f18.8)))') &
-     'd3etot_t5(dw shear) = ',d3etot_t5(1,1),  ',',d3etot_t5(2,1),&
+     write(msg,'(2(2(a,f18.8)a))') &
+     'd3etot_t5(dw shear) = ',d3etot_t5(1,1),  ',',d3etot_t5(2,1),ch10,&
      'd3etot_t5(up shear) = ',d3etot_t5(1,2),  ',',d3etot_t5(2,2)
    end if
    call wrtout(std_out,msg,'COLL')
