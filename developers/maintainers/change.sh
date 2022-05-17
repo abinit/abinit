13f32b75646611b65b50bccc1f8c26984e289577
gonze <xavier.gonze@uclouvain.be>
2022-05-17 12:38:42 +0200

    Improve warning related to localization tensor and rfdir.
    Former-commit-id: 9279c72898bf727f34c3f3ff84ceb8b04f21bc02

--- developers/maintainers/change.sh
+++ developers/maintainers/change.sh
@@ -9,7 +9,7 @@ for file in "$@"
 do
  echo "working on $file"
  rm -f tmp.file 
- sed -e 's!changes in the dielectric susceptibility may be wrong.!changes in the dielectric susceptibility may be wrong, check input variables rfatpol and rfdir.!' $file > tmp.file
+ sed -e 's!and transformation to cartesian coordinates may be wrong.!and transformation to cartesian coordinates may be wrong. Check input variable rfdir.!' $file > tmp.file
  echo "changes done "
  # put the modified file at the correct place
  mv tmp.file $file
