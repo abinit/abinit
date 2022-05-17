e419856e8a72f124c2d120af48a0e12ff6776cc5
gonze <xavier.gonze@uclouvain.be>
2022-05-17 12:33:05 +0200

    Improve warning related to rfdir and rfatpol

--- src/95_drive/m_nonlinear.F90
+++ src/95_drive/m_nonlinear.F90
@@ -1211,7 +1211,7 @@ end if
        write(message,'(a,a,a,a,a,a)')ch10,&
 &       ' dfptnl_doutput: WARNING -',ch10,&
 &       '  matrix of third-order energies incomplete,',ch10,&
-&       '  changes in the dielectric susceptibility may be wrong.'
+&       '  changes in the dielectric susceptibility may be wrong, check input variables rfatpol and rfdir.'
        call wrtout(ab_out,message,'COLL')
        call wrtout(std_out,message,'COLL')
      end if
