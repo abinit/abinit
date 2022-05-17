4bea62ac6ce7e7cb947f44cd58b28d4956794656
gonze <xavier.gonze@uclouvain.be>
2022-05-18 00:30:10 +0200

    Furtehr (and last ?) update for rfdir
    Former-commit-id: 7af1dad6e6252959bc04f4bc584c830b6db2ad26

--- developers/maintainers/change.sh
+++ developers/maintainers/change.sh
@@ -9,7 +9,7 @@ for file in "$@"
 do
  echo "working on $file"
  rm -f tmp.file 
- sed -e 's!and transformation to cartesian coordinates may be wrong.!and transformation to cartesian coordinates may be wrong. Check input variable rfdir.!' $file > tmp.file
+ sed -e 's!          0       0       0!          1       1       1!' $file > tmp.file
  echo "changes done "
  # put the modified file at the correct place
  mv tmp.file $file
