0c3b76d9e011889a81f4b5ab6a7d628a576fc7fd
Louis Bastogne <louis.bastogne@uliege.be>
2022-06-22 12:31:27 +0200

    Resolve conflict 3
    Former-commit-id: fef6359794b2169347216f103dbf7ce9c11aed2b

--- tests/tutomultibinit/__init__.py
+++ tests/tutomultibinit/__init__.py
@@ -16,7 +16,7 @@ inp_files = [
     "tmulti5_1.abi",   # basic Multibinit spin dynamics run
     "tmulti5_2.abi",   # LaFeO3 MvT
     "tmulti5_3.abi",   # 1D AFM chain with DMI (spin canting example)
-    "tmulti6_1.abi",   # Fit BaHfO3 on 1 atom
-    "tmulti7_1.abi",   # Bounding BaHfO3 
-    "tmulti8_1.abi",   # MD
+    "tmulti_l_6_1.abi",   # Fit BaHfO3 on 1 atom
+    "tmulti_l_7_1.abi",   # Bounding BaHfO3 
+    "tmulti_l_8_1.abi",   # MD
 ]
