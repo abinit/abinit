4627ee27e6d26ea8b68b8396ef0195f750128b7f
Miquel Royo <mroyo@icmab.es>
2022-09-13 15:01:09 +0200

    cp only relevant components of vtrial1 depending on nspden on another part

--- src/72_response/m_dfpt_scfcv.F90
+++ src/72_response/m_dfpt_scfcv.F90
@@ -1186,14 +1186,22 @@ subroutine dfpt_scfcv(atindx,blkflg,cg,cgq,cg1,cg1_active,cplex,cprj,cprjq,cpus,
        !same problem as with density reconstruction, TODO proper fft parallelization...
          do ifft=1,nfftf
            vtrial1_mq(2*ifft-1,1)=+vtrial1(2*ifft-1,1)
-           vtrial1_mq(2*ifft-1,2)=+vtrial1(2*ifft-1,2)
            vtrial1_mq(2*ifft  ,1)=-vtrial1(2*ifft  ,1)
-           vtrial1_mq(2*ifft  ,2)=-vtrial1(2*ifft  ,2)
-           vtrial1_mq(2*ifft-1,3)= vtrial1(2*ifft  ,4) !Re[V^12]
-           vtrial1_mq(2*ifft  ,3)= vtrial1(2*ifft-1,4) !Im[V^12],see definition of v(:,4) cplex=2 case
-           vtrial1_mq(2*ifft  ,4)= vtrial1(2*ifft-1,3) !Re[V^21]=Re[V^12]
-           vtrial1_mq(2*ifft-1,4)= vtrial1(2*ifft  ,3) !Re[V^21]=Re[V^12]
          end do
+         if (nspden >= 2) then
+           do ifft=1,nfftf
+             vtrial1_mq(2*ifft-1,2)=+vtrial1(2*ifft-1,2)
+             vtrial1_mq(2*ifft  ,2)=-vtrial1(2*ifft  ,2)
+           end do
+         end if
+         if (nspden > 2) then
+           do ifft=1,nfftf
+             vtrial1_mq(2*ifft-1,3)= vtrial1(2*ifft  ,4) !Re[V^12]
+             vtrial1_mq(2*ifft  ,3)= vtrial1(2*ifft-1,4) !Im[V^12],see definition of v(:,4) cplex=2 case
+             vtrial1_mq(2*ifft  ,4)= vtrial1(2*ifft-1,3) !Re[V^21]=Re[V^12]
+             vtrial1_mq(2*ifft-1,4)= vtrial1(2*ifft  ,3) !Re[V^21]=Re[V^12]
+           end do
+         end if
        end if
        initialized=1
      end if
