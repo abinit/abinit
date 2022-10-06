75995a48d3edd55ca5ebf6c823ff44c40d631db8
Pierre Kestener <pierre.kestener@cea.fr>
2022-10-06 11:01:54 +0200

    Add Fortran wrapper for cudaDeviceSynchronize and cudaMemPrefetchAsync
    Former-commit-id: 8514288982190ae477c8b5bd82d1a70fe87ee10c

--- shared/common/src/17_gpu_toolbox/m_gpu_toolbox.F90
+++ shared/common/src/17_gpu_toolbox/m_gpu_toolbox.F90
@@ -22,8 +22,8 @@
 
 module m_gpu_toolbox
 
- use m_initcuda
- use m_gpu_detect
+  use m_initcuda
+  use m_gpu_detect
 
 ! MG: I had to comment this import to avoid the following error on buda2_gnu_8.5_cuda
 !
@@ -38,21 +38,38 @@ module m_gpu_toolbox
 ! use, intrinsic :: iso_c_binding
 !#endif
 
- implicit none
+  implicit none
 
-!Interfaces for C bindings --- To be completed
+  !Interfaces for C bindings --- To be completed
 #ifdef HAVE_FC_ISO_C_BINDING
 #if defined HAVE_GPU_CUDA
-!interface
-!  integer(C_INT) function cuda_func() bind(C)
-!    use, intrinsic :: iso_c_binding, only : C_INT,C_PTR
-!    type(C_PTR) :: ptr
-!  end function cuda_func
+
+  interface
+
+    !  integer(C_INT) function cuda_func() bind(C)
+    !    use iso_c_binding, only : C_INT,C_PTR
+    !    type(C_PTR) :: ptr
+    !  end function cuda_func
+
+    subroutine gpu_device_synchronize() bind(c, name='gpu_device_synchronize_cpp')
+      use, intrinsic :: iso_c_binding
+      implicit none
+    end subroutine gpu_device_synchronize
+
+    subroutine gpu_data_prefetch_async(dev_ptr, count) bind(c, name='gpu_data_prefetch_async_cpp')
+      use, intrinsic :: iso_c_binding
+      implicit none
+      type(c_ptr),            value :: dev_ptr
+      integer(kind=C_SIZE_T), value :: count
+    end subroutine gpu_data_prefetch_async
+
+  end interface
+
 #endif
 #endif
 
 contains
-!!***
+  !!***
 
 end module m_gpu_toolbox
 !!***
