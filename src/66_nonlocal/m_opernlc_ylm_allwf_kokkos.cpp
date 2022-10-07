e1b20a370f2dd89cc1cbe68fa219e1aaa3128452
Pierre Kestener <pierre.kestener@cea.fr>
2022-10-07 16:26:29 +0200

    introduce abi_common_kokkos.h

--- src/66_nonlocal/m_opernlc_ylm_allwf_kokkos.cpp
+++ src/66_nonlocal/m_opernlc_ylm_allwf_kokkos.cpp
@@ -1,47 +1,4 @@
-#if defined HAVE_CONFIG_H
-#include "config.h"
-#endif
-
-#include <Kokkos_Core.hpp>
-#include <typeinfo>
-
-#include <cstdio> // for printf
-#include <cstdlib> // for EXIT_SUCCESS
-#include <iostream>
-#include <cstdint>
-#include <cassert>
-
-// Use (void) to silence unused warnings.
-#define assertm(exp, msg) assert(((void)msg, exp))
-
-#ifndef STR
-#define STR(x) #x
-#endif
-
-#define MY_ASSERT(x)
-
-#ifndef ABI_CHECK
-#define ABI_CHECK(expr, msg) if (!(expr)) { printf("Abinit failed: (%s), in file %s, line %d : %s.\n", STR(expr), __FILE__, __LINE__, msg); abort(); }
-#endif
-
-using memory_space = Kokkos::DefaultExecutionSpace::memory_space;
-using memory_trait = Kokkos::MemoryTraits<Kokkos::Unmanaged>;
-
-//! device views types for array of doubles, unmanaged memory (allocated on fortran side)
-using AbiView_r64_1d = Kokkos::View<double*, memory_space, memory_trait >;
-using AbiView_r64_2d = Kokkos::View<double**, Kokkos::LayoutLeft,   memory_space, memory_trait >;
-using AbiView_r64_3d = Kokkos::View<double***, Kokkos::LayoutLeft,  memory_space, memory_trait >;
-using AbiView_r64_4d = Kokkos::View<double****, Kokkos::LayoutLeft, memory_space, memory_trait >;
-
-//! device views types for array of doubles, managed memory
-using AbiView_r64_3d_managed = Kokkos::View<double***, Kokkos::LayoutLeft,  memory_space >;
-
-
-//! device views types for array of integers, unmanaged memory (allocated on fortran side)
-using AbiView_i32_1d = Kokkos::View<int32_t*, memory_space, memory_trait >;
-using AbiView_i32_2d = Kokkos::View<int32_t**, Kokkos::LayoutLeft,   memory_space, memory_trait >;
-using AbiView_i32_3d = Kokkos::View<int32_t***, Kokkos::LayoutLeft,   memory_space, memory_trait >;
-
+#include <abi_common_kokkos.h>
 
 extern "C"
 void opernlc_ylm_allwf_kokkos_cpp(int32_t cplex, int32_t cplex_enl, int32_t cplex_fac,
