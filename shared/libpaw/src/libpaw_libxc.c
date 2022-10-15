d6fa13e5e57df6181500a29f0ce47cf8952239ce
Marc Torrent <marc.torrent@cea.fr>
2022-10-16 01:26:01 +0200

    Previous commit was not OK for libxc5. Corrected

--- shared/libpaw/src/libpaw_libxc.c
+++ shared/libpaw/src/libpaw_libxc.c
@@ -70,7 +70,13 @@ void libpaw_xc_get_family_constants(int *xc_cst_family_unknown,
  *xc_cst_family_oep      = XC_FAMILY_OEP;
  *xc_cst_family_hyb_gga  = XC_FAMILY_HYB_GGA;
  *xc_cst_family_hyb_mgga = XC_FAMILY_HYB_MGGA;
- *xc_cst_family_hyb_lda = XC_FAMILY_HYB_LDA;
+#if ( XC_MAJOR_VERSION > 5 )
+ /* ==== libXC v6.0 and later ==== */
+ *xc_cst_family_hyb_lda  = XC_FAMILY_HYB_LDA;
+#else
+ /* ==== Before libXC v6.0 ==== */
+ *xc_cst_family_hyb_lda  = -11;
+#endif
 }
 
 /* ===============================================================
@@ -296,10 +302,19 @@ void libpaw_xc_func_set_density_threshold(XC(func_type) *xc_func, double *dens_t
  */
 int libpaw_xc_func_is_hybrid_from_id(int func_id)
  {int family; family=xc_family_from_id(func_id, NULL, NULL);
+#if ( XC_MAJOR_VERSION > 5 )
+/* ==== libXC v6.0 and later ==== */
   if (family==XC_FAMILY_HYB_GGA || family==XC_FAMILY_HYB_MGGA || family==XC_FAMILY_HYB_LDA)
    {return 1;}
   else
    {return 0;}
+#else
+/* ==== Before libXC v6.0 ==== */
+  if (family==XC_FAMILY_HYB_GGA || family==XC_FAMILY_HYB_MGGA )
+   {return 1;}
+  else
+   {return 0;}
+#endif
  }
 
 #endif
