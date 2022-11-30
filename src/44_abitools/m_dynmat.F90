080c2f8c60dfaa328dbb9b5978b17b45a7329277
Miquel Royo <mroyo@icmab.es>
2022-11-30 15:40:49 +0100

    Imaginary damping parameter rfeta incorporated in the Sternheimer equation to allow dissipation
    Former-commit-id: a79c7ea01e40846e7c9ca11b090a20cdcd505b83

--- src/44_abitools/m_dynmat.F90
+++ src/44_abitools/m_dynmat.F90
@@ -1419,7 +1419,7 @@ end subroutine chneu9
 !!
 !! SOURCE
 
-subroutine d2sym3(blkflg,d2,indsym,mpert,natom,nsym,qpt,symq,symrec,symrel,timrev,zero_by_symm)
+subroutine d2sym3(blkflg,d2,indsym,mpert,natom,nsym,qpt,symq,symrec,symrel,timrev,zero_by_symm,eta)
 
 !Arguments -------------------------------
 !scalars
@@ -1430,6 +1430,7 @@ subroutine d2sym3(blkflg,d2,indsym,mpert,natom,nsym,qpt,symq,symrec,symrel,timre
  integer,intent(inout) :: blkflg(3,mpert,3,mpert)
  real(dp),intent(in) :: qpt(3)
  real(dp),intent(inout) :: d2(2,3,mpert,3,mpert)
+ real(dp),optional,intent(in) :: eta
 
 !Local variables -------------------------
 !scalars
@@ -1437,7 +1438,7 @@ subroutine d2sym3(blkflg,d2,indsym,mpert,natom,nsym,qpt,symq,symrec,symrel,timre
  logical :: qzero
  integer :: exch12,found,idir1,idir2,idisy1,idisy2,ipert1,ipert2
  integer :: ipesy1,ipesy2,isgn,isym,ithree,itirev,nblkflg_is_one,noccur,nsym_used,quit,quit1
- real(dp) :: arg1,arg2,im,norm,re,sumi,sumr,xi,xr
+ real(dp) :: arg1,arg2,eta_,im,norm,re,sumi,sumr,xi,xr
 !arrays
  integer,pointer :: sym1_(:,:,:),sym2_(:,:,:)
  real(dp),allocatable :: d2tmp1(:,:,:),d2tmp2(:,:,:),d2work(:,:,:,:,:)
@@ -1466,6 +1467,8 @@ subroutine d2sym3(blkflg,d2,indsym,mpert,natom,nsym,qpt,symq,symrec,symrel,timre
    end do
  end if
 
+ eta_=zero; if(present(eta)) eta_=eta
+
 !Exchange of perturbations
 
 !Consider two cases : either time-reversal symmetry
@@ -1477,7 +1480,8 @@ subroutine d2sym3(blkflg,d2,indsym,mpert,natom,nsym,qpt,symq,symrec,symrel,timre
      do idir1=1,3
 
 !      Since the matrix is hermitian, the diagonal elements are real
-       d2(2,idir1,ipert1,idir1,ipert1)=zero
+!      (unles dissipation is allowed at finite omega)
+       if (eta_<tol20) d2(2,idir1,ipert1,idir1,ipert1)=zero
 
 !      do ipert2=1,mpert See notes
        do ipert2=1,min(natom+2,mpert)
