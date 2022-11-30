56b79fe97dc8d2f1dc2bac239b24ff93452e3ce0
Marc Sarraute <marc.sarraute@eolen.com>
2022-11-30 11:41:08 +0100

    [ompgpu] Add OpenMP GPU offload implementation of fourwf

---
+++ shared/common/src/17_gpu_toolbox/gpu_fft.h
@@ -0,0 +1,10 @@
+#ifndef ABINIT_SHARED_COMMON_SRC_17_GPU_FFT_H
+#define ABINIT_SHARED_COMMON_SRC_17_GPU_FFT_H
+
+#include <cufft.h>
+#include <cuda_api_error_check.h>
+
+extern cufftHandle plan_fft;
+
+#endif // ABINIT_SHARED_COMMON_SRC_17_GPU_FFT_H
+
