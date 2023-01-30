deff62ccfae7ed825ffedcfd0d81313127952fb1
asasani <a.sasani@uliege.be>
2023-01-30 11:08:54 +0100

    Revert "sign of Forces for efield applocation on MB changed"
    This reverts commit 195d495a52ef964f709400b3cdfb475cd73d03e1 [formerly 6b608ea9d396469b9761596d5461911ded573f7b].
    
    
    Former-commit-id: 822061f54a160057297cd3e84f558d313c7ca660

--- src/78_effpot/m_effective_potential.F90
+++ src/78_effpot/m_effective_potential.F90
@@ -2585,7 +2585,7 @@ if (has_ext_filed)  then
   call xmpi_sum(fcart_part , comm, ierr)
 
   energy = energy + energy_part
-  fcart = fcart - fcart_part
+  fcart = fcart + fcart_part
 
     if(abs(energy_part) > tol10 .and. need_verbose )then
       write(msg, '(a,1ES24.16,a)' ) ' Energy from electric field is :',&
