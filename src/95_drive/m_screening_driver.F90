15bbaa9668a95079c1b93db762fe892b6cb8a76b
gmatteo <gmatteo@gmail.com>
2023-04-25 07:25:38 +0200

    update refs
    Former-commit-id: 7f3ce7cd4acae540dfd4acfe60aa8677de401f5a

--- src/95_drive/m_screening_driver.F90
+++ src/95_drive/m_screening_driver.F90
@@ -1005,8 +1005,8 @@ subroutine screening(acell,codvsn,Dtfil,Dtset,Pawang,Pawrad,Pawtab,Psps,rprim)
    ABI_FREE(zw)
 
 #ifdef __HAVE_GREENX
-!if (.False.) then
-if (dtset%gwr_ntau /= 0) then
+if (.False.) then
+!if (dtset%gwr_ntau /= 0) then
    call wrtout(std_out, sjoin("Using minimax mesh with ntau:", itoa(dtset%nfreqim)))
    gaps = ebands_get_gaps(ks_ebands, gap_err)
    ABI_CHECK(gap_err == 0, "gap_err")
