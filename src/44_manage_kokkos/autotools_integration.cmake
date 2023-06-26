ee2790676cc8361e25948dde894129ab7e93e1c0
Marc Sarraute <marc.sarraute@eolen.com>
2023-06-26 18:51:28 +0200

    TMP/BROKEN Tweak ABINIT buildsystem to use a dedicated CMake for 44_manage_kokkos
    Basically, CMake is a prerequisite of Kokkos as it is hardly feasible to
    handle its complicate toolchain without it.
    
    To adress this issue, we move all Kokkos C++ files and Fortran interface
    modules in one subdirectory/library of ABINIT, and tweak generated top
    Makefile.am so CMake is used instead of regular Makefile generated by
    makemake and configure.
    
    Doing this triggers the CMake step at "make" step, which is
    counter-intuitive for a regular user... It is a first step and
    at least it works. Also, no bother occurs if Kokkos isn't used at all.
    
    TODO:
    - CMake buildsystem is broken due to hard tweak on include paths for now
    - "make install" target is broken as well, further tweak is required
    - Need to check building outside of source directory

--- src/44_manage_kokkos/autotools_integration.cmake
+++ src/44_manage_kokkos/autotools_integration.cmake
@@ -1,6 +1,3 @@
-# using 3.18 to have nice lapack imported target
-cmake_minimum_required(VERSION 3.18)
-
 #
 # Allow find_package() to use <PackageName>_ROOT variables,
 # either cmake variable or environment variables
@@ -11,7 +8,7 @@ if(NOT CMAKE_VERSION VERSION_LESS 3.12)
 endif()
 
 project(manage_kokkos_sublib
-  LANGUAGES C CXX
+  LANGUAGES C CXX Fortran
   DESCRIPTION "manage_kokkos_sublib is a CMake project aiming to integrate Kokkos code within abinit autotools buildsystem.")
 
 #
@@ -21,32 +18,37 @@ find_package(Kokkos 3.6.01 REQUIRED)
 
 if(TARGET Kokkos::kokkos)
 
-# set default c++ standard according to Kokkos version
-# Kokkos >= 4.0.00 requires c++-17
-if (NOT "${CMAKE_CXX_STANDARD}")
-  if ( ${Kokkos_VERSION} VERSION_LESS 4.0.00)
-    set(CMAKE_CXX_STANDARD 14)
-  else()
-    set(CMAKE_CXX_STANDARD 17)
+  # set default c++ standard according to Kokkos version
+  # Kokkos >= 4.0.00 requires c++-17
+  if (NOT "${CMAKE_CXX_STANDARD}")
+    if ( ${Kokkos_VERSION} VERSION_LESS 4.0.00)
+      set(CMAKE_CXX_STANDARD 14)
+    else()
+      set(CMAKE_CXX_STANDARD 17)
+    endif()
   endif()
+
+  # kokkos_check is defined in KokkosConfigCommon.cmake
+  kokkos_check( DEVICES "OpenMP" RETURN_VALUE KOKKOS_DEVICE_ENABLE_OPENMP)
+  kokkos_check( DEVICES "Cuda" RETURN_VALUE KOKKOS_DEVICE_ENABLE_CUDA)
+  kokkos_check( DEVICES "HIP" RETURN_VALUE KOKKOS_DEVICE_ENABLE_HIP)
+
+  kokkos_check( TPLS "HWLOC" RETURN_VALUE Kokkos_TPLS_HWLOC_ENABLED)
+
+  #FIXME Temporary, how should we detect HIP over CUDA ? Grepping config.h maybe ?
+  set(ABINIT_ENABLE_GPU_CUDA YES)
+  if(ABINIT_ENABLE_GPU_CUDA)
+    set(ABINIT_KOKKOS_BACKEND "Cuda")
+    kokkos_check( OPTIONS CUDA_LAMBDA RETURN_VALUE Kokkos_CUDA_LAMBDA_ENABLED)
+    kokkos_check( OPTIONS CUDA_CONSTEXPR RETURN_VALUE Kokkos_CUDA_CONSTEXPR_ENABLED)
+    kokkos_check( OPTIONS CUDA_UVM RETURN_VALUE Kokkos_CUDA_UVM_ENABLED)
+  elseif(ABINIT_ENABLE_GPU_HIP)
+    set(ABINIT_KOKKOS_BACKEND "HIP")
+  elseif(KOKKOS_DEVICE_ENABLE_OPENMP)
+    set(ABINIT_KOKKOS_BACKEND "OpenMP")
+  endif()
+
 endif()
 
-# kokkos_check is defined in KokkosConfigCommon.cmake
-kokkos_check( DEVICES "OpenMP" RETURN_VALUE KOKKOS_DEVICE_ENABLE_OPENMP)
-kokkos_check( DEVICES "Cuda" RETURN_VALUE KOKKOS_DEVICE_ENABLE_CUDA)
-kokkos_check( DEVICES "HIP" RETURN_VALUE KOKKOS_DEVICE_ENABLE_HIP)
-
-kokkos_check( TPLS "HWLOC" RETURN_VALUE Kokkos_TPLS_HWLOC_ENABLED)
-
-if(ABINIT_ENABLE_GPU_CUDA)
-  set(ABINIT_KOKKOS_BACKEND "Cuda")
-  kokkos_check( OPTIONS CUDA_LAMBDA RETURN_VALUE Kokkos_CUDA_LAMBDA_ENABLED)
-  kokkos_check( OPTIONS CUDA_CONSTEXPR RETURN_VALUE Kokkos_CUDA_CONSTEXPR_ENABLED)
-  kokkos_check( OPTIONS CUDA_UVM RETURN_VALUE Kokkos_CUDA_UVM_ENABLED)
-elseif(ABINIT_ENABLE_GPU_HIP)
-  set(ABINIT_KOKKOS_BACKEND "HIP")
-elseif(KOKKOS_DEVICE_ENABLE_OPENMP)
-  set(ABINIT_KOKKOS_BACKEND "OpenMP")
-endif()
-
-message("[abinit / kokkos] Kokkos found via find_package; default backend is ${ABINIT_KOKKOS_BACKEND}")
+message("[44_manage_kokkos] Kokkos found via find_package; default backend is ${ABINIT_KOKKOS_BACKEND}")
+message("[44_manage_kokkos] C flags == ${CMAKE_C_FLAGS}")
