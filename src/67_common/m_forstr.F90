e0c9865e3780497f717f7af8150b290c544a81ab
Marc Sarraute <marc.sarraute@eolen.com>
2023-06-28 19:54:36 +0200

    Refactor gemm_nonlop_gpu in a separate file

--- src/67_common/m_forstr.F90
+++ src/67_common/m_forstr.F90
@@ -953,15 +953,19 @@ subroutine forstrnps(cg,cprj,ecut,ecutsm,effmass_free,eigen,electronpositron,foc
 !    Setup gemm_nonlop
      if (gemm_nonlop_use_gemm) then
        gemm_nonlop_ikpt_this_proc_being_treated = my_ikpt
-       if ( use_gpu_cuda /= 666) then
-         !FIXME signs==1 not handled in CUDA GEMM nonlop
-         if ( use_gpu_cuda /= 1) then
-           call make_gemm_nonlop(my_ikpt,gs_hamk%npw_fft_k,gs_hamk%lmnmax,gs_hamk%ntypat, &
-&            gs_hamk%indlmn, gs_hamk%nattyp, gs_hamk%istwf_k, gs_hamk%ucvol, &
-&            gs_hamk%ffnl_k, gs_hamk%ph3d_k, gs_hamk%kpt_k, gs_hamk%kg_k, gs_hamk%kpg_k, &
-&            use_gpu_cuda, &
-&            compute_grad_strain=(stress_needed>0),compute_grad_atom=(optfor>0))
-         end if
+       if ( use_gpu_cuda == 0) then
+         call make_gemm_nonlop(my_ikpt,gs_hamk%npw_fft_k,gs_hamk%lmnmax, &
+             gs_hamk%ntypat, gs_hamk%indlmn, gs_hamk%nattyp, gs_hamk%istwf_k, &
+             gs_hamk%ucvol,  gs_hamk%ffnl_k, gs_hamk%ph3d_k, gs_hamk%kpt_k, &
+             gs_hamk%kg_k, gs_hamk%kpg_k, &
+             compute_grad_strain=(stress_needed>0),compute_grad_atom=(optfor>0))
+       !!FIXME signs==1 not handled in CUDA GEMM nonlop
+       !else if ( use_gpu_cuda /= 1) then
+       !  call make_gemm_nonlop_ompgpu(my_ikpt,gs_hamk%npw_fft_k,gs_hamk%lmnmax, &
+       !      gs_hamk%ntypat, gs_hamk%indlmn, gs_hamk%nattyp, gs_hamk%istwf_k, &
+       !      gs_hamk%ucvol,  gs_hamk%ffnl_k, gs_hamk%ph3d_k, gs_hamk%kpt_k, &
+       !      gs_hamk%kg_k, gs_hamk%kpg_k, &
+       !      compute_grad_strain=(stress_needed>0),compute_grad_atom=(optfor>0))
        else if ( use_gpu_cuda == 666) then
          if(mpi_enreg%paral_kgb==0) then
            call ompgpu_load_hamilt_buffers(gs_hamk%kg_k,gs_hamk%kg_kp)
@@ -972,10 +976,11 @@ subroutine forstrnps(cg,cprj,ecut,ecutsm,effmass_free,eigen,electronpositron,foc
          else
            ABI_ERROR("istwfk > 2 is not handled with OpenMP GPU offload mode !")
          end if
-         call make_gemm_nonlop_ompgpu(my_ikpt,gs_hamk%npw_fft_k,gs_hamk%lmnmax,gs_hamk%ntypat, &
-&            gs_hamk%indlmn, gs_hamk%nattyp, gs_hamk%istwf_k, gs_hamk%ucvol, &
-&            gs_hamk%ffnl_k, gs_hamk%ph3d_k, gs_hamk%kpt_k, gs_hamk%kg_k, gs_hamk%kpg_k, &
-&            compute_grad_strain=(stress_needed>0),compute_grad_atom=(optfor>0))
+         call make_gemm_nonlop_ompgpu(my_ikpt,gs_hamk%npw_fft_k,gs_hamk%lmnmax, &
+             gs_hamk%ntypat, gs_hamk%indlmn, gs_hamk%nattyp, gs_hamk%istwf_k, &
+             gs_hamk%ucvol,  gs_hamk%ffnl_k, gs_hamk%ph3d_k, gs_hamk%kpt_k, &
+             gs_hamk%kg_k, gs_hamk%kpg_k, &
+             compute_grad_strain=(stress_needed>0),compute_grad_atom=(optfor>0))
        end if
      end if
 
