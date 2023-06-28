58a8a7a9d8667a0dc0aa5686d49be77b2facbf27
Marc Sarraute <marc.sarraute@eolen.com>
2023-06-28 19:54:36 +0200

    Refactor gemm_nonlop_gpu in a separate file
    Former-commit-id: d05a4edb9ce1ec6e14ab179df7ab058bfad49311

--- src/67_common/m_dft_energy.F90
+++ src/67_common/m_dft_energy.F90
@@ -28,6 +28,7 @@ module m_dft_energy
  use m_errors
  use m_xmpi
  use m_gemm_nonlop
+ use m_gemm_nonlop_gpu
  use m_xcdata
  use m_cgtools
  use m_dtset
@@ -672,9 +673,17 @@ subroutine energy(cg,compch_fft,constrained_dft,dtset,electronpositron,&
 !    Setup gemm_nonlop
      if (gemm_nonlop_use_gemm) then
        gemm_nonlop_ikpt_this_proc_being_treated = my_ikpt
-       call make_gemm_nonlop(my_ikpt,gs_hamk%npw_fft_k,gs_hamk%lmnmax, &
-&       gs_hamk%ntypat, gs_hamk%indlmn, gs_hamk%nattyp, gs_hamk%istwf_k, gs_hamk%ucvol, gs_hamk%ffnl_k,&
-&       gs_hamk%ph3d_k, gs_hamk%kpt_k, gs_hamk%kg_k, gs_hamk%kpg_k, dtset%use_gpu_cuda)
+       if(dtset%use_gpu_cuda==0) then
+         call make_gemm_nonlop(my_ikpt,gs_hamk%npw_fft_k,gs_hamk%lmnmax, &
+             gs_hamk%ntypat, gs_hamk%indlmn, gs_hamk%nattyp, gs_hamk%istwf_k, &
+             gs_hamk%ucvol, gs_hamk%ffnl_k, &
+             gs_hamk%ph3d_k, gs_hamk%kpt_k, gs_hamk%kg_k, gs_hamk%kpg_k)
+       else if(dtset%use_gpu_cuda==1) then
+         call make_gemm_nonlop_gpu(my_ikpt,gs_hamk%npw_fft_k,gs_hamk%lmnmax, &
+             gs_hamk%ntypat, gs_hamk%indlmn, gs_hamk%nattyp, gs_hamk%istwf_k, &
+             gs_hamk%ucvol, gs_hamk%ffnl_k, &
+             gs_hamk%ph3d_k, gs_hamk%kpt_k, gs_hamk%kg_k, gs_hamk%kpg_k)
+       end if
      end if
 
 #if defined HAVE_GPU_CUDA
