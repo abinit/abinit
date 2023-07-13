4eb9539de1bc21e61bd7d4dc4805a972bb94b743
Marc Torrent <marc.torrent.prive@club-internet.Fr>
2023-07-13 19:53:47 +0200

    usekden was not correctly detected in invars2

--- src/57_iovars/m_invars2.F90
+++ src/57_iovars/m_invars2.F90
@@ -234,7 +234,7 @@ subroutine invars2(bravais,dtset,iout,jdtset,lenstr,mband,msym,npsp,string,usepa
 !scalars
  integer,parameter :: master = 0
  integer :: bantot,berryopt,dmatsize,ndim,getocc,narr,nprocs
- integer :: iat,iatom,iband,ii,iimage,ikpt,intimage,ionmov,isppol,ixc_current
+ integer :: iat,iatom,iband,ii,iimage,ikpt,intimage,ionmov,isppol,ixc_here
  integer :: densfor_pred,ipsp,iscf,isiz,itypat,jj,kptopt,lpawu,marr,natom,natomcor,nband1,nberry
  integer :: niatcon,nimage,nkpt,nkpthf,npspalch,nqpt,nsp,nspinor,nsppol,nsym,ntypalch,ntypat,ntyppure
  integer :: occopt,occopt_tmp,response,sumnbl,tfband,tnband,tread,tread_alt,tread_dft,tread_fock,tread_key,tread_extrael
@@ -1814,7 +1814,7 @@ subroutine invars2(bravais,dtset,iout,jdtset,lenstr,mband,msym,npsp,string,usepa
      tread=1
    end if
  end if
- ixc_current=dtset%ixc
+ ixc_here=dtset%ixc
 
  call intagm(dprarr,intarr,jdtset,marr,1,string(1:lenstr),'ixcrot',tread,'INT')
  if(tread==1) dtset%ixcrot=intarr(1)
@@ -1825,38 +1825,39 @@ subroutine invars2(bravais,dtset,iout,jdtset,lenstr,mband,msym,npsp,string,usepa
  call intagm(dprarr,intarr,jdtset,marr,1,string(1:lenstr),'ixc_sigma',tread,'INT')
  if(tread==1)then
    dtset%ixc_sigma=intarr(1)
-   if( dtset%optdriver==RUNL_SIGMA .and. mod(dtset%gwcalctyp,10)==5)ixc_current=dtset%ixc_sigma
+   if (dtset%optdriver==RUNL_SIGMA.and.mod(dtset%gwcalctyp,10)==5) ixc_here=dtset%ixc_sigma
  end if
 
- ! Initialize xclevel and usefock
- call get_xclevel(ixc_current,dtset%xclevel,dtset%usefock)
+ ! Initialize some data related to ixc
+ call get_xclevel(ixc_here,dtset%xclevel,dtset%usefock)
+ if (ixc_here<0) then
+   call libxc_functionals_init(ixc_here,dtset%nspden,xc_functionals=xcfunc_tmp)
+ end if
 
  call intagm(dprarr,intarr,jdtset,marr,1,string(1:lenstr),'auxc_ixc',tread,'INT')
  if(tread==1) dtset%auxc_ixc=intarr(1)
  ! If the default value had been given, possibly switch on
  ! the auxc_ixc corresponding to ixc, if the latter is an hybrid
  if(dtset%auxc_ixc==0)then
-   call get_auxc_ixc(dtset%auxc_ixc,ixc_current)
+   call get_auxc_ixc(dtset%auxc_ixc,ixc_here)
  end if
 
  ! Now take care of the parameters for hybrid functionals
  if(dtset%usefock==1)then
 
-   if(ixc_current ==40 .or. ixc_current ==41 .or. ixc_current ==42)then
+   if(ixc_here ==40 .or. ixc_here ==41 .or. ixc_here ==42)then
      dtset%hyb_mixing_sr=zero
      dtset%hyb_range_dft=zero ; dtset%hyb_range_fock=zero
-     if(ixc_current==40)dtset%hyb_mixing=one
-     if(ixc_current==41)dtset%hyb_mixing=quarter
-     if(ixc_current==42)dtset%hyb_mixing=third
-   else if(ixc_current==-427)then
+     if(ixc_here==40)dtset%hyb_mixing=one
+     if(ixc_here==41)dtset%hyb_mixing=quarter
+     if(ixc_here==42)dtset%hyb_mixing=third
+   else if(ixc_here==-427)then
      ! Special case of HSE03
      dtset%hyb_mixing=zero  ; dtset%hyb_mixing_sr=quarter
      dtset%hyb_range_dft=0.15_dp*two**third  ; dtset%hyb_range_fock=0.15_dp*sqrt(half)
-   else if (ixc_current<0) then
-     call libxc_functionals_init(ixc_current,dtset%nspden,xc_functionals=xcfunc_tmp)
+   else if (ixc_here<0) then
      call libxc_functionals_get_hybridparams(hyb_mixing=dtset%hyb_mixing,hyb_mixing_sr=dtset%hyb_mixing_sr,&
        hyb_range=dtset%hyb_range_dft,xc_functionals=xcfunc_tmp)
-     call libxc_functionals_end(xc_functionals=xcfunc_tmp)
      dtset%hyb_range_fock=dtset%hyb_range_dft
    end if
 
@@ -1931,8 +1932,7 @@ subroutine invars2(bravais,dtset,iout,jdtset,lenstr,mband,msym,npsp,string,usepa
 &         dtset%densfor_pred/=0.and.abs(dtset%densfor_pred)/=5) then
    !In case of density mixing (iscf>=10) and correction of forces (densfor_pred/=0 and 5)
    !  we need Kxc. If it is not available, we switch to potential mixing (iscf<10)
-   if (dtset%ixc<0) call libxc_functionals_init(dtset%ixc,dtset%nspden,xc_functionals=xcfunc_tmp)
-   if (.not.has_kxc(dtset%ixc,xcfunc_tmp)) then
+   if (.not.has_kxc(ixc_here,xcfunc_tmp)) then
      call intagm(dprarr,intarr,jdtset,marr,1,string(1:lenstr),'optforces',tread,'INT')
      if (dtset%optforces/=0.or.intarr(1)/=0) then
        dtset%iscf=dtset%iscf-10
@@ -1940,7 +1940,6 @@ subroutine invars2(bravais,dtset,iout,jdtset,lenstr,mband,msym,npsp,string,usepa
        ABI_COMMENT(msg)
      end if
    end if
-   if (dtset%ixc<0) call libxc_functionals_end(xc_functionals=xcfunc_tmp)
  end if
 
  call intagm(dprarr,intarr,jdtset,marr,1,string(1:lenstr),'vdw_df_acutmin',tread,'DPR')
@@ -2725,7 +2724,8 @@ subroutine invars2(bravais,dtset,iout,jdtset,lenstr,mband,msym,npsp,string,usepa
  if(tread==1) then
    dtset%usekden=intarr(1)
  else
-   dtset%usekden=merge(1,0,libxc_functionals_ismgga().or.dtset%ixc==31.or.dtset%ixc==34.or.dtset%ixc==35)
+   if (ixc_here<0) dtset%usekden=merge(1,0,libxc_functionals_ismgga(xc_functionals=xcfunc_tmp))
+   if (ixc_here==31.or.ixc_here==34.or.ixc_here==35) dtset%usekden=1
  end if
  if (dtset%usekden == 1 .and. dtset%nimage == 1) dtset%prtkden = 1
 
@@ -3761,6 +3761,10 @@ subroutine invars2(bravais,dtset,iout,jdtset,lenstr,mband,msym,npsp,string,usepa
 #endif
    call xmpi_barrier(comm)
    ABI_ERROR_NODUMP("Aborting now")
+ end if
+
+ if (ixc_here<0) then
+   call libxc_functionals_end(xc_functionals=xcfunc_tmp)
  end if
 
  call timab(191,2,tsec)
