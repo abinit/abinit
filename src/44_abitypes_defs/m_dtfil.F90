a202860d3710ef9357dca293a0b91aa8cfe9593d
Marc Torrent <marc.torrent@cea.fr>
2023-08-17 20:36:14 +0200

    Corrections avec 2nd round on testfarm
    Former-commit-id: 4119d44f92673d98df223bbc25975af7e79ffe31

--- src/44_abitypes_defs/m_dtfil.F90
+++ src/44_abitypes_defs/m_dtfil.F90
@@ -654,8 +654,7 @@ subroutine dtfil_init(dtfil,dtset,filnam,filstat,idtset,jdtset_,mpi_enreg,ndtset
      stringfile='_KDEN'
    end if
    stringvar='kden'
-   call mkfilename(filnam,filkdensin,dtset%getkden,idtset,dtset%irdkden,jdtset_,ndtset,stringfile,stringvar,will_read, &
-                   getpath=dtset%getden_filepath)
+   call mkfilename(filnam,filkdensin,dtset%getkden,idtset,dtset%irdkden,jdtset_,ndtset,stringfile,stringvar,will_read)
    if(will_read==0)filkdensin=trim(filnam_ds(3))//'_KDEN'
    ireadkden=will_read
    if ((dtset%optdriver==RUNL_GSTATE.or.dtset%optdriver==RUNL_GWLS).and.dtset%iscf<0) ireadkden=1
@@ -676,8 +675,7 @@ subroutine dtfil_init(dtfil,dtset,filnam,filstat,idtset,jdtset_,mpi_enreg,ndtset
    stringfile='_PAWDEN'
  end if
  stringvar='pawden'
- call mkfilename(filnam,filpawdensin,dtset%getpawden,idtset,dtset%irdden,jdtset_,ndtset,stringfile,stringvar,will_read, &
-                 getpath=dtset%getden_filepath)
+ call mkfilename(filnam,filpawdensin,dtset%getpawden,idtset,dtset%irdden,jdtset_,ndtset,stringfile,stringvar,will_read)
  if(will_read==0)filpawdensin=trim(filnam_ds(3))//'_PAWDEN'
 
  ! According to get1den, build _DEN file name, referred as fildens1in
