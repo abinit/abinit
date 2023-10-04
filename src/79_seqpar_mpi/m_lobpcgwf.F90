1273b6405b3cd81905823d57e82c9688b3c77940
Marc Sarraute <marc.sarraute@eolen.com>
2023-10-04 21:30:38 +0200

    Make dummy zero-sized array a one-sized array (for CRAY)

--- src/79_seqpar_mpi/m_lobpcgwf.F90
+++ src/79_seqpar_mpi/m_lobpcgwf.F90
@@ -118,7 +118,7 @@ subroutine lobpcgwf2(cg,dtset,eig,enl_out,gs_hamk,kinpw,mpi_enreg,&
 
  ! Important things for NC
  integer,parameter :: choice=1, paw_opt=0, signs=1
- type(pawcprj_type) :: cprj_dum(gs_hamk%natom,0)
+ type(pawcprj_type) :: cprj_dum(gs_hamk%natom,1)
  integer :: iband, shift
  real(dp) :: gsc_dummy(0,0)
  real(dp), allocatable :: l_gvnlxc(:,:)
@@ -342,7 +342,7 @@ end subroutine lobpcgwf2
   type(xgBlock_t), intent(inout) :: BX
   integer         :: blockdim
   integer         :: spacedim
-  type(pawcprj_type) :: cprj_dum(l_gs_hamk%natom,0)
+  type(pawcprj_type) :: cprj_dum(l_gs_hamk%natom,1)
   double precision :: dum
   double precision, parameter :: inv_sqrt2 = 1/sqrt2
   double precision, pointer :: cg(:,:)
