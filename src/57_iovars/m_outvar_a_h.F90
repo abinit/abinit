34c4dc3cbe217c9a92d1a42b2b36cae9caceac47
Augustin Blanchet <blancheta@u-b-barber>
2023-11-13 16:01:21 +0100

    Preparing useextfpmd 5 bis

--- src/57_iovars/m_outvar_a_h.F90
+++ src/57_iovars/m_outvar_a_h.F90
@@ -1397,6 +1397,9 @@ subroutine outvar_a_h (choice,dmatpuflag,dtsets,iout,&
  intarr(1,:)=dtsets(:)%extfpmd_nbdbuf
  call prttagm(dprarr,intarr,iout,jdtset_,2,marr,1,narrm,ncid,ndtset_alloc,'extfpmd_nbdbuf','INT',0)
 
+ dprarr(1,:)=dtsets(:)%extfpmd_ecut
+ call prttagm(dprarr,intarr,iout,jdtset_,1,marr,1,narrm,ncid,ndtset_alloc,'extfpmd_ecut','ENE',0)
+
 !Special treatment of the default values for the hybrid functional parameters.
  do ii=1,4
    if(ii==1)dprarr(1,:)=dtsets(:)%hyb_mixing
