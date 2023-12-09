ee6a00a1a347d7a70971fe616fd57188dc4d2a22
trunk <trunk@abinit.org>
2023-12-09 09:26:16 +0100

    Try to fix 'CHARACTER expression will be truncated in assignment'

--- src/67_common/m_sigtk.F90
+++ src/67_common/m_sigtk.F90
@@ -670,7 +670,7 @@ subroutine sigtk_kpts_in_erange(dtset, cryst, ebands, psps, pawtab, prefix, comm
  end if
 
  ! Build new header with fine k-mesh (note kptrlatt_orig == kptrlatt)
- codvsn = ABINIT_VERSION
+ codvsn = trim(ABINIT_VERSION)
  call hdr_init_lowlvl(fine_hdr, fine_ebands, psps, pawtab, dummy_wvl, codvsn, pertcase0, &
    dtset%natom, dtset%nsym, dtset%nspden, dtset%ecut, dtset%pawecutdg, dtset%ecutsm, dtset%dilatmx, &
    dtset%intxc, dtset%ixc, dtset%stmbias, dtset%usewvl, dtset%pawcpxocc, dtset%pawspnorb, dtset%ngfft, dtset%ngfftdg, &
