a5b48cae3e54943985f45be051462d7187e15bbd
Marc Torrent <marc.torrent@cea.fr>
2023-12-21 10:33:15 +0100

    Another typo corrected

--- config.h.cmake
+++ config.h.cmake
@@ -367,7 +367,7 @@
 #cmakedefine HAVE_OPENMP_OFFLOAD @HAVE_OPENMP_OFFLOAD@
 
 /* Define to 1 if you want to activate support for OpenMP GPU offload. */
-#cmakedefine HAVE_OPENMP_OFFLOAD @HAVE_OPENMP_OFFLOAD_DATASTRUCTURE@
+#cmakedefine HAVE_OPENMP_OFFLOAD_DATASTRUCTURE @HAVE_OPENMP_OFFLOAD_DATASTRUCTURE@
 
 /* Set to 1 if OpenMP has a working implementation of COLLAPSE. */
 #cmakedefine HAVE_OMP_COLLAPSE @HAVE_OMP_COLLAPSE@
