aaf218ae9496a493aa452bf2c5858b5da2590185
gonze <xavier.gonze@uclouvain.be>
2024-03-22 06:00:14 +0100

    One more comment
    Former-commit-id: 4db1619a732172f265286c83564a268d398f8a09

--- tests/v10/__init__.py
+++ tests/v10/__init__.py
@@ -10,6 +10,6 @@ keywords = [
 
 #: List of input files
 inp_files = [
-    # Fake test t01.abi, to initiate the tests/v10 directory .
+    # Fake test t01.abi, to initiate the tests/v10 directory . SHOULD BE REPLACED BY THE TEST OF A REAL NEW DEVELOPMENT.
     "t01.abi" ,  # test post treatment of an increase of symmetry due to geometry optimization
 ]
