65ada9ca0e01805a49b1afe115ff397030bfa2d4
Marc Sarraute <marc.sarraute@eolen.com>
2024-04-04 23:26:04 +0200

    Support paw_opt==3 && choice==54,55 in opernld_ylm_allwf

--- src/66_nonlocal/m_gemm_nonlop.F90
+++ src/66_nonlocal/m_gemm_nonlop.F90
@@ -1864,8 +1864,8 @@ contains
 
     ! opernld
     if(signs==1) then
-      if(choice==2 .or. choice==3 .or. choice==23) then
-        call opernld_ylm_allwf(choice,cplex,cplex_fac,&
+      if(choice==2 .or. choice==3 .or. choice==23 .or. choice==54 .or. choice==55) then
+        call opernld_ylm_allwf(choice,cplex,cplex_fac,ddkk,&
         &       dprojections,vnl_dprojections,s_dprojections,d2projections,&
         &       enlk,enlout,projections,vnl_projections,s_projections,&
         &       ndat,nd2gxdt,ndgxdt,&
