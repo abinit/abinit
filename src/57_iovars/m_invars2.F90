4ecd603d3ce9bc3105b5fa6e104ff114902d6afb
Siyu Chen <siyu.noreply@gmail.com>
2024-05-03 17:41:24 +0100

    Fix typo

--- src/57_iovars/m_invars2.F90
+++ src/57_iovars/m_invars2.F90
@@ -1687,7 +1687,7 @@ subroutine invars2(bravais,dtset,iout,jdtset,lenstr,mband,msym,npsp,string,usepa
  if(tread==1) dtset%irddvdb = intarr(1)
 
  call intagm(dprarr,intarr,jdtset,marr,1,string(1:lenstr),'irddrhodb',tread,'INT')
- if(tread==1) dtset%irddvdb = intarr(1)
+ if(tread==1) dtset%irddrhodb = intarr(1)
 
  call intagm(dprarr,intarr,jdtset,marr,1,string(1:lenstr),'irdpawden',tread,'INT')
  if(tread==1) dtset%irdpawden=intarr(1)
