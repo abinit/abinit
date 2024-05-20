fb9e2e5ad472ff3a8a72134a330f9550f62e722b
Matthieu Verstraete <matthieu.jean.verstraete@gmail.com>
2024-05-20 16:41:53 +0200

    generators for rhombohedral settings were incorrect I think, using hexagonal matrices instead of correct Bilbao ones
    Former-commit-id: 7410329adc044c0bac302355af42521e8e98ebac

--- src/57_iovars/m_chkinp.F90
+++ src/57_iovars/m_chkinp.F90
@@ -1071,7 +1071,8 @@ subroutine chkinp(dtsets,iout,mpi_enregs,ndtset,ndtset_alloc,npsp,pspheads,comm)
    ! getxred
    if(dt%getxcart/=0)then
      cond_string(1)='getxcart' ; cond_values(1)=dt%getxcart
-     ! Make sure that dt%getxred is 0
+     ! Make sure that dt%getxred is 0. NB: this has already been checked elsewhere:
+     !  that only one of xred xcart xrandom getxred getxcart are set 
      call chkint_eq(1,1,cond_string,cond_values,ierr,'getxred',dt%getxred,1,[0],iout)
    end if
 
