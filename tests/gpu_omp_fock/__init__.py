ce95be5a48b091c10483af3b2fb878bf5a1155fc
Marc Sarraute <marc.sarraute@eolen.com>
2024-06-12 17:21:20 +0200

    Add first non-regression test for Fock operator on GPU
    Former-commit-id: cfb7d6237665094463549785d202c20b8ed7a181

---
+++ tests/gpu_omp_fock/__init__.py
@@ -0,0 +1,41 @@
+"""Global variables associated to the test suite."""
+
+#: List of CPP variables that should be defined in config.h in order to enable this suite.
+need_cpp_vars = [
+"HAVE_GPU",
+"HAVE_OPENMP_OFFLOAD",
+]
+
+#: List of keywords that are automatically added to all the tests of this suite. 
+keywords = [
+]
+
+#: This suite contains tests executed with different numbers of MPI processes.
+is_multi_parallel = True
+
+#: List of input files
+inp_files = [
+# Smoke tests on silicium, no forces nor stress (based on libxc#51)
+"t01.abi", # Basic test, reference
+"t02.abi", # Use GEMM nonlop
+"t03.abi", # Use GPU OpenMP
+# "HPC" Perf tests, no forces nor stress
+"t04.abi", # Basic test, reference
+"t05.abi", # Use GPU OpenMP
+# Smoke tests on silicium, with batching, no forces nor stress (based on libxc#51)
+"t06.abi", # Basic test, reference
+"t07.abi", # Use GEMM nonlop
+"t08.abi", # Use GPU OpenMP
+"t09.abi", # Use GPU OpenMP (NC), no batching
+"t10.abi", # Use GPU OpenMP (NC)
+# Smoke tests on silicium, with stress (based on libxc#51)
+"t11.abi", # Basic test, reference, PAW
+"t12.abi", # With batching
+"t13.abi", # With batching, Use GEMM nonlop
+"t14.abi", # With batching, Use GPU OpenMP
+# Smoke tests on methane, with stress&forces (based on libxc#71)
+"t15.abi", # Basic test, reference, PAW
+"t16.abi", # With batching
+"t17.abi", # With batching, Use GEMM nonlop
+"t18.abi", # With batching, Use GPU OpenMP
+]
