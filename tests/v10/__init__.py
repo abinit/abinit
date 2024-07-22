2d34ae2e33dc0b30f0d651ed954ca5aba4c06d02
Fabien Bruneval <fabien.bruneval@.cea.fr>
2024-07-22 10:43:11 +0200

    move new tests from v10 -> gwr
    Former-commit-id: 2cf52fc4a0e71129f0b9a756daa96e5b94a5212f

--- tests/v10/__init__.py
+++ tests/v10/__init__.py
@@ -13,7 +13,5 @@ inp_files = [
     # Fake test t01.abi, to initiate the tests/v10 directory . SHOULD BE REPLACED BY THE TEST OF A REAL NEW DEVELOPMENT.
     "t01.abi" ,  # test post treatment of an increase of symmetry due to geometry optimization
     "t02.abi" ,  # check Wyckoff positions of trigonal groups 143-167
-    "t21.abi" ,  # test Gamma-only HF with time-reversal symmetry (real wavefunctions)
-    "t22.abi" ,  # test Gamma-only HF without time-reversal symmetry (complex wavefunctions)
     "t40.abi" ,  # test orbmag calculation when using spatial symmetries for GS nuclear dipole 
 ]
