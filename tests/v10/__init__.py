d79c35eee9982b40dea1199aa5a35a6b8a378b32
Fabien Bruneval <fabien.bruneval@.cea.fr>
2024-07-22 10:27:36 +0200

    additional changes

--- tests/v10/__init__.py
+++ tests/v10/__init__.py
@@ -14,5 +14,6 @@ inp_files = [
     "t01.abi" ,  # test post treatment of an increase of symmetry due to geometry optimization
     "t02.abi" ,  # check Wyckoff positions of trigonal groups 143-167
     "t21.abi" ,  # test Gamma-only HF with time-reversal symmetry (real wavefunctions)
+    "t22.abi" ,  # test Gamma-only HF without time-reversal symmetry (complex wavefunctions)
     "t40.abi" ,  # test orbmag calculation when using spatial symmetries for GS nuclear dipole 
 ]
