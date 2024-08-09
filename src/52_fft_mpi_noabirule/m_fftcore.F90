6e4658b4a56a23f730d6182109cdca991aeae435
Matteo Giantomassi <gmatteo@gmail.com>
2024-08-09 13:57:16 +0200

    Improve docs of m_dvdb, fill gvals with -inf with kfilter=='none'
    Former-commit-id: e9acdb8bb3d9966bacc6604f0c7b6f39d23ea61f

--- src/52_fft_mpi_noabirule/m_fftcore.F90
+++ src/52_fft_mpi_noabirule/m_fftcore.F90
@@ -3942,8 +3942,7 @@ end subroutine indfftrisc
 !!
 !! SOURCE
 
-
-subroutine kpgsph(ecut,exchn2n3d,gmet,ikg,ikpt,istwf_k,kg,kpt,mkmem,mpi_enreg,mpw,npw)
+subroutine kpgsph(ecut, exchn2n3d, gmet, ikg, ikpt, istwf_k, kg, kpt, mkmem, mpi_enreg, mpw, npw)
 
 !Arguments ------------------------------------
 !scalars
@@ -4328,7 +4327,6 @@ end subroutine kpgsph
 
 subroutine kpgcount(ecut,exchn2n3d,gmet,istwfk,kpt,ngmax,ngmin,nkpt)
 
-
 !Arguments ------------------------------------
 !scalars
  integer,intent(in) :: exchn2n3d,nkpt
@@ -4422,7 +4420,8 @@ end subroutine kpgcount
 !!
 !! SOURCE
 
-subroutine get_kg(kpoint, istwf_k, ecut, gmet, npw_k, kg_k, kin_sorted)
+subroutine get_kg(kpoint, istwf_k, ecut, gmet, npw_k, kg_k, &
+                  kin_sorted) ! optional
 
 !Arguments ------------------------------------
 !scalars
@@ -4448,7 +4447,7 @@ subroutine get_kg(kpoint, istwf_k, ecut, gmet, npw_k, kg_k, kin_sorted)
  call initmpi_seq(MPI_enreg_seq)
 
  ! Calculate the number of G-vectors for this k-point.
- call kpgsph(ecut,exchn2n3d0, gmet, ikg0, 0, istwf_k, kg_dum, kpoint, 0, MPI_enreg_seq, 0, npw_k)
+ call kpgsph(ecut, exchn2n3d0, gmet, ikg0, 0, istwf_k, kg_dum, kpoint, 0, MPI_enreg_seq, 0, npw_k)
 
  ! Allocate and calculate the set of G-vectors.
  ABI_MALLOC(kg_k,(3,npw_k))
