895292085cc846c586ce10f37d22d0ab39205202
Marc Torrent <marc.torrent@cea.fr>
2024-10-20 00:19:41 +0200

    Correct syntax error in variables_abinit.py
    Former-commit-id: 4e08914be5083cdecfbf0c67495ab3f9f0f6fc0f

--- abimkdocs/variables_abinit.py
+++ abimkdocs/variables_abinit.py
@@ -11859,8 +11859,8 @@ Variable(
     topics=['SCFControl_expert'],
     dimensions="scalar",
     defaultval=ValueWithConditions({'[[wfoptalg]] == 1 or 11 ': 6, 'defaultval': 4}),
-    mnemonics="Number of LINE minimizations,"
-    commentdefault="4 for conjugate-gradient-based algorithm, 6 for spectrum-filtering-based algorithms"
+    mnemonics="Number of LINE minimizations",
+    commentdefault="4 for conjugate-gradient-based algorithm, 6 for spectrum-filtering-based algorithms",
     added_in_version="before_v9",
     text=r"""
 For conjugate-gradient based algorithms (conjugate gradient or LOBPCG):
