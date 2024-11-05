afe1480207a5b5f353de7a8da2908ad6efc227e1
Augustin Blanchet <augustin.blanchet@cea.fr>
2024-11-05 21:52:38 +0100

    Fixing another indeterminate form...
    Former-commit-id: f736ca78037828afb69f4b808238deb5acc5b597

--- shared/common/src/33_xc_lowlevel/m_xcpbe.F90
+++ shared/common/src/33_xc_lowlevel/m_xcpbe.F90
@@ -5518,7 +5518,7 @@ subroutine fec_kdt16(rho,grho,iflag,fc,v1c,v2c,einc,tsc,degauss)
  real(dp),parameter :: twothird=two/three
  real(dp) :: fc_lda,einc_lda,tsc_lda,vc_lda
  real(dp) :: Bc,dBcdrs,dBcdt
- real(dp) :: t,dtdn,tF
+ real(dp) :: t,dtdn,tF,tilde_ds1dqc,tilde_qc
  real(dp) :: kf,ks,rs,drsdn
  real(dp) :: qc,dqcdn,dqcdt
  real(dp) :: expe,af,dadf
@@ -5578,7 +5578,12 @@ subroutine fec_kdt16(rho,grho,iflag,fc,v1c,v2c,einc,tsc,degauss)
 ! fc_lda is the energy per electron
 !
  v1c = h0 + (ga/s1)*ds1da*dadf*(vc_lda - fc_lda) + (ga/s1)*ds1dqc*(dqcdn + dqcdt*dtdn)
- v2c = rho * (ga/s1) * ds1dqc * qc/grho
+!A. Blanchet: fixing indeterminate form by introducing tilde_qc and tilde_ds1dqc (when grho == 0)
+!v2c = rho * (ga/s1) * ds1dqc * qc/grho
+ tilde_qc = 1._dp/(2.d0*ks*rho) * sqrt(Bc) ! tilde_qc == qc /sqrt(grho)
+ tilde_ds1dqc = be(iflag)/ga*2._dp*tilde_qc*xy + be(iflag)/ga*qc*qc*dxy * 2._dp*af*tilde_qc ! tilde_ds1dqc = ds1dqc/sqrt(grho)
+ v2c = rho * (ga/s1) * tilde_ds1dqc * tilde_qc
+ write(0,*) v2c
 ! first version:
 !tsc = rho*(ga/s1) * ds1da * dadf * tsc_lda - rho*(ga/s1) * t*dqcdt
 !
