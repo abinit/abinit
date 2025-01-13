81153508969edc1a9b02a6b8ea513970cc1cb6b1
Josef Zwanziger <jzwanzig@dal.ca>
2025-01-13 18:01:06 -0400

    working on getgh1c_mgga
    Former-commit-id: 767826e6fafe539f47c76fd1cbf1e60953dd6be9

--- src/72_response/m_dfpt_vtowfk.F90
+++ src/72_response/m_dfpt_vtowfk.F90
@@ -565,7 +565,7 @@ subroutine dfpt_vtowfk(cg,cgq,cg1,cg1_active,cplex,cprj,cprjq,cprj1,&
        if( (ipert .EQ. natom+1) .AND. (ASSOCIATED(rf_hamkq%vxctaulocal)) ) then
          ABI_MALLOC(ghc_vxctau,(2,npw1_k*nspinor))
          ! ndat hard-coded as 1
-         call getgh1c_mGGA(cwave1,rf_hamkq%dkinpw_k,gs_hamkq%gbound_k,ghc_vxctau,gs_hamkq%gprimd,idir,gs_hamkq%istwf_k,&
+         call getgh1c_mGGA(cwave1,gs_hamkq%gbound_k,ghc_vxctau,gs_hamkq%gmet,gs_hamkq%gprimd,idir,gs_hamkq%istwf_k,&
               & gs_hamkq%kg_k,kinpw1,gs_hamkq%kpt_k,gs_hamkq%mgfft,mpi_enreg,nspinor,gs_hamkq%n4,gs_hamkq%n5,&
               & gs_hamkq%n6,1,gs_hamkq%ngfft,npw_k,gs_hamkq%nvloc,rf_hamkq%vxctaulocal,gs_hamkq%gpu_option)
 !        There is an additional factor of 4 with respect to the bare matrix element
