433d9a03de7c2e0d9e484ac2a4ad4bb1a461461b
Marc Sarraute <marc.sarraute@eolen.com>
2025-01-15 15:32:52 +0100

    Add a dedicated testsuite for nspinor==2 with OpenMP GPU
    Former-commit-id: 4abe19946978c0d195734d8af139848f05031c50

---
+++ tests/gpu_omp_2spinors/__init__.py
@@ -0,0 +1,19 @@
+"""Global variables associated to the test suite."""
+
+#: List of CPP variables that should be defined in config.h in order to enable this suite.
+need_cpp_vars = [
+"HAVE_GPU",
+"HAVE_OPENMP_OFFLOAD",
+]
+
+#: List of keywords that are automatically added to all the tests of this suite. 
+keywords = [
+]
+
+#: This suite contains tests executed with different numbers of MPI processes.
+is_multi_parallel = True
+
+#: List of input files
+inp_files = [
+"t01.abi", # PAW, GS, using CHEBFI and LOBPCG (AlP, nspinor==2, nspden==1)
+]
