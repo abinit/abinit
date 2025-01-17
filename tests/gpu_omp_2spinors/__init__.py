8ee398d6f04f569ae16e535e5ed1425cc03ee0e3
Marc Sarraute <marc.sarraute@eolen.com>
2025-01-17 18:22:32 +0100

    Add test checking nspden==4 with GPU in getghc and mkrho (vtowfk missing)

--- tests/gpu_omp_2spinors/__init__.py
+++ tests/gpu_omp_2spinors/__init__.py
@@ -17,4 +17,5 @@ is_multi_parallel = True
 inp_files = [
 "t01.abi", # PAW, GS, using CHEBFI and LOBPCG (AlP, nspinor==2, nspden==1)
 "t02.abi", # PAW, GS+DFPT, only DDK (AlP, nspinor==2, nspden==1)
+"t03.abi", # NC , GS+DFPT, DDK + phonons (Bi, nspinor=={1,2}, nspden==1)
 ]
