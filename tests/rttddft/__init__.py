00766789bfead59b000c30741463c9330e158eb5
Fabien BRIEUC <Fabien.BRIEUC@cea.fr>
2025-02-25 14:28:48 +0100

    New tests for TDEF

--- tests/rttddft/__init__.py
+++ tests/rttddft/__init__.py
@@ -6,11 +6,14 @@ need_cpp_vars = [
 
 #: List of keywords that are automatically added to all the tests of this suite.
 keywords = [
+"RTTDDFT",
 ]
 
 #: List of input files
 inp_files = [
 "t01.abi", # Tests ER and EMR propagator in PAW (Ni FCC nonmagnetic)
 "t02.abi", # Tests ER and EMR propagator in NC (Al FCC)
-"t03.abi", # Tests restart capabilities (ER propagator Al FCC NC)
+"t03.abi", # Tests restart capabilities (Al FCC NC)
+"t04.abi", # Tests Impulsional TD electric field (without induced potential vector)
+"t05.abi", # Tests Impulsional TD electric field (with induced potential vector)
 ]
