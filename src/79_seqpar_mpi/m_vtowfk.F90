223466a1cd40f4ff78c45b4e36b4d2081ac64b40
Marc Sarraute <marc.sarraute@eolen.com>
2025-02-26 09:46:11 +0100

    Set default gemm_nonlop_block_size to gpu_nl_splitsize
    Former-commit-id: bd13c32b9c7454bc69a6caaa637b79305d4cc04b

--- src/79_seqpar_mpi/m_vtowfk.F90
+++ src/79_seqpar_mpi/m_vtowfk.F90
@@ -800,7 +800,7 @@ subroutine vtowfk(cg,cgq,cprj,cpus,dphase_k,dtefield,dtfil,dtset,&
  ! recompute distribution as projectors arrays are bigger in this case
  if(optforces==1 .and. dtset%gpu_nl_distrib==1) then
    blksize_gemm_nonlop_tmp = gemm_nonlop_block_size; is_distrib_tmp = gemm_nonlop_is_distributed
-   gemm_nonlop_block_size = 0
+   gemm_nonlop_block_size = dtset%gpu_nl_splitsize
    call get_gemm_nonlop_ompgpu_blocksize(ikpt,gs_hamk,mpi_enreg%bandpp,npw_k,nband_k,&
    &                        dtset%nspinor,mpi_enreg%paral_kgb,mpi_enreg%nproc_band,&
    &                        optforces,0,-1,gs_hamk%gpu_option,&
