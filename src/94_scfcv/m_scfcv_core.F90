7d4de691a620b32d5a5bc3ee0b8787020f07eda3
Ioanna Lygatsika <ioanna-maria.lygatsika@cea.fr>
2025-03-17 10:17:47 +0100

    fix missing init destroy invovl for slice algo
    Former-commit-id: d7a4d9452a2e11c608079e10fd2c3cfb9228551c

--- src/94_scfcv/m_scfcv_core.F90
+++ src/94_scfcv/m_scfcv_core.F90
@@ -482,8 +482,9 @@ subroutine scfcv_core(atindx,atindx1,cg,cprj,cpus,dmatpawu,dtefield,dtfil,dtpawu
 !Fock: be sure that the pointer is initialized to Null.
  nullify(fock)
 
-!If Chebishev Filtering algo is used, init invovl routine structure
- if((dtset%wfoptalg == 1 .or. dtset%wfoptalg == 111) .and. psps%usepaw == 1 .and. dtset%cprj_in_memory==0) then
+!If Chebishev Filtering or Slicing algo is used, init invovl routine structure
+ if((dtset%wfoptalg == 1 .or. dtset%wfoptalg == 111 .or. dtset%wfoptalg == 112) .and. psps%usepaw == 1 &
+&   .and. dtset%cprj_in_memory==0) then
    call init_invovl(dtset%nkpt)
  end if
 
@@ -2072,7 +2073,8 @@ subroutine scfcv_core(atindx,atindx1,cg,cprj,cpus,dmatpawu,dtefield,dtfil,dtpawu
  ABI_FREE(rmm_diis_status)
  ABI_SFREE(nhatgr)
 
- if((dtset%wfoptalg == 1 .or. dtset%wfoptalg == 111)  .and. psps%usepaw == 1 .and. dtset%cprj_in_memory==0) then
+ if((dtset%wfoptalg == 1 .or. dtset%wfoptalg == 111 .or. dtset%wfoptalg == 112)  .and. psps%usepaw == 1 &
+&     .and. dtset%cprj_in_memory==0) then
    call destroy_invovl(dtset%nkpt,dtset%gpu_option)
  end if
 
