3d44873963dcd711f83b11cd372a61684ee06657
Matteo Giantomassi <gmatteo@gmail.com>
2025-05-03 17:59:11 +0200

    Add comments

--- cmake/try_compile/have_mpi_allocate_shared_cptr.F90
+++ cmake/try_compile/have_mpi_allocate_shared_cptr.F90
@@ -1,4 +1,4 @@
-program have_mpi3
+program have_mpi_allocate_shared_cptr
 
   use mpi
   use iso_c_binding
@@ -7,14 +7,18 @@ program have_mpi3
 
   integer :: win, disp_unit, ierr, comm
   type(c_ptr) :: baseptr
-  integer(kind=MPI_ADDRESS_KIND) :: my_size, address
+  integer(kind=MPI_ADDRESS_KIND) :: my_size
 
   call MPI_Init(ierr)
 
   my_size = 0; disp_unit = 1
-  call MPI_WIN_ALLOCATE_SHARED(my_size, disp_unit, MPI_INFO_NULL, comm, address, win, ierr)
+  ! Here we pass a c_ptr although the "standard" API defined in mpi module expects integer(MPI_ADDRESS_KIND).
+  ! For real world applications we need an API that supports C_PTR so that we can convert to a Fortran pointer.
+  ! This problem was fixed in mpi_f08 but Abinit is not ready for that.
+  ! See also https://github.com/pmodels/mpich/issues/2659
+  call MPI_WIN_ALLOCATE_SHARED(my_size, disp_unit, MPI_INFO_NULL, comm, baseptr, win, ierr)
   call MPI_WIN_SHARED_QUERY(win, 0, my_size, disp_unit, baseptr, ierr)
 
   call MPI_Finalize(ierr)
 
-end program have_mpi3
+end program have_mpi_allocate_shared_cptr
--- config/m4/arch-mpi.m4
+++ config/m4/arch-mpi.m4
@@ -819,10 +819,15 @@ AC_DEFUN([_ABI_MPI_CHECK_ALLOCATE_SHARED_PTR], [
 
        integer :: win, disp_unit, ierr, comm
        type(c_ptr) :: baseptr
-       integer(kind=MPI_ADDRESS_KIND) :: my_size, address
+       integer(kind=MPI_ADDRESS_KIND) :: my_size
+
+       ! Here we pass a c_ptr although the "standard" API defined in mpi module expects integer(MPI_ADDRESS_KIND).
+       ! For real world applications we need an API that supports C_PTR so that we can convert to a Fortran pointer.
+       ! This problem was fixed in mpi_f08 but Abinit is not ready for that.
+       ! See also https://github.com/pmodels/mpich/issues/2659
 
        my_size = 0; my_size =  disp_unit
-       call MPI_WIN_ALLOCATE_SHARED(my_size, disp_unit, MPI_INFO_NULL, comm, address, win, ierr)
+       call MPI_WIN_ALLOCATE_SHARED(my_size, disp_unit, MPI_INFO_NULL, comm, baseptr, win, ierr)
        call MPI_WIN_SHARED_QUERY(win, 0, my_size, disp_unit, baseptr, ierr)
     ]])], [abi_mpi_allocate_shared_ok="yes"], [abi_mpi_allocate_shared_ok="no"])
   AC_LANG_POP([Fortran])
