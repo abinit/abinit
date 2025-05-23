c7df431d26138bcee8387e5bf0f8c02367c5c294
Clémentine Barat <clementine.barat@hotmail.fr>
2025-05-23 11:00:45 +0200

    chkinp for chi0 based precon
    Former-commit-id: 7a3cff207ca3b125d53b4797faab3cd21ed2820d
    Former-commit-id: d6c5db146bd512d4aea1d9d0c44b317e4020cd61

--- src/57_iovars/m_chkinp.F90
+++ src/57_iovars/m_chkinp.F90
@@ -1855,10 +1855,10 @@ subroutine chkinp(dtsets, iout, mpi_enregs, ndtset, ndtset_alloc, npsp, pspheads
      cond_string(1)='iprcel' ; cond_values(1)=dt%iprcel
      call chkint_eq(1, 1, cond_string, cond_values, ierr, 'npfft', dt%npfft, 1, [1], iout)
      if (dt%iprcel == 202) then
-      !LDOS chi0-based preconditioning (iprcel=202) incompatible with PAW (TODO).
-      call chkint_eq(0, 1, cond_string, cond_values, ierr, 'usepaw', dt%usepaw, 1, [0], iout)
-      !LDOS chi0-based preconditioning (iprcel=202) needs a smooth smearing.
-      call chkint_eq(0, 1, cond_string, cond_values, ierr, 'occopt', dt%occopt, 5, [3, 4, 5, 6, 7], iout) 
+       !LDOS chi0-based preconditioning (iprcel=202) incompatible with PAW (TODO).
+       call chkint_eq(0, 1, cond_string, cond_values, ierr, 'usepaw', dt%usepaw, 1, [0], iout)
+       !LDOS chi0-based preconditioning (iprcel=202) needs a smooth smearing.
+       call chkint_eq(0, 1, cond_string, cond_values, ierr, 'occopt', dt%occopt, 5, [3, 4, 5, 6, 7], iout) 
      end if
    end if
 
