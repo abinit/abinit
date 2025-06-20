84ab50f51240c3e10568080e725bff47bff83656
Matteo Giantomassi <gmatteo@gmail.com>
2025-06-21 00:47:25 +0200

    code cleanup
    Former-commit-id: 445ffc247ca142b44202df0fe134b60808f28a33

--- tests/pymods/six.py
+++ tests/pymods/six.py
@@ -53,7 +53,7 @@ else:
         MAXSIZE = int((1 << 31) - 1)
     else:
         # It's possible to have sizeof(long) != sizeof(Py_ssize_t).
-        class X(object):
+        class X:
             def __len__(self):
                 return 1 << 31
         try:
@@ -78,7 +78,7 @@ def _import_module(name):
     return sys.modules[name]
 
 
-class _LazyDescr(object):
+class _LazyDescr:
 
     def __init__(self, name):
         self.name = name
@@ -152,7 +152,7 @@ class MovedAttribute(_LazyDescr):
         return getattr(module, self.attr)
 
 
-class _SixMetaPathImporter(object):
+class _SixMetaPathImporter:
     """
     A meta path importer to import six.moves and its submodules.
 
