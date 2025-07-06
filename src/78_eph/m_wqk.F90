4544f12a9747009b53067a5dace4e3966e0cd452
Matteo Giantomassi <gmatteo@gmail.com>
2025-07-06 18:02:37 +0200

    Savig work
    Former-commit-id: dbd45e1c00e1ad66b715339f1b8871cdde4aa32e
    Former-commit-id: 366a6dd988669a3121d51bd522aac0847d8c8ba9

--- src/78_eph/m_wqk.F90
+++ src/78_eph/m_wqk.F90
@@ -156,7 +156,7 @@ subroutine wqk_run(wfk0_path, dtfil, ngfft, ngfftf, dtset, cryst, ebands, wfk_hd
  character(len=5000) :: msg, qkp_string
 !arrays
  integer :: nbsum, my_bsum_start(dtset%nsppol), my_bsum_stop(dtset%nsppol), my_nbsum(dtset%nsppol)
- integer :: g0_k(3), g0_kq(3), units(2), work_ngfft(18), gmax(3)
+ integer :: g0_k(3), g0_kq(3), units(2), work_ngfft(18), gmax(3), prtwqk
  integer :: mapl_k(6), mapl_kq(6), mapc_qq(6)
  integer,allocatable :: kg_k(:,:), kg_kq(:,:), my_pp_inds(:)
  integer,allocatable :: gbound_k(:,:), gbound_kq(:,:), gbound_c(:,:), nband(:,:), wfd_istwfk(:)
@@ -176,6 +176,9 @@ subroutine wqk_run(wfk0_path, dtfil, ngfft, ngfftf, dtset, cryst, ebands, wfk_hd
    ABI_ERROR("PAW not implemented")
    ABI_UNUSED((/pawang%nsym, pawrad(1)%mesh_size/))
  end if
+ if (dtset%nsppol == 2) then
+   ABI_ERROR("nsppol implemented")
+ end if
 
  my_rank = xmpi_comm_rank(comm); nprocs = xmpi_comm_size(comm); units = [std_out, ab_out]
  call cwtime(cpu_all, wall_all, gflops_all, "start")
@@ -184,8 +187,11 @@ subroutine wqk_run(wfk0_path, dtfil, ngfft, ngfftf, dtset, cryst, ebands, wfk_hd
  ecut = dtset%ecut ! dtset%dilatmx
 
  ! Open WQK.nc file and go to data mode.
- NCF_CHECK(nctk_open_modify(root_ncid, strcat(dtfil%filnam_ds(4), "_WQK.nc") , comm))
- NCF_CHECK(nctk_set_datamode(root_ncid))
+ prtwqk = 1
+ if (prtwqk /= 0) then
+   NCF_CHECK(nctk_open_create(root_ncid, strcat(dtfil%filnam_ds(4), "_WQK.nc") , comm))
+   NCF_CHECK(nctk_set_datamode(root_ncid))
+ end if
 
  !call wrtout(units, sjoin("- Number of q-points/spin completed:", itoa(count(done_qbz_spin == 1)), "/", itoa(sigma%nkcalc)))
 
@@ -361,27 +367,58 @@ subroutine wqk_run(wfk0_path, dtfil, ngfft, ngfftf, dtset, cryst, ebands, wfk_hd
  !qbuf_size = 16
  call wrtout(std_out, sjoin(" Begin computation of GWPT e-ph matrix elements with qbuf_size:", itoa(qbuf_size)), pre_newlines=1)
 
- ! ===============================
- ! Loop over MPI distributed spins
- ! ===============================
+ !do my_is=1,gstore%my_nspins
+ !NCF_CHECK(nf90_inq_ncid(root_ncid, strcat("gqk", "_spin", itoa(spin)), spin_ncid))
+
+ ! TODO: Should introduce the possibility of specifying different nb states
+ ! for the incoming and the intermediate states.
+ !nb = gqk%nb
+ nb = 1
+ ABI_MALLOC(iq_buf, (2, qbuf_size))
 
 #if 0
- do my_is=1,gstore%my_nspins
-   !NCF_CHECK(nf90_inq_ncid(root_ncid, strcat("gqk", "_spin", itoa(spin)), spin_ncid))
+ !ABI_MALLOC_OR_DIE(ur_nk,  (nfft*nspinor, gqk%bstart:gqk%bstop), ierr)
+ !ABI_MALLOC_OR_DIE(ur_mkq, (nfft*nspinor, gqk%bstart:gqk%bstop), ierr)
+ !ABI_MALLOC_OR_DIE(my_gbuf, (gqk%cplex, nb, nb, natom3, gqk%my_nk, qbuf_size), ierr)
 
-   ! TODO: Should introduce the possibility of specifying different nb states
-   ! for the incoming and the intermediate states.
-   nb = gqk%nb
-   ABI_MALLOC(iq_buf, (2, qbuf_size))
+ ! ==================
+ ! Loop over k-points
+ ! ==================
 
-   ABI_MALLOC_OR_DIE(ur_nk,  (nfft*nspinor, gqk%bstart:gqk%bstop), ierr)
-   ABI_MALLOC_OR_DIE(ur_mkq, (nfft*nspinor, gqk%bstart:gqk%bstop), ierr)
-   ABI_MALLOC_OR_DIE(my_gbuf, (gqk%cplex, nb, nb, natom3, gqk%my_nk, qbuf_size), ierr)
+ do my_ik=1,gqk%my_nk
+   ! NB: All procs in gqk%pert_comm and gqk%bsum_com and gqk%pp_sum_comm enter this section.
+   !iqbuf_cnt = 1 + mod(my_iq - 1, qbuf_size)
+   !iq_buf(:, iqbuf_cnt) = [my_iq, iq_bz]
 
-   ! ============================================================
-   ! Loop over MPI distributed q-points in Sigma_q (gqk%qpt_comm)
-   ! ============================================================
+   ! Set entry to zero. Important as there are cycle instructions inside these loops
+   ! and we don't want random numbers written to disk.
+   !my_gbuf(:,:,:,:, my_ik, iqbuf_cnt) = zero
+
+   ! Symmetry indices for kk.
+   kk = gqk%my_kpts(:, my_ik)
+   ! The k-point and the symmetries relating the BZ k-point to the IBZ.
+   ik_ibz = gqk%my_k2ibz(1, my_ik) ; isym_k = gqk%my_k2ibz(2, my_ik)
+   trev_k = gqk%my_k2ibz(6, my_ik); g0_k = gqk%my_k2ibz(3:5,my_ik)
+   isirr_k = (isym_k == 1 .and. trev_k == 0 .and. all(g0_k == 0))
+   mapl_k = gqk%my_k2ibz(:, my_ik)
+   !print *, "my_ik", my_ik, " of my_nk:", gqk%my_nk
+
+   kk_ibz = ebands%kptns(:,ik_ibz)
+   istwf_k_ibz = wfd%istwfk(ik_ibz); npw_k_ibz = wfd%npwarr(ik_ibz)
+   !print *, "ik_ibz:", ik_ibz, "kk:", kk, "kk_ibz:", kk_ibz
+
+   print_time_kk = my_rank == 0 .and. (my_ik <= LOG_MODK .or. mod(my_ik, LOG_MODK) == 0)
+   if (print_time_kk) call cwtime(cpu_kk, wall_kk, gflops_kk, "start")
+
+   ! Get npw_k, kg_k for kk
+   call wfd%get_gvec_gbound(cryst%gmet, ecut, kk, ik_ibz, isirr_k, dtset%nloalg, & ! in
+                            istwf_k, npw_k, kg_k, nkpg_k, kpg_k, gbound_k)         ! out
+
+   ! ==================
+   ! Loop over q-points
+   ! ==================
    do my_iq=1,gqk%my_nq
+     !qpt = ??
      qq_is_gamma = sum(qpt**2) < tol14
      iq_bz = gqk%my_q2bz(my_iq)
 
@@ -407,98 +444,55 @@ subroutine wqk_run(wfk0_path, dtfil, ngfft, ngfftf, dtset, cryst, ebands, wfk_hd
        call wrtout(std_out, sjoin(msg, ", for spin:", itoa(spin)), pre_newlines=1)
      end if
 
-     !if (isirr_q) then
-     !else
+     ! Find k + q in the extended zone and extract symmetry info.
+     ! Be careful here because there are two umklapp vectors to be considered as:
+     !
+     !   k + q = k_bz + g0_bz = IS(k_ibz) + g0_ibz + g0_bz
+     !
+     kq = kk + qq_ibz
+
+     !if (kpts_map("symrel", ebands%kptopt, cryst, gstore%krank_ibz, 1, kq, mapl_kq) /= 0) then
+     !  write(msg, '(4a)' )"k-mesh is not closed!",ch10, "k+q could not be generated from a symmetrical one.",trim(ltoa(kq))
+     !  ABI_ERROR(msg)
      !end if
+     ikq_ibz = mapl_kq(1); isym_kq = mapl_kq(2); trev_kq = mapl_kq(6); g0_kq = mapl_kq(3:5)
+     isirr_kq = (isym_kq == 1 .and. trev_kq == 0 .and. all(g0_kq == 0))
+     kq_ibz = ebands%kptns(:, ikq_ibz)
+     istwf_kq_ibz = wfd%istwfk(ikq_ibz); npw_kq_ibz = wfd%npwarr(ikq_ibz)
 
-     ! ==================
-     ! Loop over k-points
-     ! ==================
+     ! Get npw_kq, kg_kq for k+q.
+     call wfd%get_gvec_gbound(cryst%gmet, ecut, kq, ikq_ibz, isirr_kq, dtset%nloalg, &  ! in
+                              istwf_kq, npw_kq, kg_kq, nkpg_kq, kpg_kq, gbound_kq)      ! out
 
-     do my_ik=1,gqk%my_nk
-       ! NB: All procs in gqk%pert_comm and gqk%bsum_com and gqk%pp_sum_comm enter this section.
-       iqbuf_cnt = 1 + mod(my_iq - 1, qbuf_size)
-       iq_buf(:, iqbuf_cnt) = [my_iq, iq_bz]
+     ABI_MALLOC(ug_k, (2, npw_k*nspinor))
+     ABI_MALLOC(ug_kq, (2, npw_kq*nspinor))
 
-       ! Set entry to zero. Important as there are cycle instructions inside these loops
-       ! and we don't want random numbers written to disk.
-       my_gbuf(:,:,:,:, my_ik, iqbuf_cnt) = zero
+     ! Precompute ur_nk and ur_mkq for all m and n indices treated by me.
+     ! TODO: Can distribute operations inside gqk%pert_comm
+     do n_k=gqk%bstart, gqk%bstop ! do n_k=gqk%n_start, gqk%n_stop
+       call wfd%rotate_cg(n_k, ndat1, spin, kk_ibz, npw_k, kg_k, istwf_k, &
+                          cryst, mapl_k, gbound_k, work_ngfft, work, ug_k, urs_kbz=ur_nk(:,n_k))
+     end do
 
-       ! Symmetry indices for kk.
-       kk = gqk%my_kpts(:, my_ik)
-       ! The k-point and the symmetries relating the BZ k-point to the IBZ.
-       ik_ibz = gqk%my_k2ibz(1, my_ik) ; isym_k = gqk%my_k2ibz(2, my_ik)
-       trev_k = gqk%my_k2ibz(6, my_ik); g0_k = gqk%my_k2ibz(3:5,my_ik)
-       isirr_k = (isym_k == 1 .and. trev_k == 0 .and. all(g0_k == 0))
-       mapl_k = gqk%my_k2ibz(:, my_ik)
-       !print *, "my_ik", my_ik, " of my_nk:", gqk%my_nk
+     do m_kq=gqk%bstart, gqk%bstop ! do m_kq=gqk%m_start, gqk%m_stop
+       call wfd%rotate_cg(m_kq, ndat1, spin, kq_ibz, npw_kq, kg_kq, istwf_kq, &
+                          cryst, mapl_kq, gbound_kq, work_ngfft, work, ug_kq, urs_kbz=ur_mkq(:,m_kq))
+     end do
 
-       kk_ibz = ebands%kptns(:,ik_ibz)
-       istwf_k_ibz = wfd%istwfk(ik_ibz); npw_k_ibz = wfd%npwarr(ik_ibz)
-       !print *, "ik_ibz:", ik_ibz, "kk:", kk, "kk_ibz:", kk_ibz
+     ! Be careful here because pp should run over the list of wavevectors in the screening matrix!
+     ! as qmesh%bz is not necessarily equivalent to the k-mesh for the wavefunctions,
+     ! and we have to use the ipp_bz index to symmetrize W(pp_bz) from W(pp_ibz).
+     !
+     ! TODO: Should order nbz in shells so that one can reduce the memory required
+     ! to store W(pp) if pp_parallelism is activated.
 
-       print_time_kk = my_rank == 0 .and. (my_ik <= LOG_MODK .or. mod(my_ik, LOG_MODK) == 0)
-       if (print_time_kk) call cwtime(cpu_kk, wall_kk, gflops_kk, "start")
+     ABI_FREE(kpg_k)
+     ABI_FREE(kpg_kq)
+     ABI_FREE(ug_k)
+     ABI_FREE(ug_kq)
 
-       ! Get npw_k, kg_k for kk
-       call wfd%get_gvec_gbound(cryst%gmet, ecut, kk, ik_ibz, isirr_k, dtset%nloalg, & ! in
-                                istwf_k, npw_k, kg_k, nkpg_k, kpg_k, gbound_k)         ! out
-
-       ! Find k + q in the extended zone and extract symmetry info.
-       ! Be careful here because there are two umklapp vectors to be considered as:
-       !
-       !   k + q = k_bz + g0_bz = IS(k_ibz) + g0_ibz + g0_bz
-       !
-       kq = kk + qq_ibz
-
-       !if (kpts_map("symrel", ebands%kptopt, cryst, gstore%krank_ibz, 1, kq, mapl_kq) /= 0) then
-       !  write(msg, '(4a)' )"k-mesh is not closed!",ch10, "k+q could not be generated from a symmetrical one.",trim(ltoa(kq))
-       !  ABI_ERROR(msg)
-       !end if
-       ikq_ibz = mapl_kq(1); isym_kq = mapl_kq(2); trev_kq = mapl_kq(6); g0_kq = mapl_kq(3:5)
-       isirr_kq = (isym_kq == 1 .and. trev_kq == 0 .and. all(g0_kq == 0))
-       kq_ibz = ebands%kptns(:, ikq_ibz)
-       istwf_kq_ibz = wfd%istwfk(ikq_ibz); npw_kq_ibz = wfd%npwarr(ikq_ibz)
-
-       ! Get npw_kq, kg_kq for k+q.
-       call wfd%get_gvec_gbound(cryst%gmet, ecut, kq, ikq_ibz, isirr_kq, dtset%nloalg, &  ! in
-                                istwf_kq, npw_kq, kg_kq, nkpg_kq, kpg_kq, gbound_kq)      ! out
-
-       ABI_MALLOC(ug_k, (2, npw_k*nspinor))
-       ABI_MALLOC(ug_kq, (2, npw_kq*nspinor))
-
-       ! Precompute ur_nk and ur_mkq for all m and n indices treated by me.
-       ! TODO: Can distribute operations inside gqk%pert_comm
-       do n_k=gqk%bstart, gqk%bstop ! do n_k=gqk%n_start, gqk%n_stop
-         call wfd%rotate_cg(n_k, ndat1, spin, kk_ibz, npw_k, kg_k, istwf_k, &
-                            cryst, mapl_k, gbound_k, work_ngfft, work, ug_k, urs_kbz=ur_nk(:,n_k))
-       end do
-
-       do m_kq=gqk%bstart, gqk%bstop ! do m_kq=gqk%m_start, gqk%m_stop
-         call wfd%rotate_cg(m_kq, ndat1, spin, kq_ibz, npw_kq, kg_kq, istwf_kq, &
-                            cryst, mapl_kq, gbound_kq, work_ngfft, work, ug_kq, urs_kbz=ur_mkq(:,m_kq))
-       end do
-
-       ! Be careful here because pp should run over the list of wavevectors in the screening matrix!
-       ! as qmesh%bz is not necessarily equivalent to the k-mesh for the wavefunctions,
-       ! and we have to use the ipp_bz index to symmetrize W(pp_bz) from W(pp_ibz).
-       !
-       ! TODO: Should order nbz in shells so that one can reduce the memory required
-       ! to store W(pp) if pp_parallelism is activated.
-
-       ABI_FREE(kpg_k)
-       ABI_FREE(kpg_kq)
-       ABI_FREE(ug_k)
-       ABI_FREE(ug_kq)
-
-       ! Dump buffer
-       !if (iqbuf_cnt == qbuf_size) call dump_my_gbuf()
-
-       if (print_time_kk) then
-         call inds2str(3, "My k-point", my_ik, gqk%my_nk, gqk%glob_nk, msg)
-         call cwtime_report(msg, cpu_kk, wall_kk, gflops_kk); if (my_ik == LOG_MODK) call wrtout(std_out, "...", do_flush=.True.)
-       end if
-     end do ! my_ik
+     ! Dump buffer
+     !if (iqbuf_cnt == qbuf_size) call dump_my_gbuf()
 
      if (print_time_qq) then
        call inds2str(2, "My q-point", my_iq, gqk%my_nq, gqk%glob_nq, msg)
@@ -506,23 +500,33 @@ subroutine wqk_run(wfk0_path, dtfil, ngfft, ngfftf, dtset, cryst, ebands, wfk_hd
      end if
    end do ! iq_ibz
 
-   ! Dump the remainder.
-   if (iqbuf_cnt /= 0) call dump_my_gbuf()
+    if (print_time_kk) then
+      call inds2str(3, "My k-point", my_ik, gqk%my_nk, gqk%glob_nk, msg)
+      call cwtime_report(msg, cpu_kk, wall_kk, gflops_kk); if (my_ik == LOG_MODK) call wrtout(std_out, "...", do_flush=.True.)
+    end if
 
-   ABI_FREE(ur_nk)
-   ABI_FREE(ur_mkq)
-   ABI_FREE(iq_buf)
-   ABI_FREE(my_gbuf)
- end do ! my_is
+ end do ! my_ik
+#endif
+
+ ! Dump the remainder.
+ !if (iqbuf_cnt /= 0) call dump_my_gbuf()
+
+ !ABI_FREE(ur_nk)
+ !ABI_FREE(ur_mkq)
+ ABI_FREE(iq_buf)
+ !ABI_FREE(my_gbuf)
+ !end do ! my_is
 
  call cwtime_report(" vkk calculation", cpu_all, wall_all, gflops_all, end_str=ch10)
 
  ! Set vqk_completed to 1 so that we can easily check if restarted is needed.
- !if (my_rank == master) then
- !  NCF_CHECK(nf90_put_var(root_ncid, root_vid("vqk_completed"), 1))
- !end if
- NCF_CHECK(nf90_close(root_ncid))
- call xmpi_barrier(comm)
+ if (prtwqk /= 0) then
+   !if (my_rank == master) then
+   !  NCF_CHECK(nf90_put_var(root_ncid, root_vid("vqk_completed"), 1))
+   !end if
+   NCF_CHECK(nf90_close(root_ncid))
+   call xmpi_barrier(comm)
+ end if
 
  ! Output some of the results to ab_out for testing purposes
  !call gstore%print_for_abitests(dtset)
@@ -536,14 +540,13 @@ subroutine wqk_run(wfk0_path, dtfil, ngfft, ngfftf, dtset, cryst, ebands, wfk_hd
  ABI_FREE(gbound_k)
  ABI_FREE(gbound_kq)
  ABI_FREE(gbound_c)
- ABI_FREE(done_qbz_spin)
+ !ABI_FREE(done_qbz_spin)
 
  call wfd%free(); call vcp%free()
  call qmesh%free(); call gsph_c%free()
 
  call xmpi_barrier(comm) ! This to make sure that the parallel output of GSTORE is completed
  call cwtime_report(" wqk_run: MPI barrier before returning.", cpu_all, wall_all, gflops_all, end_str=ch10, comm=comm)
-#endif
 
 contains
 
@@ -599,10 +602,10 @@ subroutine dump_my_gbuf()
 
  ! Only one proc sets the entry in done_qbz_spin to 1 for all the q-points in the buffer.
  !if (all(gqk%coords_qkpb_sumbp(2:3) == [0, 0]))  then
-   do ii=1,iqbuf_cnt
-     iq_bz = iq_buf(2, ii)
-     NCF_CHECK(nf90_put_var(root_ncid, root_vid("gstore_done_qbz_spin"), 1, start=[iq_bz, spin]))
-   end do
+ !  do ii=1,iqbuf_cnt
+ !    iq_bz = iq_buf(2, ii)
+ !    NCF_CHECK(nf90_put_var(root_ncid, root_vid("gstore_done_qbz_spin"), 1, start=[iq_bz, spin]))
+ !  end do
  !end if
 
  ! Zero the counter before returning
