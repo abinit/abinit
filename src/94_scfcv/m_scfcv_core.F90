2db3d7e861c54285d710af1702389fbfee19aefb
Fabien BRIEUC <Fabien.BRIEUC@cea.fr>
2025-07-18 10:27:55 +0200

    Correction to ensure usekden works for mixing on density and potential correctly
    Former-commit-id: 38379ff9dc0a9644dc2993750f1a630b0c21b784

--- src/94_scfcv/m_scfcv_core.F90
+++ src/94_scfcv/m_scfcv_core.F90
@@ -766,7 +766,11 @@ subroutine scfcv_core(atindx,atindx1,cg,cprj,cpus,dmatpawu,dtefield,dtfil,dtpawu
    dielar(5)=dtset%diegap;dielar(6)=dtset%dielam
    dielar(7)=dtset%diemix;if (dtset%iscf>=10) dielar(7)=dtset%diemixmag
    ABI_MALLOC(nvresid,(nfftf,dtset%nspden))
-   ABI_MALLOC(nvtauresid,(nfftf,dtset%nspden*dtset%usekden))
+   if (dtset%iscf>=10) then
+      ABI_MALLOC(nvtauresid,(nfftf,dtset%nspden*dtset%usekden))
+   else
+      ABI_MALLOC(nvtauresid,(nfftf,dtset%nspden*usevxctau))
+   end if
    if (nstep==0) then
     nvresid=zero
     nvtauresid=zero
