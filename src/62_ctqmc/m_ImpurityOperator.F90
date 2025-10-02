b05cad1590a76419eca081fe691d392f5df3547b
Frédéric Gendron <frederic.gendron@cea.fr>
2025-10-02 16:01:55 +0200

    fix diagmatlu
    Former-commit-id: e47a0c282b455fe92211836783f4737ba7d83751
    Former-commit-id: 60b91ebc0cddde1e8e2f924a18dfe12d83769212

--- src/62_ctqmc/m_ImpurityOperator.F90
+++ src/62_ctqmc/m_ImpurityOperator.F90
@@ -951,7 +951,7 @@ DOUBLE PRECISION FUNCTION ImpurityOperator_getNewOverlap(this, CdagC_1)
     IF ( otherFlavor .EQ. flavor ) CYCLE
     overlap = ImpurityOperator_overlapSegFlav(this,CdagC_2(1:2),otherflavor)
     totalOverlap = totalOverlap &
-                 + overlap * this%mat_U(otherFlavor,flavor)
+                 + overlap * dble(this%mat_U(otherFlavor,flavor))
     this%updates(otherFlavor) = -sign * overlap
   END DO
 
@@ -1414,7 +1414,7 @@ DOUBLE PRECISION FUNCTION ImpurityOperator_overlapFlavor(this,flavor)
     IF ( otherFlavor .EQ. flavor ) CYCLE
     overlap = this%overlaps(otherFlavor,flavor)
     totalOverlap = totalOverlap &
-                 + overlap * this%mat_U(otherFlavor,flavor)
+            + overlap * dble(this%mat_U(otherFlavor,flavor))
   END DO
 
   ImpurityOperator_overlapFlavor = totalOverlap
@@ -1468,11 +1468,11 @@ DOUBLE PRECISION FUNCTION ImpurityOperator_overlapSwap(this,flavor1,flavor2)
     ELSE IF ( otherFlavor .EQ. flavor1 ) THEN
       overlap = this%overlaps(otherFlavor,flavor2)
       totalOverlap = totalOverlap &
-                   + overlap * this%mat_U(otherFlavor,flavor2)
+                   + overlap * dble(this%mat_U(otherFlavor,flavor2))
     ELSE
       overlap = this%overlaps(otherFlavor,flavor2)
       totalOverlap = totalOverlap &
-                   + overlap * this%mat_U(otherFlavor,flavor1)
+                   + overlap * dble(this%mat_U(otherFlavor,flavor1))
     END IF
   END DO
 
@@ -1926,64 +1926,6 @@ SUBROUTINE ImpurityOperator_getErrorOverlap(this,DE)
   DE(2,2) = ABS(totalE1 - totalE2)
 
 END SUBROUTINE ImpurityOperator_getErrorOverlap
-
- !!****f* ABINIT/m_ImpurityOperator/ImpurityOperator_getErrorOverlapComplex                  
- !! NAME                                                                              
- !!  ImpurityOperator_getErrorOverlapComplex                                                 
- !!                                                                                   
- !! FUNCTION                                                                          
- !!  compute error on the overlap (numerical accumulation)                            
- !!                                                                                   
- !! COPYRIGHT                                                                         
- !!  Copyright (C) 2013-2025 ABINIT group (J. Bieder)                                 
- !!  This file is distributed under the terms of the                                  
- !!  GNU General Public License, see ~abinit/COPYING                                  
- !!  or http://www.gnu.org/copyleft/gpl.txt .                                         
- !!                                                                                   
- !! INPUTS                                                                            
- !!  this=ImpurityOperator                                                            
- !!                                                                                   
- !! OUTPUT                                                                            
- !!  DE=save the error                                                                
- !!                                                                                   
- !! SIDE EFFECTS                                                                      
- !!                                                                                   
- !! NOTES                                                                             
- !!                                                                                   
- !! SOURCE                                                                            
-                                                                                      
- SUBROUTINE ImpurityOperator_getErrorOverlapComplex(this,DE)                                 
-
-!Arguments ------------------------------------                             
-  TYPE(ImpurityOperator), INTENT(INOUT) :: this                             
-  COMPLEX(KIND=8), DIMENSION(:,:), INTENT(INOUT) :: DE                     
-!Local variables ------------------------------                             
-  COMPLEX(KIND=8)                              :: localD1                  
-  COMPLEX(KIND=8)                              :: localD2                  
-  COMPLEX(KIND=8)                              :: totalE1                  
-  COMPLEX(KIND=8)                              :: totalE2                  
-  INTEGER                                       :: iflavor1                 
-  INTEGER                                       :: iflavor2                 
-  INTEGER                                       :: flavors                  
-                                                                            
-  IF ( .NOT. ALLOCATED(this%particles) ) &                                  
-    CALL ERROR("ImpurityOperator_getErrorOverlap : no particle set ")       
-                                                                            
-  totalE1 = cmplx(0.d0,0.d0,kind=8)                                                            
-  totalE2 = cmplx(0.d0,0.d0,kind=8)                                                            
-  flavors = this%flavors                                                    
-  DO iflavor1 = 1, flavors                                                  
-    DO iflavor2 = iflavor1+1, flavors                                       
-      localD1 = ImpurityOperator_overlapIJ(this,iflavor1,iflavor2)          
-      localD2 = this%overlaps(iflavor2,iflavor1)                            
-      totalE1 = totalE1 + localD1 * this%mat_U(iflavor1,iflavor2)           
-      totalE2 = totalE2 + localD2 * this%mat_U(iflavor1,iflavor2)           
-    END DO                                                                  
-  END DO                                                                    
-                                                                            
-  DE(2,2) = ABS(totalE1 - totalE2)                                          
-                                                                            
-END SUBROUTINE ImpurityOperator_getErrorOverlapComplex                             
 
 !!***
 !#ifdef CTQMC_CHECK
