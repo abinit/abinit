7ca27035937b73b75f8f6f9fc9e21a2adf1e75ed
Marc Sarraute <marc.sarraute@asplus.fr>
2025-10-10 19:32:23 +0200

    Add initial support for CUDA 13
    There were minor changes in API from 12 to 13:
    - Some error codes were removed from cuFFT, other were added
    - Clock-rate is no longer a device property
      - still found in device attributes
    - API calls for cudaMemAdvise and cudaMemPrefetchAsync changed
    
    
    Former-commit-id: c7e666de5aca74f2e3dfb3d9b8492ff5677aa81d

--- shared/common/src/17_gpu_toolbox/cuda_api_error_check.h
+++ shared/common/src/17_gpu_toolbox/cuda_api_error_check.h
@@ -184,14 +184,18 @@ static const char *_cudaGetErrorEnum(cufftResult error) {
     case CUFFT_UNALIGNED_DATA:
       return "CUFFT_UNALIGNED_DATA";
 
+#if CUDA_VERSION < 13000
     case CUFFT_INCOMPLETE_PARAMETER_LIST:
       return "CUFFT_INCOMPLETE_PARAMETER_LIST";
+#endif
 
     case CUFFT_INVALID_DEVICE:
       return "CUFFT_INVALID_DEVICE";
 
+#if CUDA_VERSION < 13000
     case CUFFT_PARSE_ERROR:
       return "CUFFT_PARSE_ERROR";
+#endif
 
     case CUFFT_NO_WORKSPACE:
       return "CUFFT_NO_WORKSPACE";
@@ -199,11 +203,27 @@ static const char *_cudaGetErrorEnum(cufftResult error) {
     case CUFFT_NOT_IMPLEMENTED:
       return "CUFFT_NOT_IMPLEMENTED";
 
+#if CUDA_VERSION < 13000
     case CUFFT_LICENSE_ERROR:
       return "CUFFT_LICENSE_ERROR";
+#endif
 
     case CUFFT_NOT_SUPPORTED:
       return "CUFFT_NOT_SUPPORTED";
+
+#if CUDA_VERSION >= 13000
+    case CUFFT_MISSING_DEPENDENCY:
+      return "CUFFT_MISSING_DEPENDENCY";
+
+    case CUFFT_NVRTC_FAILURE:
+      return "CUFFT_NVRTC_FAILURE";
+
+    case CUFFT_NVJITLINK_FAILURE:
+      return "CUFFT_NVJITLINK_FAILURE";
+
+    case CUFFT_NVSHMEM_FAILURE:
+      return "CUFFT_NVSHMEM_FAILURE";
+#endif
   }
 
   return "<unknown>";
