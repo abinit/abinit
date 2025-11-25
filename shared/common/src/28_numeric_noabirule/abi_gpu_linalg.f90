3ab9ccb0cf4abf948d729fee427d503dc61b142f
Marc Sarraute <marc.sarraute@asplus.fr>
2025-11-25 11:35:25 +0100

    Fix GPU memory leak in abi_gpu_linalg (hegvd, heevd, potrf)
    Former-commit-id: d60fb568dce6bec26c6c0c7d0fb03b1a2c067d95

--- shared/common/src/28_numeric_noabirule/abi_gpu_linalg.f90
+++ shared/common/src/28_numeric_noabirule/abi_gpu_linalg.f90
@@ -2780,7 +2780,7 @@ subroutine abi_gpu_work_resizeCptr(array,current_dim,asked_dim)
 ! *************************************************************************
 
   if ( current_dim < asked_dim  ) then
-    if(current_dim == 0) then
+    if(current_dim /= 0) then
       call dealloc_on_gpu(array)
     end if
     current_dim = asked_dim
