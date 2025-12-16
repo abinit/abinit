7965fb9422c83ebeb20669b63804664efa688513
Le Shu <le.shu@student.uclouvain.be>
2025-12-16 17:27:16 +0100

    small change
    Former-commit-id: 01557f7507a45f9bd9b811acd7b27f63cc6f3fc7
    Former-commit-id: ae3086519a14ddebadf21a2f1b40f9713bc0e091

--- src/66_nonlocal/m_contract.F90
+++ src/66_nonlocal/m_contract.F90
@@ -2061,19 +2061,18 @@ end subroutine metcon_so
 subroutine metric_so(amet,soc_weight,gprimd,pauli,alpha,beta)
 
 !Arguments ------------------------------------
-!arrays
- real(dp),intent(in) :: alpha, beta
 !scalars
+ real(dp),intent(in) :: alpha, beta
+!arrays
  real(dp),intent(in) :: gprimd(3,3),soc_weight(3)
  real(dp),intent(out) :: amet(2,3,3,2,2),pauli(2,2,2,3)
 
 !Local variables-------------------------------
 !scalars
- integer :: iy1,iy2,m1,m2,n,cb2,sb2
+ integer :: iy1,iy2,m1,m2,n
 !arrays
- real(dp) :: buffer1(3,3,2,2) !,buffer2(3,3,3,3,2,2)
+ real(dp) :: buffer1(3,3,2,2), cb2, sb2 !,buffer2(3,3,3,3,2,2)
  complex(dp) :: S(2,2,3), Srot(2,2), U(2,2), ep, em
-
 ! **********************************************************************
 
 !Fill in Pauli matrices and make them spin matrices:
@@ -2084,23 +2083,24 @@ subroutine metric_so(amet,soc_weight,gprimd,pauli,alpha,beta)
  pauli(1,1,1,3)= 1.d0;pauli(1,2,2,3)=-1.d0
  pauli(:,:,:,:)= 0.5d0*pauli(:,:,:,:)
 
- ! test the beta and alpha is zero
- S(:,:,:) = cmplx(pauli(1,:,:,:), pauli(2,:,:,:), kind=dp)
+ if (.not.(abs(alpha) < tol8 .and. abs(beta) < tol8)) then
+   S(:,:,:) = cmplx(pauli(1,:,:,:), pauli(2,:,:,:), kind=dp)
 
- cb2 = cos(half*beta); sb2 = sin(half*beta)
- em = exp(-j_dpc*0.5_dp*alpha); ep = conjg(em)
+   cb2 = cos(half*beta); sb2 = sin(half*beta)
+   em = exp(-j_dpc*0.5_dp*alpha); ep = conjg(em)
 
- U(1,1) =  cb2 * em; U(1,2) = -sb2 * em
- U(2,1) =  sb2 * ep; U(2,2) =  cb2 * ep
+   U(1,1) =  cb2 * em; U(1,2) = -sb2 * em
+   U(2,1) =  sb2 * ep; U(2,2) =  cb2 * ep
 
- do n = 1, 3
-   Srot(:,:) = matmul(conjg(transpose(U)), matmul(S(:,:,n), U))
-   S(:,:,n)  = Srot(:,:)
- end do
+   do n = 1, 3
+     Srot(:,:) = matmul(conjg(transpose(U)), matmul(S(:,:,n), U))
+     S(:,:,n)  = Srot(:,:)
+   end do
     
- pauli(:,:,:,:) = 0.0_dp
- pauli(1,:,:,:) = real(S(:,:,:), kind=dp)
- pauli(2,:,:,:) = aimag(S(:,:,:))
+   pauli(:,:,:,:) = 0.0_dp
+   pauli(1,:,:,:) = real(S(:,:,:), kind=dp)
+   pauli(2,:,:,:) = aimag(S(:,:,:))
+ end if
 
 !Construct the antisymmetric tensor:
  amet(:,:,:,:,:)=0.d0
