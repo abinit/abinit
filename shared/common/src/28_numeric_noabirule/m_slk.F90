07895aa3b6c417d3b1fc030982ff38a5179297d8
Matteo Giantomassi <gmatteo@gmail.com>
2026-01-01 15:57:00 +0100

    Execute other sections on the GPU
    Former-commit-id: b56ddfa7e70c7eb112a7cd5cfcfd603e9510035d

--- shared/common/src/28_numeric_noabirule/m_slk.F90
+++ shared/common/src/28_numeric_noabirule/m_slk.F90
@@ -31,7 +31,6 @@ module m_slk
  use m_xomp
  use m_errors
  use m_abicore
-
 #ifdef HAVE_LINALG_ELPA
  use m_elpa
 #endif
@@ -421,6 +420,19 @@ module m_slk
  complex(sp),external :: PCLATRA
  complex(dp),external :: PZLATRA
 #endif
+
+
+#ifdef HAVE_GPU
+  interface
+    subroutine my_gpu_memset(gpu_ptr, val, size_in_bytes) bind(c, name="gpu_memset_cpp_")
+      use, intrinsic :: iso_c_binding
+      type(c_ptr),                    intent(in) :: gpu_ptr
+      integer(kind=c_int32_t),        intent(in)    :: val
+      integer(kind=c_size_t),         intent(in)    :: size_in_bytes
+    end subroutine my_gpu_memset
+  end interface
+#endif
+
 
 contains  !==============================================================================
 !!***
@@ -1330,34 +1342,34 @@ subroutine basemat_gpu_set_zero(mat)
 !Arguments ------------------------------------
  class(basemat_t),target,intent(inout) :: mat
 
+#ifdef HAVE_OPENMP_OFFLOAD
 !Local variables-------------------------------
  type(c_ptr) :: gpu_ptr
 ! *********************************************************************
 
-#ifdef HAVE_OPENMP_OFFLOAD
  select type (mat)
  class is (slkmat_dp_t)
    if (allocated(mat%buffer_cplx)) then
-     !gpu_ptr = xomp_get_mapped_ptr(c_loc(mat%buffer_cplx)
-     !ABI_CHECK_CNULL(gpu_ptr, "buffer_cplx not on GPU!")
-     !call gpu_memset(gpu_ptr, 0, mat%bufsize*dp*2)
+     gpu_ptr = xomp_get_mapped_ptr(c_loc(mat%buffer_cplx)
+     ABI_CHECK_CNULL(gpu_ptr, "buffer_cplx not on GPU!")
+     call my_gpu_memset(gpu_ptr, 0, mat%bufsize*dp*2)
    end if
    if (allocated(mat%buffer_real)) then
-     !gpu_ptr = xomp_get_mapped_ptr(c_loc(mat%buffer_real)
-     !ABI_CHECK_CNULL(gpu_ptr, "buffer_real not on GPU!")
-     !call gpu_memset(gpu_ptr, 0, mat%bufsize*dp)
+     gpu_ptr = xomp_get_mapped_ptr(c_loc(mat%buffer_real)
+     ABI_CHECK_CNULL(gpu_ptr, "buffer_real not on GPU!")
+     call my_gpu_memset(gpu_ptr, 0, mat%bufsize*dp)
    end if
  class is (slkmat_sp_t)
    if (allocated(mat%buffer_cplx)) then
-     !mat%buffer_cplx = cmplx(cvalue, kind=sp)
-     !gpu_ptr = xomp_get_mapped_ptr(c_loc(mat%buffer_cplx)
-     !ABI_CHECK_CNULL(gpu_ptr, "buffer_cplx not on GPU!")
-     !call gpu_memset(gpu_ptr, 0, mat%bufsize*sp*2)
+     mat%buffer_cplx = cmplx(cvalue, kind=sp)
+     gpu_ptr = xomp_get_mapped_ptr(c_loc(mat%buffer_cplx)
+     ABI_CHECK_CNULL(gpu_ptr, "buffer_cplx not on GPU!")
+     call my_gpu_memset(gpu_ptr, 0, mat%bufsize*sp*2)
    end if
    if (allocated(mat%buffer_real)) then
-     !gpu_ptr = xomp_get_mapped_ptr(c_loc(mat%buffer_real)
-     !ABI_CHECK_CNULL(gpu_ptr, "buffer_real not on GPU!")
-     !call gpu_memset(gpu_ptr, 0, mat%bufsize*sp)
+     gpu_ptr = xomp_get_mapped_ptr(c_loc(mat%buffer_real)
+     ABI_CHECK_CNULL(gpu_ptr, "buffer_real not on GPU!")
+     call my_gpu_memset(gpu_ptr, 0, mat%bufsize*sp)
    end if
  end select
 #else
