cfc245d9a84a3d1187a7b122bf3e261965ba03a9
gmatteo <gmatteo@gmail.com>
2026-01-22 00:31:21 +0100

    RMM-DIIS wit GPU seems to work -> allow it and add test
    Former-commit-id: b997897d2b37865d41d9760b55efe6b1f5687f48
    Former-commit-id: a39907df0f2a68d0c4032a7216a4868f9c2a90db

--- tests/gpu_omp/__init__.py
+++ tests/gpu_omp/__init__.py
@@ -76,4 +76,6 @@ inp_files = [
 #Other
 "t51.abi", # test CHEBFI (istwfk==1, npband==1, paral_kgb==1) with gpu_thread_limit set to 64
 "t52.abi", # PAW, GS, CGWF,       (Fe,  nspinor==2, nspden==4, paral_kgb==1), with fixed_occ
+#
+"t53.abi", # RMM-DIIS with NC pseudos
 ]
