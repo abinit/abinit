cba511fc5f1c9e6388ee6d6576d0f2eb3cabea6c
trunk <trunk@abinit.org>
2026-01-22 09:00:12 +0100

    Now update copyright to 2026 first step ...
    Former-commit-id: 7f9a3e8976382968f5f2761fdfc6a2aa67061c88

--- src/78_effpot/effpot_xml.c
+++ src/78_effpot/effpot_xml.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015-2025 ABINIT group (AM)
+ * Copyright (C) 2015-2026 ABINIT group (AM)
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
