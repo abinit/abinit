498c246dbc63ebe74676bc81778a9b4541d3109e
Frederic GENDRON <Frederic.GENDRON@cea.fr>
2026-01-23 17:00:23 +0100

    cleaning
    Former-commit-id: 53879de09716237e8047eeae39d1a0bc3fea4a25
    Former-commit-id: 8dad58c1dca5f6c22eff826289097ae0e1a29304

--- src/68_dmft/m_forctqmc.F90
+++ src/68_dmft/m_forctqmc.F90
@@ -267,7 +267,6 @@ subroutine qmc_prep_ctqmc(cryst_struc,green,self,hu,paw_dmft,pawang,pawprtvol,we
    rot_type_vee = 3 !  
  end if ! useylm
 
- write(*,*)"Hello Fred rot_type in forctqmc=",rot_type_vee
  ! if(useylm==1.and.opt_diag/=1) ABI_ERROR("useylm==1 and opt_diag/=0 is not possible")
  if (hu(1)%jpawu_zero .and. nsppol == 2) nsppol_imp = 2 ! J=0 and nsppol=2
  if (.not. hu(1)%jpawu_zero .or. nsppol /= 2) nsppol_imp = 1  ! J/=0 ou nsppol=1
