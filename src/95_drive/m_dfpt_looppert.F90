c85fc83e28bc2b96ae56ad0c8c96b78902729965
Miquel Royo <mroyo@icmab.es>
2026-01-26 12:41:22 +0100

    Remove a useless reference to autoparal in response function

--- src/95_drive/m_dfpt_looppert.F90
+++ src/95_drive/m_dfpt_looppert.F90
@@ -362,7 +362,7 @@ subroutine dfpt_looppert(atindx,blkflg,codvsn,cpus,dim_eigbrd,dim_eig2nkq,doccde
    kramers_deg=.false.
  end if
  
- if (.not.kramers_deg.and.(dtset%npfft/=1.or.dtset%autoparal/=0)) then
+ if (.not.kramers_deg.and.dtset%npfft/=1) then
    write(msg,'(a,a)')'   WARNING: RF routines are not adapted to fft parallelization'//char(10)//&
 & '   when tim1rev=0. The result of this calculation will most surely be wrong if npfft/=1.'
    ABI_WARNING(msg)
