e10657e6a1e43cf43db4443943a4e6fdfb9a11e5
Le Shu <le.shu@student.uclouvain.be>
2026-01-27 12:04:46 +0100

    minor error
    Former-commit-id: b4398c9f78343590d93f5cbb64ac0ef459eda4b4
    Former-commit-id: e5d6d3ae484bc00a65e6e8134d2c4f425ce2e8aa

--- src/66_nonlocal/m_nonlop_pl.F90
+++ src/66_nonlocal/m_nonlop_pl.F90
@@ -35,11 +35,11 @@ module m_nonlop_pl
  use m_metstr
  use m_opernl
 
- use defs_abitypes, only : MPI_type
- use m_geometry,   only : strconv
- use m_kg,         only : ph1d3d
- use m_contract,   only : cont22cso, cont22so, cont24, cont33cso, cont33so, cont35, cont22, cont3, cont13, &
-                          metcon, metcon_so, metric_so
+ use defs_abitypes,   only : MPI_type
+ use m_geometry,      only : strconv
+ use m_kg,            only : ph1d3d
+ use m_contract,      only : cont22cso, cont22so, cont24, cont33cso, cont33so, cont35, cont22, cont3, cont13, &
+                             metcon, metcon_so, metric_so
  use m_numeric_tools, only : geteuler
  implicit none
 
