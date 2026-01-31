32d91833b21dbc9b6b09dc32b08490d9e47e7fd7
Josef Zwanziger <jzwanzig@dal.ca>
2026-01-31 09:23:23 -0400

    minor comment edits
    Former-commit-id: 99d4ec9e5b475b5de3975539c1dba38e3e631886
    Former-commit-id: 8457b39fd4cba18ee8f8b8de88477dade53678b3

--- src/72_response/m_orbmag.F90
+++ src/72_response/m_orbmag.F90
@@ -1091,7 +1091,7 @@ subroutine orbmag_cc_k(atindx,cprj1_k,dimlmn,dtset,eig_k,fermie,gcg1_k,gs_hamk,i
   !scalars
   integer :: adir,bdir,cpopt,gdir,ndat,nn,npwsp,sij_opt,tim_getghc,type_calc
   real(dp) :: doti,dotr,epsabg,lams
-  complex(dp) :: b1,m1,m1_mu,prefac_b,prefac_m
+  complex(dp) :: b1,m1,prefac_b,prefac_m
   !arrays
   real(dp),allocatable :: bra(:,:),ghc(:,:),gsc(:,:),gvnlxc(:,:),ket(:,:)
   type(pawcprj_type),allocatable :: cwaveprj1(:,:)
@@ -1117,7 +1117,6 @@ subroutine orbmag_cc_k(atindx,cprj1_k,dimlmn,dtset,eig_k,fermie,gcg1_k,gs_hamk,i
    do nn = 1, nband_k
 
      m1 = czero
-     m1_mu = czero
      b1 = czero
 
      do gdir = 1, 3
@@ -1129,11 +1128,11 @@ subroutine orbmag_cc_k(atindx,cprj1_k,dimlmn,dtset,eig_k,fermie,gcg1_k,gs_hamk,i
          & mkmem_rbz,dtset%natom,1,nband_k,dtset%nspinor,dtset%nsppol,0)
 
        ! compute H|Pc du> and S|Pc du>
+       ! [H+E*S - 2\mu*S]|ket> is needed for orbmag
+       ! -2*S|ket> needed for Chern
        call getghc(cpopt,ket,cwaveprj1,ghc,gsc,gs_hamk,gvnlxc,lams,mpi_enreg,&
          & ndat,dtset%prtvol,sij_opt,tim_getghc,type_calc)
 
-       ! [H+E*S - 2\mu*S]|ket> is needed for orbmag
-       ! -2*S|ket> needed below for Chern
        ghc(1:2,1:npwsp) = ghc(1:2,1:npwsp) + gsc(1:2,1:npwsp)*(eig_k(nn) - two*fermie)
 
        do bdir = 1, 3
@@ -1149,15 +1148,11 @@ subroutine orbmag_cc_k(atindx,cprj1_k,dimlmn,dtset,eig_k,fermie,gcg1_k,gs_hamk,i
 
          dotr = DOT_PRODUCT(bra(1,:),gsc(1,:))+DOT_PRODUCT(bra(2,:),gsc(2,:))
          doti = DOT_PRODUCT(bra(1,:),gsc(2,:))-DOT_PRODUCT(bra(2,:),gsc(1,:))
-         b1 = b1 -two*prefac_b*CMPLX(dotr,doti)
-         !m1 = m1 + prefac_m*CMPLX(dotr,doti)*eig_k(nn)
-         !m1_mu = m1_mu - two*prefac_m*CMPLX(dotr,doti)*fermie
-
+         b1 = b1 - two*prefac_b*CMPLX(dotr,doti)
        end do !bdir
  
      end do !gdir
 
-     !orbmag_mesh%omesh(nn,ikpt,isppol,adir,incc) = real(m1 + m1_mu)
      orbmag_mesh%omesh(nn,ikpt,isppol,adir,incc) = real(m1)
      orbmag_mesh%cmesh(nn,ikpt,isppol,adir,ibcc) = real(b1)
 
