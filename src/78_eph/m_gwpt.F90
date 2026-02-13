389062a53b4ea791f1825c811fc836cbfffeeb85
Matteo Giantomassi <gmatteo@gmail.com>
2026-02-13 02:21:57 +0100

    Use gstore_brange to specify nb_kq (higher priority than nband)
    Former-commit-id: 5c3c86857d1b635c2b9ba4820ac3e97eb2711b40
    Former-commit-id: b90e3f2f7a5371f522c4040e4c249265ccd754ee

--- src/78_eph/m_gwpt.F90
+++ src/78_eph/m_gwpt.F90
@@ -357,8 +357,8 @@ subroutine gwpt_run(wfk0_path, dtfil, ngfft, ngfftf, dtset, cryst, ebands, dvdb,
                            "atom", dtset%gstore_gname, .False., comm)
  end if
 
+ ! FIXME: Fix problem with IO (some q-points are not written)
  ! Open GSTORE.nc file and go to data mode.
- ! TODO: Fix problem with IO (some q-points are not written)
  !NCF_CHECK(nctk_open_modify(root_ncid, gstore%path, comm))
  ! use xmpi_comm_self otherwise there will be a deadlock on lemaitre4
  NCF_CHECK(nctk_open_modify(root_ncid, gstore%path, xmpi_comm_self))
