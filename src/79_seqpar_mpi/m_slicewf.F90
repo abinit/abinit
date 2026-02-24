a08018dfa9b011319d30dd62ebad42313fa18ab1
Ioanna Lygatsika <ioanna-maria.lygatsika@cea.fr>
2026-02-24 21:04:40 +0100

    fixed ndbfuf not useful actually but bug solved
    Former-commit-id: a0dd704d83e5785d3ffeac80763b212ca1e0fe28
    Former-commit-id: 92e18bae9141cbfd4b7c342a9a14a41d5408279c

--- src/79_seqpar_mpi/m_slicewf.F90
+++ src/79_seqpar_mpi/m_slicewf.F90
@@ -238,7 +238,7 @@ subroutine slicewf(cg,dtset,eig,occ,enl_out,gs_hamk,mpi_enreg,&
  write(std_out,*) 'spacedim=', spacedim
 
  call slice_init(slice,dtset%nslice,nband,spacedim,dtset%tolwfr_diago,dtset%paral_kgb,&
-        dtset%paral_slice,dtset%mdeg_filter,dtset%tolfilter,dtset%ecut,l_mpi_enreg%bandpp,&
+        dtset%paral_slice,dtset%mdeg_filter,dtset%nbdbuf,dtset%tolfilter,dtset%ecut,l_mpi_enreg%bandpp,&
         space,spacecom,me_g0,me_g0_fft,l_paw,l_mpi_enreg%comm_spinorfft,l_mpi_enreg%comm_band,&
         dtset%spectral_cut,l_gs_hamk%gpu_option,gpu_kokkos_nthrd=dtset%gpu_kokkos_nthrd,&
         gpu_thread_limit=dtset%gpu_thread_limit)
