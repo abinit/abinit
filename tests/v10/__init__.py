dd56ca11a0e0bd1958dcb7f78ae6e64e3c274452
Ioanna Lygatsika <ioanna-maria.lygatsika@cea.fr>
2026-03-27 01:47:35 +0100

    add test without MPI paral_kgb=0
    Former-commit-id: 51b78eeba2fa3fd9d713555062a5a6e90ad76873

--- tests/v10/__init__.py
+++ tests/v10/__init__.py
@@ -66,6 +66,7 @@ inp_files = [
     "t110.abi",  # Test for toldmag
     "t121.abi",  # Test for hybrid function using fock_icutcoul, fock_rcut
     "t122.abi",  # Test meta-GGA NC pseudopotential reading: compare PSML vs UPF for bulk Si
-    "t150.abi",  # Test sequential slice execution in spectrum slicing diagonalization
-    "t151.abi",  # Test parallel slice execution in spectrum slicing diagonalization
+    "t150.abi",  # Test sequential slice execution in spectrum slicing diagonalization with 4 MPI
+    "t151.abi",  # Test parallel slice execution in spectrum slicing diagonalization with 4 MPI
+    "t152.abi",  # Test sequential slice execution in spectrum slicing diagonalization without MPI
 ]
