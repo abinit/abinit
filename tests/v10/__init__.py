4e9e58e4acb80ee154e7742bc7dcb147cc5f8eb3
Ioanna Lygatsika <ioanna-maria.lygatsika@cea.fr>
2026-03-31 11:18:30 +0200

    fix tests
    Former-commit-id: 804552e653fb849b45e67af272a8705bf95d67c6

--- tests/v10/__init__.py
+++ tests/v10/__init__.py
@@ -68,5 +68,5 @@ inp_files = [
     "t122.abi",  # Test meta-GGA NC pseudopotential reading: compare PSML vs UPF for bulk Si
     "t150.abi",  # Test sequential slice execution in spectrum slicing diagonalization with 4 MPI
     "t151.abi",  # Test parallel slice execution in spectrum slicing diagonalization with 4 MPI
-    "t152.abi",  # Test sequential slice execution in spectrum slicing diagonalization without MPI
+    "t152.abi",  # Test sequential slice execution in spectrum slicing diagonalization with 1 MPI
 ]
