d5f2480767bf6dc83fd44545b08582c71bde334a
Marc Sarraute <marc.sarraute@asplus.fr>
2026-04-02 00:56:56 +0200

    Add helpers for retrieving CUDA/HIP versions and display it in output
    Former-commit-id: 17b3ab76eb08b543842071a7e51dd5232df96374

--- shared/common/src/17_gpu_toolbox/m_gpu_toolbox.F90
+++ shared/common/src/17_gpu_toolbox/m_gpu_toolbox.F90
@@ -86,6 +86,16 @@ module m_gpu_toolbox
 
   interface
 
+    function gpu_get_lib_version_major() bind(c, name='gpu_get_lib_version_major_cpp') result(major)
+      use, intrinsic :: iso_c_binding
+      integer(kind=c_int) :: major
+    end function gpu_get_lib_version_major
+
+    function gpu_get_lib_version_minor() bind(c, name='gpu_get_lib_version_minor_cpp') result(minor)
+      use, intrinsic :: iso_c_binding
+      integer(kind=c_int) :: minor
+    end function gpu_get_lib_version_minor
+
     subroutine gpu_device_synchronize() bind(c, name='gpu_device_synchronize_cpp')
       use, intrinsic :: iso_c_binding
     end subroutine gpu_device_synchronize
