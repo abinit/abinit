27f594aedb6e282ad8c9b56a802ceeb651db8602
Miquel Royo <mroyo@icmab.es>
2026-04-17 14:29:03 +0200

    Move tests for scalar potential and 2D coulomb cutoff from v9 to 10 directory
    Former-commit-id: a3774ef080af3e859a0b37630f80426acd44a5f7

--- tests/v10/__init__.py
+++ tests/v10/__init__.py
@@ -48,6 +48,9 @@ inp_files = [
     "t44.abi" ,  # test all zora input values on orbmag calculation
     "t50.abi" ,  # test the projected SOC for GBT
     "t51.abi" ,  # test spin quantization axis
+    "t52.abi",  # test linear-response calculation with 2D Coulomb cutoff in BN monolayer
+    "t53.abi",  # test linear-response calculation with 2D short-range Coulomb kernel in BN monolayer
+    "t54.abi",  # test finite-q scalar potential linear-response calculation with PCM short-circuit conditions
     "t61.abi" ,  # test total energy due to electron-phonon interaction: step 1 DFT+DFPT
     "t62.abi" ,  # test total energy due to electron-phonon interaction: step 2 quadrupole
     "t63.abi" ,  # test total energy due to electron-phonon interaction: step 3 merge DDB
