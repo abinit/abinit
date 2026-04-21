ad7209af0255cfc84f92864451856a613471aa5b
Matteo Giantomassi <gmatteo@gmail.com>
2026-04-21 16:59:17 +0200

    suite_report.html --> index.html
    Former-commit-id: 4acadde77d664f09b8966530a670f57ae12851bf

--- tests/pymods/testsuite.py
+++ tests/pymods/testsuite.py
@@ -4555,7 +4555,7 @@ class AbinitTestSuite:
                 "status2html": status2html,
             }
 
-            fname = os.path.join(self.workdir, "suite_report.html")
+            fname = os.path.join(self.workdir, "index.html")
             fh = open(fname, "w")
 
             header = """
