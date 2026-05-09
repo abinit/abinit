8d44c8374032a1e4f869d9ba17264305130ea536
trunk <trunk@abinit.org>
2026-05-09 07:52:26 +0200

    Finalize v10.6.6
    Former-commit-id: f6dcb226e990835f619d4c84d52f5865fa9954bb

--- shared/common/src/14_hidewrite/m_specialmsg.F90
+++ shared/common/src/14_hidewrite/m_specialmsg.F90
@@ -206,8 +206,8 @@ subroutine herald(code_name,code_version,iout)
 
 !RELEASE TIME FROM ABIRULES
  year_rel=2026
- mm_rel=3
- version_rel='10.6.5 '
+ mm_rel=5
+ version_rel='10.6.6 '
 !END OF RELEASE TIME
 
 !The technique used hereafter is the only one that we have found to obtain
