0cbe5c1e45d20ddba911f6e0d6ca1e7917557e6a
Josef Zwanziger <jzwanzig@dal.ca>
2026-06-05 14:45:37 -0300

    trying different switches with zora < 0 cases
    Former-commit-id: b0ce2bd8a5d03cd801a56ed792ba88593c7301e4
    Former-commit-id: 9befc4d5bfefd9b0c17cd2fefc9bcb6dde801c5d

--- shared/libpaw/src/m_pawdij.F90
+++ shared/libpaw/src/m_pawdij.F90
@@ -2918,9 +2918,9 @@ subroutine pawdijso(dijso,cplex_dij,qphase,ndij,nspden,pawang,pawrad,pawtab,&
    case(-4)
      use_soc=.FALSE.; use_sd=.TRUE.; use_fc=.TRUE.
    case(-3)
-     use_soc=.FALSE.; use_sd=.FALSE.; use_fc=.TRUE.
+     use_soc=.TRUE.; use_sd=.FALSE.; use_fc=.TRUE.
    case(-2)
-     use_soc=.FALSE.; use_sd=.TRUE.; use_fc=.FALSE.
+     use_soc=.TRUE.; use_sd=.TRUE.; use_fc=.FALSE.
    case(-1)
      use_soc=.TRUE.; use_sd=.FALSE.; use_fc=.FALSE.
    case(1)
