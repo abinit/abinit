0bca296738d2308b047e616bad46743bbd7ce3d7
James Boust <james.boust@cea.fr>
2026-08-06 14:40:21 +0200

    Edit m_paw_atom_solve.F90
    Former-commit-id: b106694dbb181486c2946b557819f739c0bdd5e6
    Former-commit-id: 1472962064079db54bcb14aa040c58dbc78f8372

--- shared/libpaw/src/m_paw_atom_solve.F90
+++ shared/libpaw/src/m_paw_atom_solve.F90
@@ -1162,7 +1162,7 @@ subroutine atompaw_init(pawtab,pawrad,atp,atm,sctol,elin_mode,vhtnzc_mode,tpaw_m
      endif
    enddo
    file_xml_core=TRIM(atp%pot%sym)//'-tmp.corewf.xml'
-   call xmlprtcore(trim(file_xml_core),atp,zcore,mesh_data,input_string)
+   call xmlprtcore(trim(file_xml_core),atp,zcore,mesh_data,atp%input_string)
    file_xml_core=trim(atp%pot%sym)//'-tmp.xml'
    call destroy_mesh_data(mesh_data)
  else
